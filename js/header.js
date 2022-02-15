99aed909a0f87aeb1ec92619c1ae6a96c5028436
sju02048 <sju02048@naver.com>
2022-02-15 17:28:54 +0900

    생성: header image

---
+++ js/header.js
@@ -0,0 +1,12 @@
+class Header {
+  constructor(params) {
+    this.view();
+  }
+
+  view() {
+    // const kakaopage = document.querySelector(".kakaopage");
+    // console.log("kakaopage :>> ", kakaopage);
+  }
+}
+
+const header = new Header();
