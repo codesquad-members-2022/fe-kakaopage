b3307568641ff16f8f3b1f4692424a233e1ed40e
sju02048 <sju02048@naver.com>
2022-02-16 09:43:26 +0900

    추가: 헤더부분 사진 삽입

--- js/header.js
+++ js/header.js
@@ -1,12 +1,45 @@
+import { $ } from "./util.js";
+const imagePath = "/image/header";
+
 class Header {
   constructor(params) {
-    this.view();
+    this.kakaopage();
+    this.kakaopage_menu();
   }
 
-  view() {
-    // const kakaopage = document.querySelector(".kakaopage");
-    // console.log("kakaopage :>> ", kakaopage);
+  kakaopage() {
+    $("h1.kakaopage").insertAdjacentHTML(
+      "beforeend",
+      this.addTag("img", "css-kakaopage")
+    );
+    $("img.css-kakaopage").src = `${imagePath}/logo-kakaopage.svg`;
+  }
+
+  kakaopage_menu() {
+    // search
+    $("div.search-wrap").insertAdjacentHTML(
+      "beforeend",
+      this.addTag("img", "css-searchBtn")
+    );
+    $("img.css-searchBtn").src = `${imagePath}/btn_search.png`;
+
+    // stage
+    $("div.menu-stage").insertAdjacentHTML(
+      "beforeend",
+      this.addTag("img", "css-stage")
+    );
+    $("img.css-stage").src = `${imagePath}/pc-logo-stage.svg`;
+
+    $("div.menu-stage-tooltip").insertAdjacentHTML(
+      "beforeend",
+      this.addTag("img", "css-stage-tooltip")
+    );
+    $("img.css-stage-tooltip").src = `${imagePath}/stage-tooltip.svg`;
+  }
+
+  addTag(tagName, className) {
+    return `<${tagName} class="${className}"></${tagName}`;
   }
 }
 
-const header = new Header();
+export default Header;
