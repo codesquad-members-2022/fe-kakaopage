f4bca74bf0b4494ba1c4213a30ce137d3687f50a
Muffin <jinlog9@gmail.com>
2022-02-16 17:00:00 +0900

    refactoring: footer 영역 펼치기/접기 아이콘 선택시 detail 영역 js classList toggle 이벤트 사용

--- footer.js
+++ footer.js
@@ -2,17 +2,12 @@ export default function footer() {
     const businessInfoEl = document.querySelector(".footer__business_info");
     const businessDetails = document.querySelector(".footer__details");
     businessInfoEl.addEventListener("click", ()=> {
-        if(businessInfoEl.classList.contains("footer__business_info_off")) {
-            businessInfoEl.classList.remove("footer__business_info_off");
-            businessInfoEl.classList.add("footer__business_info_on");
-            businessInfoEl.style.setProperty("--background", "url(images/icon_trace_on.png)");
-            businessDetails.style.display = 'block';
-        } else if(businessInfoEl.classList.contains("footer__business_info_on")) {
-            businessInfoEl.classList.remove("footer__business_info_on");
-            businessInfoEl.classList.add("footer__business_info_off");
-            businessInfoEl.style.setProperty("--background", "url(images/icon_trace_off.png)");
-            businessDetails.style.display = 'none';
-        }
+        businessInfoEl.classList.toggle("footer__business_info_off")
+        businessDetails.classList.toggle("no_display")
+
+        businessDetails.classList.contains("no_display") ? 
+        businessInfoEl.style.setProperty("--background", "url(images/icon_trace_off.png)") :
+        businessInfoEl.style.setProperty("--background", "url(images/icon_trace_on.png)");
     });
 
 }
