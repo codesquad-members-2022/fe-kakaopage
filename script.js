24b6096c1f081ddec50ad74dba518b5ed07ff420
Inu Jung <inust33@gmail.com>
2022-02-16 15:27:05 +0900

    [미션1] Inu -카카오페이지 웹툰 탭  (#18)
    * header finished
    
    * Banner Box done
    
    * ad banner & daily comics

---
+++ script.js
@@ -0,0 +1,7 @@
+document.body.style.cssText= `display:flex; justify-content:center; background-color:gray; max-width:300px`
+
+const header = document.createElement('header');
+header.style.cssText=`width:100vw; background-color:white; display:flex; height:100px;`
+const nav = document.createElement('nav');
+const section = document.createElement('section');
+const footer = document.createElement('footer');
