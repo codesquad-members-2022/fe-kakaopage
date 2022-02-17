18ff1e3bf9b1803ac6d3f0c822c5fe4c41352d53
Millie <jay.lee.2280@gmail.com>
2022-02-17 14:37:31 +0900

    Feat: load webtoon data asynchronously & tab switching

--- src/js/index.js
+++ src/js/index.js
@@ -1,23 +1,61 @@
-const navHeader = document.querySelector('.nav--header');
-const mainContents = document.querySelectorAll('.main-contents');
+import { DATA_URL, $ } from './util.js';
 
-mainContents.forEach(node => {
-  node.style.display = 'none';
-});
+const $days = $('.days');
+const $webtoons = $('.webtoons');
 
-function handleNavHeaderClick(e) {
-  if (e.target.tagName === 'LI') {
-    for (let i = 0; i < e.target.parentNode.children.length; i++) {
-      e.target.parentNode.children[i].classList.remove('selected-nav-header');
-    }
-    e.target.classList.add('selected-nav-header');
+async function loadWebtoons() {
+  // Better way?
+  const response = await fetch(DATA_URL);
+  const wtJson = await response.json();
+  const webtoons = await wtJson.webtoon;
+  return webtoons;
 
-    mainContents.forEach(node => {
-      node.style.display = 'none';
-    });
-
-    document.getElementById(e.target.dataset.id).style.display = 'block';
-  }
+  // return fetch(DATA_URL)
+  //   .then(response => response.json())
+  //   .then(json => json.webtoon);
 }
 
-navHeader.addEventListener('click', handleNavHeaderClick);
+function displayWebtoons(wt) {
+  $webtoons.innerHTML = wt.map(w => createWebtoonList(w)).join('');
+}
+
+function createWebtoonList(wt) {
+  return `
+  <li class="wt">
+    <img class="wt__image" src="${wt.url}" alt="webtoon-image"/>
+    <div class="wt__discription">
+      <p class="title">${wt.title}</p>
+      <p class="summary">${wt.summary}</p>
+      <p class="info">${wt.info}</p>
+    </div>
+  </li>
+  `;
+}
+
+function filterWebtoons(wt) {
+  $days.addEventListener('click', e => {
+    if (!e.target.matches('.days > .day__name')) return;
+    activateDay(e);
+    filterWebtoonsByDay(e, wt);
+  });
+}
+
+function filterWebtoonsByDay(e, wt) {
+  const day = e.target.id;
+  day === 'whole'
+    ? displayWebtoons(wt)
+    : displayWebtoons(wt.filter(v => v.day === day));
+}
+
+function activateDay({ target }) {
+  [...$days.children].forEach(day => {
+    day.classList.toggle('selected-day', day === target);
+  });
+}
+
+loadWebtoons()
+  .then(wt => {
+    displayWebtoons(wt.filter(v => v.day === 'mon'));
+    filterWebtoons(wt);
+  })
+  .catch(console.log);
---
+++ src/js/util.js
@@ -0,0 +1,3 @@
+export const DATA_URL = 'data/data.json';
+
+export const $ = selector => document.querySelector(selector);
