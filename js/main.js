f99838fd046b53caa2be35a9de4d61a4db14115d
Muffin <jinlog9@gmail.com>
2022-02-18 02:26:18 +0900

    feat: main(영역) 함수에서 만든 컴포넌트들 추가, 삭제 할 수 있도록 함수형태로 분리

--- js/main.js
+++ js/main.js
@@ -1,17 +1,36 @@
 import { $ } from "./utils.js";
 import { issueComponent } from "../component/issueComponent.js";
-import { issueObj } from "./constants.js";
+import { advertiseComponent } from "../component/advertiseComponent.js";
+import { issueObj, recommendationObj, rowListTitleObj } from "./constants.js";
+import { rowListComponent } from "../component/rowListComponent.js";
 
 const main = {
+    addAdvertiseComponent: function() {
+        this.removeAdvertiseComponent();
+        return advertiseComponent();
+    },
+    removeAdvertiseComponent: function() {
+        const advertiseEl = $(".advertise");
+        if(advertiseEl === null) return;
+        advertiseEl.remove();
+    },
     addIssueComponent: function(menu) {
         this.removeIssueComponent();
-        const carousel = $(".carousel");
-        carousel.insertAdjacentHTML("afterend", issueComponent(issueObj[menu]));
+        return issueComponent(issueObj[menu]);
     },
     removeIssueComponent: function() {
         const issueEl = $(".issue");
         if(issueEl === null) return;
         issueEl.remove();
+    },
+    addRecommendationComponent: function(menu) {
+        this.removeRecommendationComponent();
+        return rowListComponent(rowListTitleObj.recommendation, recommendationObj[menu]);
+    },
+    removeRecommendationComponent: function() {
+        const recommendEl = $(".recommend");
+        if(recommendEl === null) return;
+        recommendEl.remove();
     }
 }
 
