d23a60bde33013bf539a39e1ac5d590377de745f
youryu0212 <youryu0212@naver.com>
2022-02-18 05:40:24 +0900

    add : create event handler

---
+++ handler.js
@@ -0,0 +1,14 @@
+import {changeImageBoxInfo} from './change_image_box.js';
+import {changeImageCardInfo} from './change_image_card.js';
+import {weeklyDataInfo} from './data.js';
+
+let boxInfo,cardInfo;
+const handler = (boxInfo,cardInfo) => {
+  changeImageBoxInfo(boxInfo);
+  changeImageCardInfo(cardInfo);
+}
+
+const el = document.querySelectorAll(".weekly-nav");
+for (let i = 0 ; i<el.length ; i++){
+  el[i].addEventListener("click", ()=>{handler(weeklyDataInfo[i]["box"],weeklyDataInfo[i]["card"])},false);
+}
