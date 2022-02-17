ec9e26e64b40d15cd3afc54b777ed30ebe276786
suuzin <8oozin@gmail.com>
2022-02-18 00:14:05 +0900

    build js파일

---
+++ kakao.js
@@ -0,0 +1,10 @@
+//웹툰을 클릭한다.
+const mainCategoryMenu = document.querySelector(".main_category ul li:nth-child(3)");
+const slideShow = document.querySelector(".slideshow_slide");
+function changeContent(){
+    console.log("test");
+    //슬라이드 그림이 바뀐다.
+    slideShow.style.background 
+
+}
+mainCategoryMenu.addEventListener("click",changeContent);
