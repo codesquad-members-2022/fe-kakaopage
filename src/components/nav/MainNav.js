d992892c076c7df682f9d8794f37a7ded4ad8cb4
mina-gwak <mina.gwak@kakao.com>
2022-02-18 03:14:39 +0900

    feat: Create main nav component

---
+++ src/components/nav/MainNav.js
@@ -0,0 +1,5 @@
+import Nav from './Nav.js';
+
+const MainNav = (mainNavItems) => `<nav class="contents main-nav">${Nav(mainNavItems)}</nav>`;
+
+export default MainNav;
---
+++ src/data/index.js
@@ -0,0 +1,40 @@
+const MainNavItems = [
+  {
+    name: '홈',
+    isSelected: true,
+  },
+  {
+    name: '요일연재',
+    isSelected: false,
+  },
+  {
+    name: '웹툰',
+    isSelected: false,
+  },
+  {
+    name: '소년',
+    isSelected: false,
+  },
+  {
+    name: '드라마',
+    isSelected: false,
+  },
+  {
+    name: '로맨스',
+    isSelected: false,
+  },
+  {
+    name: '로판',
+    isSelected: false,
+  },
+  {
+    name: '액션무협',
+    isSelected: false,
+  },
+  {
+    name: 'BL',
+    isSelected: false,
+  },
+];
+
+export { MainNavItems };
