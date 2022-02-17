11bdbc2eeefa0ac5bda2f60c86db43ada45f35e5
SeungHyun <fm10033@gmail.com>
2022-02-18 00:03:15 +0900

    요일 클릭시 노란 밑줄 구현

--- kakaopageMain.js
+++ kakaopageMain.js
@@ -1,7 +1,7 @@
-function changeNavBorderColor(e) {
-  const navTabs = e.parentNode.childNodes;
-  for (let i = 1; i < navTabs.length; i += 2) {
-    const tab = navTabs[i];
+function changeBorderColor(e) {
+  const Tabs = e.parentNode.childNodes;
+  for (let i = 1; i < Tabs.length; i += 2) {
+    const tab = Tabs[i];
     tab.style.borderBottom = "none";
   }
   e.style.borderBottom = "3px solid #ffd200";
