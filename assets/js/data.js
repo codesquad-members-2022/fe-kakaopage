93a3fca6acd695c77271a39648d8d3db04c8a8af
ju-kkim <wndus8198@naver.com>
2022-02-18 00:17:49 +0900

    Feat: Top banner 데이터 추가

---
+++ assets/js/data.js
@@ -0,0 +1,220 @@
+const topBanner = {
+    home : [
+        {
+            title : "내가 죽였다",
+            image : "https://dn-img-page.kakao.com/download/resource?kid=bu1cDf/hzp2e5hDx9/kk12ZeaZln5VkyfoC4dfak",
+            badge : {
+                name : "이벤트",
+                src : "https://static-page.kakao.com/static/pc/badge-bigthum-event.svg?2c00fc6eb18517e8f006adfaf464530b",
+            },
+            category : "웹툰",
+            viewer : "20.5만",
+            description : "누가 아군이고, 누가 적인가."
+        },
+        {
+            title : "미슐랭스타",
+            image : "https://dn-img-page.kakao.com/download/resource?kid=bu1cDf/hzp2e5hDx9/kk12ZeaZln5VkyfoC4dfak",
+            badge : {
+                name : "이벤트",
+                src : "https://static-page.kakao.com/static/pc/badge-bigthum-event.svg?2c00fc6eb18517e8f006adfaf464530b",
+            },
+            category : "웹툰",
+            viewer : "59만",
+            description : "최고의 쉐프가 되는건 나야!"
+        }
+    ],
+    day : [
+        {
+            title : "지옥에서 돌아온 성좌님",
+            image : "https://dn-img-page.kakao.com/download/resource?kid=vY9EO/hzhOm4DsAe/ooN3xLPLUZPU6Uid2Matlk",
+            badge : {
+                name : "up",
+                src : "https://static-page.kakao.com/static/pc/badge-bigthum-up.svg?a70b9cea4cb6b972e794d199820782a2",
+            },
+            category : "웹툰",
+            viewer : "46.7만",
+            description : "인간 최초의 성좌가 되는 길!"
+        },
+        {
+            title : "딸 바보가<br>되었습니다",
+            image : "https://dn-img-page.kakao.com/download/resource?kid=NfO9u/hy41PSMsl2/KOnmIlC3bI2q5Lk46RuLL1",
+            badge : {
+                name : "up",
+                src : "https://static-page.kakao.com/static/pc/badge-bigthum-up.svg?a70b9cea4cb6b972e794d199820782a2",
+            },
+            category : "웹툰",
+            viewer : "55.2만",
+            description : "갑자기 생긴 딸이 너무 귀여워?!"
+        }
+    ],
+    webtoon : [
+        {
+            title : "0번째 마법사",
+            image : "https://dn-img-page.kakao.com/download/resource?kid=crcdq/hzb7xD95BW/CrwhRk7eAC9dtQV5Oi3Crk",
+            badge : {
+                name : "HOT",
+                src : "https://static-page.kakao.com/static/pc/badge-bigthum-hot.svg?a7f7622e9d554d5e0ad1517a53de3ad3",
+            },
+            category : "웹툰",
+            viewer : "11.5만",
+            description : "천재 마법사의 몰락과 대재앙"
+        },
+        {
+            title : "내가 죽였다",
+            image : "https://dn-img-page.kakao.com/download/resource?kid=bu1cDf/hzp2e5hDx9/kk12ZeaZln5VkyfoC4dfak",
+            badge : {
+                name : "이벤트",
+                src : "https://static-page.kakao.com/static/pc/badge-bigthum-event.svg?2c00fc6eb18517e8f006adfaf464530b",
+            },
+            category : "웹툰",
+            viewer : "20.5만",
+            description : "누가 아군이고, 누가 적인가."
+        }
+    ],
+    boy : [
+        {
+            title : "재앙급 영웅님이<br>귀환하셨다",
+            image : "https://dn-img-page.kakao.com/download/resource?kid=h9pKI/hzhOj1GHgu/nknGkl2t4iCPpKKKWcrrx0",
+            badge : {
+                name : "up",
+                src : "https://static-page.kakao.com/static/pc/badge-bigthum-up.svg?a70b9cea4cb6b972e794d199820782a2",
+            },
+            category : "웹툰",
+            viewer : "57.3만",
+            description : "우리가 죽인 놈이 돌아왔다고?!"
+        },
+        {
+            title : "리턴 서바이벌",
+            image : "https://dn-img-page.kakao.com/download/resource?kid=bDvkGC/hzacbW2cAQ/ZS1Z5mX4Mpk9yN6A82B8q0",
+            badge : {
+                name : "up",
+                src : "https://static-page.kakao.com/static/pc/badge-bigthum-up.svg?a70b9cea4cb6b972e794d199820782a2",
+            },
+            category : "웹툰",
+            viewer : "62.7만",
+            description : "6개월 후, 이 세상은 멸망한다!"
+        }
+    ],
+    drama : [
+        {
+            title : "정령왕의 딸",
+            image : "https://dn-img-page.kakao.com/download/resource?kid=ce21It/hyF23tXM8m/0wARwb5At6jjKwlfj3Kjo1",
+            badge : {
+                name : "이벤트",
+                src : "https://static-page.kakao.com/static/pc/badge-bigthum-event.svg?2c00fc6eb18517e8f006adfaf464530b",
+            },
+            category : "웹툰",
+            viewer : "75만",
+            description : "우리 아빠가 정령왕이라고?!"
+        },
+        {
+            title : "미슐랭스타",
+            image : "https://dn-img-page.kakao.com/download/resource?kid=bu1cDf/hzp2e5hDx9/kk12ZeaZln5VkyfoC4dfak",
+            badge : {
+                name : "이벤트",
+                src : "https://static-page.kakao.com/static/pc/badge-bigthum-event.svg?2c00fc6eb18517e8f006adfaf464530b",
+            },
+            category : "웹툰",
+            viewer : "59만",
+            description : "최고의 쉐프가 되는건 나야!"
+        }
+    ],
+    romance : [
+        {
+            title : "카니발 베이비",
+            image : "https://dn-img-page.kakao.com/download/resource?kid=crmCtI/hzp2fpA944/JXIxqzQgkXxsNtm4WY9KYk",
+            badge : {
+                name : "이벤트",
+                src : "https://static-page.kakao.com/static/pc/badge-bigthum-event.svg?2c00fc6eb18517e8f006adfaf464530b",
+            },
+            category : "웹툰",
+            viewer : "17만",
+            description : "기다무 런칭기념, 캐시 이벤트!"
+        },
+        {
+            title : "별이 빈 소원",
+            image : "https://dn-img-page.kakao.com/download/resource?kid=b3bP2H/hzmU0W4e9h/XQ3hCkptBgc9vRWEtZYzKK",
+            badge : {
+                name : "이벤트",
+                src : "https://static-page.kakao.com/static/pc/badge-bigthum-event.svg?2c00fc6eb18517e8f006adfaf464530b",
+            },
+            category : "웹툰",
+            viewer : "12.5만",
+            description : "기다무 런칭기념, 캐시 이벤트!"
+        }
+    ],
+    romance_fantasy : [
+        {
+            title : "프린세스",
+            image : "https://dn-img-page.kakao.com/download/resource?kid=3WZCZ/hzp2jyLZrg/vfuKRbyRrb7ECbNjXmdCe0",
+            badge : {
+                name : "이벤트",
+                src : "https://static-page.kakao.com/static/pc/badge-bigthum-event.svg?2c00fc6eb18517e8f006adfaf464530b",
+            },
+            category : "웹툰",
+            viewer : "30만",
+            description : "기간한정, 1시간마다 무료! "
+        },
+        {
+            title : "명랑자매백서",
+            image : "https://dn-img-page.kakao.com/download/resource?kid=cIkbV/hzp2jeskqa/yfdCWUlr912AfAaQ0TPep1",
+            badge : {
+                name : "이벤트",
+                src : "https://static-page.kakao.com/static/pc/badge-bigthum-event.svg?2c00fc6eb18517e8f006adfaf464530b",
+            },
+            category : "웹툰",
+            viewer : "12.2만",
+            description : "기간한정, 10화까지 무료!"
+        }
+    ],
+    action : [
+        {
+            title : "생사결: 여래의 복수",
+            image : "https://dn-img-page.kakao.com/download/resource?kid=bvTf2k/hyVkNjqnei/i5Nufi7Nw0jpL2uRvcn7Q1",
+            badge : {
+                name : "up",
+                src : "https://static-page.kakao.com/static/pc/badge-bigthum-up.svg?a70b9cea4cb6b972e794d199820782a2",
+            },
+            category : "웹툰",
+            viewer : "58.1만",
+            description : "여래 현현! 다 무릎을 꿇어라!"
+        },
+        {
+            title : "무림제왕의 은퇴생활",
+            image : "https://dn-img-page.kakao.com/download/resource?kid=vMdT6/hzp2j6Bg2G/21JsrgScl5ufPAYDdQl0Qk",
+            badge : {
+                name : "이벤트",
+                src : "https://static-page.kakao.com/static/pc/badge-bigthum-event.svg?2c00fc6eb18517e8f006adfaf464530b",
+            },
+            category : "웹툰",
+            viewer : "9.7만",
+            description : "무림 맹주 같은 거, 너나 해!"
+        }
+    ],
+    BL : [
+        {
+            title : "낯선 농담",
+            image : "https://dn-img-page.kakao.com/download/resource?kid=bSJzu3/hzp2dZBYCr/kLD95SpQhco8xykTq1DE6k",
+            badge : {
+                name : "이벤트",
+                src : "https://static-page.kakao.com/static/pc/badge-bigthum-event.svg?2c00fc6eb18517e8f006adfaf464530b",
+            },
+            category : "웹툰",
+            viewer : "12.7만",
+            description : "또 시작해버렸다, 짝사랑"
+        },
+        {
+            title : "일상다정사",
+            image : "https://dn-img-page.kakao.com/download/resource?kid=GEAde/hzb7yJ6WrU/8Nw8XS00UyJVlQaJBRakMK",
+            badge : {
+                name : "추천",
+                src : "https://static-page.kakao.com/static/pc/badge-bigthum-recommend.svg?9cba9a2bf1ae05f840c588da3ed9d37f",
+            },
+            category : "웹툰",
+            viewer : "55.5만",
+            description : "내 형 말고, 나랑 사귀자."
+        }
+    ],
+}
+
+export {topBanner}
