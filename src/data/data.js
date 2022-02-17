017676170e56c0edc7737ad321fbc2c68c091788
Sangbeom Heo <mansaout@gmail.com>
2022-02-18 02:50:09 +0900

    Feat: "Add 웹툰탭의 콘텐츠 데이터"

---
+++ src/data/data.js
@@ -0,0 +1,50 @@
+const webtoonData = {
+  category: ['홈', '요일연재', '소년', '드라마', '로맨스', '로판', '액션무협', 'BL'],
+  0: {
+    mainBanner: [
+      {
+        title: '도사강호',
+        caption: '시각이 다르면 무공도 다르다',
+        tag: 'up',
+        type: '웹툰',
+        view: '30.5',
+        path: 'static/thumb/main_toon_1.png',
+      },
+      {
+        title: '내가 죽였다',
+        caption: '누가 아군이고, 누가 적인가',
+        tag: 'event',
+        type: '웹툰',
+        view: '20.4',
+        path: 'static/thumb/main_toon_2.png',
+      },
+    ],
+    gridMenu: [
+      ['오늘 UP', '195'],
+      ['오늘 신작', '3'],
+      ['오리지널', '2,335'],
+      ['완결까지 정주행'],
+      ['독립운동가 웹툰'],
+      ['오늘 랭킹', '1위'],
+    ],
+    promotionBanner: ['promo_toon_1.png', 'promo_toon_2.png'],
+  },
+};
+
+export { webtoonData };
+
+// const Item = {
+//   id: 'a0001', // id
+//   title: '플랜터스의 정원', // 제목
+//   caption: '저는 당신이 마음에 들었어요!', // 캡션
+//   type: '웹툰', // 콘텐츠 종류: 웹소설 or 웹툰
+//   category: '드라마', // 카테고리: 소년, 드라마, 로맨스, 로판, 액션무협, BL, 판타지, 현판, 무협, 판드, 단행본
+//   day: 5, // 연재 요일: 0, 1, 2, 3, 4, 5, 6
+//   author: '김현식', // 작가
+//   score: 7.5, // 별점: 0.0~10.0
+//   views: 3.5, // 조회수(만 명)
+//   age: 'all', // 연령: all, 15
+//   new: true, // 신규 여부: true, false
+//   up: true, // 업데이트 여부: true, false
+//   crown: true, // 왕관 여부: true, false
+// };
