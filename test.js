b8368f7e042b278bc8169d2b089fa2c2fe1fe2d6
otterp012 <otterp012@gmail.com>
2022-02-17 14:47:00 +0900

    fix: section 부분 css 수정

---
+++ test.js
@@ -0,0 +1,19 @@
+
+function renderArticle__webtoonRowList() {
+    return `                <li class="article__webtoon-row-list">
+    <div class="article__webtoon-row-img-wrapper">
+        <img src="./useimages/환골탈태.png" alt="">
+    </div>
+    <div class="article__webtoon-row-text-wrapper">
+        <h3 class="article__webtoon-row-tittle">
+            제목
+        </h3>
+        <p class="article__webtoon-row-text">천천히 치유되는 그날의 상처</p>
+        <p class="article__webtoon-row-text">1.3만명</p>
+    </div>
+</li>`
+}
+
+function renderArticle__webtoon() {
+    return 
+}
