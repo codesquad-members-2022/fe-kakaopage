eeefcf09227d85c492cde860425678f503666627
Muffin <jinlog9@gmail.com>
2022-02-18 02:25:10 +0900

    feat: 메뉴에 따라 광고 컴포넌트, 추천리스트 컴포넌트, 이슈 컴포넌트 레이아웃 배치. 이 부분 어떻게 하면 더 깔끔하게 만들지 고민하자

--- js/category.js
+++ js/category.js
@@ -6,37 +6,54 @@ const category = {
         const categoryEl = $(".main__nav-ul");
         categoryEl.addEventListener("click", (e) => {
             if(e.target.tagName === 'A') {
-                // 똑같은 곳 눌렀을시 -> return 
-
-                this.changeColor(categoryEl.children);
+                this.removeLinkColor(categoryEl.children);
                 e.target.classList.add('main__nav_link_active');
-
                 // 각 메뉴에 맞게 컴포넌트 핸들링?
-                // 나중에 함수로 따로 빼기
+
+                // if문 .... 나중에 함수로 따로 빼기
+                const carousel = $(".carousel");
                 if(e.target.textContent === '홈') {
-                    main.addIssueComponent('홈');
+                    main.removeRecommendationComponent();
+                    carousel.insertAdjacentHTML("afterend", main.addAdvertiseComponent('홈'));
+                    carousel.insertAdjacentHTML("afterend", main.addIssueComponent('홈'));
                 } else if(e.target.textContent === '요일연재') {
+                    main.removeAdvertiseComponent();
+                    main.removeRecommendationComponent();
                     main.removeIssueComponent();
                 } else if(e.target.textContent === '웹툰') {
+                    main.removeAdvertiseComponent();
+                    main.removeRecommendationComponent();
                     main.removeIssueComponent();
                 } else if(e.target.textContent === '소년') {
-                    main.addIssueComponent('소년');
+                    carousel.insertAdjacentHTML("afterend", main.addAdvertiseComponent('소년'));
+                    carousel.insertAdjacentHTML("afterend", main.addRecommendationComponent('소년'));
+                    carousel.insertAdjacentHTML("afterend", main.addIssueComponent('소년'));
                 } else if(e.target.textContent === '드라마') {
-                    main.addIssueComponent('드라마');
+                    carousel.insertAdjacentHTML("afterend", main.addAdvertiseComponent('드라마'));
+                    carousel.insertAdjacentHTML("afterend", main.addRecommendationComponent('드라마'));
+                    carousel.insertAdjacentHTML("afterend", main.addIssueComponent('드라마'));
                 } else if(e.target.textContent === '로맨스') {
-                    main.addIssueComponent('로맨스');
+                    carousel.insertAdjacentHTML("afterend", main.addAdvertiseComponent('로맨스'));
+                    carousel.insertAdjacentHTML("afterend", main.addRecommendationComponent('로맨스'));
+                    carousel.insertAdjacentHTML("afterend", main.addIssueComponent('로맨스'));
                 } else if(e.target.textContent === '로판') {
-                    main.addIssueComponent('로판');
+                    carousel.insertAdjacentHTML("afterend", main.addAdvertiseComponent('로판'));
+                    carousel.insertAdjacentHTML("afterend", main.addRecommendationComponent('로판'));
+                    carousel.insertAdjacentHTML("afterend", main.addIssueComponent('로판'));
                 } else if(e.target.textContent === '액션무협') {
-                    main.addIssueComponent('액션무협');
+                    carousel.insertAdjacentHTML("afterend", main.addAdvertiseComponent('액션무협'));
+                    carousel.insertAdjacentHTML("afterend", main.addRecommendationComponent('액션무협'));
+                    carousel.insertAdjacentHTML("afterend", main.addIssueComponent('액션무협'));
                 } else if(e.target.textContent === 'BL') {
-                    main.addIssueComponent('BL');
+                    carousel.insertAdjacentHTML("afterend", main.addAdvertiseComponent('BL'));
+                    carousel.insertAdjacentHTML("afterend", main.addRecommendationComponent('BL'));
+                    carousel.insertAdjacentHTML("afterend", main.addIssueComponent('BL'));
                 }
             }
         });
     },
     
-    changeColor: function(elements) {
+    removeLinkColor: function(elements) {
         for(const element of elements) {
             element.firstChild.classList.remove('main__nav_link_active')
         }
