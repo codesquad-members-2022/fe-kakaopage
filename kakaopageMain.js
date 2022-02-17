82edee1273acb280285716a9004ac16acf5a0e33
SeungHyun <fm10033@gmail.com>
2022-02-18 02:58:11 +0900

    readme 작성, 장르텝 클릭시 이미지 변경 구현

--- kakaopageMain.js
+++ kakaopageMain.js
@@ -139,24 +139,46 @@ const weekdayContents = [
   `,
   ],
 ];
-
-function resetCssStyle(e) {
+const eventContents = [
+  ["홈", "/FE/FE01-html/images/소설 _ 회귀자의 증권사 생활 6타 _ 15-24.png"],
+  [
+    "요일연재",
+    "/FE/FE01-html/images/소설 _ 마법 아카데미의 육체파 천재 8타 _ 0-24.png",
+  ],
+  [
+    "웹툰",
+    "/FE/FE01-html/images/소설 _ 그 악녀가 검을 든 이유 2타 _ 10-24.png",
+  ],
+  ["소년", "/FE/FE01-html/images/웹툰 _ 롱리브더킹 1타 _ 21-24.png"],
+  [
+    "드라마",
+    "/FE/FE01-html/images/웹툰 _ 8클래스 마법사의 회귀 0타 _ 21-23.png",
+  ],
+  ["로맨스", "/FE/FE01-html/images/웹툰 _ 학사재생 9타 _ 0-24.png"],
+  [
+    "로판",
+    "/FE/FE01-html/images/웹툰 _ 남주를 주웠더니 남편이 생겨버렸다 3타 _ 15-23.png",
+  ],
+  ["액션무협", "/FE/FE01-html/images/웹툰 _ 더 라이브7타 _ 21-24.png"],
+  ["BL", "/FE/FE01-html/images/소설 _ 그 악녀가 검을 든 이유 2타 _ 10-24.png"],
+];
+function changeTabsColor(e) {
   const Tabs = e.parentNode.childNodes;
   for (let i = 1; i < Tabs.length; i += 2) {
     const tab = Tabs[i];
     tab.style.borderBottom = "none";
     tab.style.color = "#999";
   }
+  e.style.color = `black`;
 }
 
 function changeBorderColor(e) {
-  resetCssStyle(e);
+  changeTabsColor(e);
   e.style.borderBottom = "3px solid #ffd200";
 }
 function changeDayAndContents(e) {
   changeBorderColor(e);
   const dayInfo = e.innerText;
-  e.style.color = "black";
   const weekdayContentWrap = document.querySelector("#weekdayContentWrapId");
   while (weekdayContentWrap.hasChildNodes()) {
     weekdayContentWrap.removeChild(weekdayContentWrap.firstChild);
@@ -169,6 +191,16 @@ function changeDayAndContents(e) {
       }
 
       weekdayContentWrap.insertAdjacentHTML("afterbegin", temp);
+    }
+  });
+}
+function changeEventSlideContents(e) {
+  const genre = e.innerText;
+  const slide = document.querySelector("#eventSlideId");
+  eventContents.map((content) => {
+    if (genre === content[0]) {
+      const imageUrl = content[1];
+      slide.style.backgroundImage = `url('${imageUrl}')`; // 안된다...
     }
   });
 }
