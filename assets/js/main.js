013bb71ef7082d1da7f3234a56882bf05b4aff77
ju-kkim <wndus8198@naver.com>
2022-02-18 00:18:17 +0900

    Feat: 탭 클릭시 top banner 변경 기능 추가

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,5 @@
+import {topBanner} from "./data.js";
+
 const gnb = document.querySelector(".gnb ul");
 const lnb = document.querySelector(".lnb ul");
 const dayNav = document.querySelector(".day_nav ul");
@@ -9,7 +11,9 @@ gnb.addEventListener("click", (e) => {
 
 lnb.addEventListener("click", (e) => {
     const eventTarget = e.target;
+    const targetLnb = eventTarget.closest('li').getAttribute('data-lnb');
     toActivateNav(eventTarget, lnb);
+    changeTopBanner(targetLnb);
 })
 
 dayNav.addEventListener("click", (e) => {
@@ -23,3 +27,40 @@ function toActivateNav(target, nav) {
     currentActive.classList.remove('active');
     clickTarget.classList.add('active');
 }
+
+function changeTopBanner(targetLnb) {
+    const banner = document.querySelector('.top_banner');
+    
+    const targetBannerData = topBanner[targetLnb];
+    let itemHtml = '';
+
+    targetBannerData.forEach(data => {
+        
+        itemHtml += `
+        <div class="item item_type1">
+            <div class="img">
+                <img src="${data.image}" alt="${data.title}">
+            </div>
+            <div class="txt">
+                <div class="info_box">
+                    <h3 class="ttl">${data.title}</h3>
+                    <div class="info">
+                        <span class="badge">
+                            <img src="${data.badge.src}" alt="${data.badge.name}">
+                        </span>
+                        <span class="info_txt cat">${data.category}</span>
+                        <span class="info_txt viewer">${data.viewer}명</span>
+                    </div>
+                </div>
+                <p class="desc">${data.description}</p>
+            </div>
+        </div>`;
+    });
+
+
+    banner.querySelector('.slider_items').innerHTML = itemHtml;
+}
+
+(function() {
+    lnb.querySelector('li:first-child').click();
+}())
