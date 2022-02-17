ef07d6188f03cc3bfaa949ed4b9e76691842b68e
Sangbeom Heo <mansaout@gmail.com>
2022-02-17 15:23:00 +0900

    Refactor: "data-catagory의 value 수정"
    정확한 의미 표현을 위해
    toon -> webtoon
    novel -> webnovel

--- src/js/components/gnb.js
+++ src/js/components/gnb.js
@@ -3,8 +3,8 @@ const createGnb = () => {
   <div class="gnb__inner">
     <ul class="gnb__list">
       <li data-category="home" class="gnb__item">홈</li>
-      <li data-category="toon" class="gnb__item">웹툰&#8729;<img src="static/icon/bmbadge_waitfree_black.svg" alt="" /></li>
-      <li data-category="novel" class="gnb__item">웹소설&#8729;<img src="static/icon/bmbadge_waitfree_black.svg" alt="" /></li>
+      <li data-category="webtoon" class="gnb__item">웹툰&#8729;<img src="static/icon/bmbadge_waitfree_black.svg" alt="" /></li>
+      <li data-category="webnovel" class="gnb__item">웹소설&#8729;<img src="static/icon/bmbadge_waitfree_black.svg" alt="" /></li>
       <li data-category="movie" class="gnb__item  gnb__item--dot">영화</li>
       <li data-category="broadcast" class="gnb__item gnb__item--dot">방송</li>
       <li data-category="book" class="gnb__item  gnb__item--dot">책</li>
