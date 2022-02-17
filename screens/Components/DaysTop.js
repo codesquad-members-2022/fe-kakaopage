26da0ea647e20440916bf9c6467caecc49cf1397
healtheloper <58503584+healtheloper@users.noreply.github.com>
2022-02-18 00:42:12 +0900

    feat: DaysTop component data render

--- screens/Components/DaysTop.js
+++ screens/Components/DaysTop.js
@@ -5,7 +5,9 @@
  */
 import DaysList from "./DaysList.js";
 
-const DaysTop = (day) => {
+const DaysTop = (inputDay) => {
+  const days = ["월", "화", "수", "목", "금", "토", "일", "완결"];
+
   return `<li class="mainBox main__contents">
     <div class="contents">
       <div class="contents__header">
@@ -19,19 +21,15 @@ const DaysTop = (day) => {
       </div>
       <div class="contents__body">
         <ul class="contents__daysNav">
-          <li class="${day === "월" ? "selected" : ""}" data-days="mon">월</li>
-          <li class="${day === "화" ? "selected" : ""}" data-days="tue">화</li>
-          <li class="${day === "수" ? "selected" : ""}" data-days="wed">수</li>
-          <li class="${day === "목" ? "selected" : ""}" data-days="thu">목</li>
-          <li class="${day === "금" ? "selected" : ""}" data-days="fri">금</li>
-          <li class="${day === "토" ? "selected" : ""}" data-days="sat">토</li>
-          <li class="${day === "일" ? "selected" : ""}" data-days="sun">일</li>
-          <li class="${
-            day === "완료" ? "selected" : ""
-          }" data-days="comp">완결</li>
+          ${days
+            .map(
+              (day) =>
+                `<li class='${inputDay === day ? "selected" : ""}'>${day}</li>`
+            )
+            .join("")}
         </ul>
         <ul class="contentsCard">
-          ${DaysList(day, 10)}
+          ${DaysList(inputDay, 10)}
         </ul>
       </div>
     </div>
