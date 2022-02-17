57f05e4725045c4199d520dbf71a418ee47eef81
DESKTOP-88VMMCI\user <luzverde0314@gmail.com>
2022-02-18 01:55:18 +0900

    fetch: main 함수 작성

--- main.js
+++ main.js
@@ -1,6 +1,4 @@
 import { createPageAction, createPageBL, createPageDaily, createPageDrama, createPageHome, createPageRofan, createPageRomance, createPageTeen, createPageWebtoon } from "./createPage.js";
-import { createFormat0, createFormat10, createFormat11, createFormat12, createFormat1A, createFormat1B, createFormat2, createFormat3, createFormat4, createFormat5, createFormat6, createFormat7, createFormat8, createFormat9 } from "./format.js";
-import { dummy } from "./sources.js";
 import { addClickEventToElement, addToContents, pickFromArr, randomView } from "./util.js";
 
 const main = () => {
