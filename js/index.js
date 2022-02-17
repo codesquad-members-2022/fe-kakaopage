73c1af99b0948709163f1dc407e967d807eb68b8
BB-choi <choibobae0809@gmail.com>
2022-02-18 05:04:30 +0900

    fix: Mixed Content 오류 해결
    - GitHub Pages로 발행 후 생기던 Mixed Content 오류 해결

--- js/index.js
+++ js/index.js
@@ -30,15 +30,15 @@ const coverImg = {
     img: {
         홈: "./images/img_cover.png",
         요일연재:
-            "http://dn-img-page.kakao.com/download/resource?kid=cl0NGc/hzacezTSJp/rNn75CBqttbKDY0Cgpierk",
-        웹툰: "http://dn-img-page.kakao.com/download/resource?kid=bk7A6r/hzhOmqqVvd/3GO9mPkES4m1GdjxnAqhW0",
-        소년: "http://dn-img-page.kakao.com/download/resource?kid=crcdq/hzb7xD95BW/CrwhRk7eAC9dtQV5Oi3Crk",
-        드라마: "http://dn-img-page.kakao.com/download/resource?kid=bTBWMG/hzp2ifulZF/YkpvjY3a5Dl89wX9RtnxDk",
-        로맨스: "http://dn-img-page.kakao.com/download/resource?kid=gVCvh/hzhOgRNPsz/a3PU9dhXVYCsbBncuWU9tK",
-        로판: "http://dn-img-page.kakao.com/download/resource?kid=lZEeK/hzp2gIFRTI/SzC5Z3nFktXKSjBj9l0ZUk",
+            "//dn-img-page.kakao.com/download/resource?kid=cl0NGc/hzacezTSJp/rNn75CBqttbKDY0Cgpierk",
+        웹툰: "//dn-img-page.kakao.com/download/resource?kid=bk7A6r/hzhOmqqVvd/3GO9mPkES4m1GdjxnAqhW0",
+        소년: "//dn-img-page.kakao.com/download/resource?kid=crcdq/hzb7xD95BW/CrwhRk7eAC9dtQV5Oi3Crk",
+        드라마: "//dn-img-page.kakao.com/download/resource?kid=bTBWMG/hzp2ifulZF/YkpvjY3a5Dl89wX9RtnxDk",
+        로맨스: "//dn-img-page.kakao.com/download/resource?kid=gVCvh/hzhOgRNPsz/a3PU9dhXVYCsbBncuWU9tK",
+        로판: "//dn-img-page.kakao.com/download/resource?kid=lZEeK/hzp2gIFRTI/SzC5Z3nFktXKSjBj9l0ZUk",
         액션무협:
-            "http://dn-img-page.kakao.com/download/resource?kid=b1G6sz/hzp2e5hBDs/rZxJLKfnt79jknCnuLJ83k",
-        BL: "http://dn-img-page.kakao.com/download/resource?kid=bDvkGC/hzacbW2cAQ/ZS1Z5mX4Mpk9yN6A82B8q0",
+            "//dn-img-page.kakao.com/download/resource?kid=b1G6sz/hzp2e5hBDs/rZxJLKfnt79jknCnuLJ83k",
+        BL: "//dn-img-page.kakao.com/download/resource?kid=bDvkGC/hzacbW2cAQ/ZS1Z5mX4Mpk9yN6A82B8q0",
     },
     title: {
         홈: "웹툰-홈",
