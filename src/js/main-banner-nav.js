0fcef8404506f25f964d932e39439f30e8169fb4
jthw1005 <skl0105@naver.com>
2022-02-18 10:56:53 +0900

    Refactor: page 첫 로드 시 홈 탭에 노란 밑줄을 default로 추가

--- src/js/main-banner-nav.js
+++ src/js/main-banner-nav.js
@@ -1,23 +1,15 @@
+function handleNavFocusIn(e) {
+  const linkTag = e.target.parentElement;
+  const listTags = linkTag.parentElement.querySelectorAll("li");
+
+  listTags.forEach((el) => {
+    el.classList.remove("underline--yellow");
+  });
+  linkTag.classList.add("underline--yellow");
+}
 
 const navLink = document.querySelectorAll(".main-banner__nav__link");
 
-function handleNavFocusIn(e) {
-    const linkTag = e.target.parentElement;
-    linkTag.classList.add("underline--yellow")
-}
-
-function handleNavFocusOut(e) {
-    const linkTag = e.target.parentElement;
-    linkTag.classList.remove("underline--yellow")
-}
-
-navLink.forEach(el => {
-    el.addEventListener("focusin", handleNavFocusIn);
-})
-
-navLink.forEach(el => {
-    el.addEventListener("focusout", handleNavFocusOut);
-})
-
-
-
+navLink.forEach((el) => {
+  el.addEventListener("focusin", handleNavFocusIn);
+});
