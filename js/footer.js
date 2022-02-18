70c2f62669a96eb8b3663960794275134a03cf70
Muffin <jinlog9@gmail.com>
2022-02-18 12:05:55 +0900

    rename: 경로 수정으로 인해 Image 파일 경로 재설정

--- js/footer.js
+++ js/footer.js
@@ -9,8 +9,8 @@ export const footer = {
             businessDetails.classList.toggle("no_display")
 
             businessDetails.classList.contains("no_display") ? 
-            businessInfoEl.style.setProperty("--background", "url(/images/icon_trace_off.png)") :
-            businessInfoEl.style.setProperty("--background", "url(/images/icon_trace_on.png)");
+            businessInfoEl.style.setProperty("--background", "url(../images/icon_trace_off.png)") :
+            businessInfoEl.style.setProperty("--background", "url(../images/icon_trace_on.png)");
         });
     }
 }
