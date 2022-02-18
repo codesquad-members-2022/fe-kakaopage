495c8eab17b6fac30503cea5d68e00c87a27a4b3
Muffin <jinlog9@gmail.com>
2022-02-18 18:16:27 +0900

    feat: 요일 컴포넌트 핸들함수 추가

--- js/handleCategoryMenu.js
+++ js/handleCategoryMenu.js
@@ -6,8 +6,10 @@ export const handleCategoryMenu = (menu) => {
     main.removeAdvertiseComponent();
     main.removeRecommendationComponent();
     main.removeIssueComponent();
+    main.removeWeeksComponent();
 
     carousel.insertAdjacentHTML("afterend", main.addAdvertiseComponent(menu) ? main.addAdvertiseComponent(menu) : "");
     carousel.insertAdjacentHTML("afterend", main.addRecommendationComponent(menu) ? main.addRecommendationComponent(menu) : "");
     carousel.insertAdjacentHTML("afterend", main.addIssueComponent(menu) ? main.addIssueComponent(menu) : "");
+    carousel.insertAdjacentHTML("afterend", main.addWeeksComponent(menu) ? main.addWeeksComponent(menu) : "");
 }
