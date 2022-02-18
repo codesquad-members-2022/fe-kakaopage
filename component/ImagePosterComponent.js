b40e17563a5d5f88916c44d0bf32bd6a9f2bbf0d
Muffin <jinlog9@gmail.com>
2022-02-18 18:06:40 +0900

    refactor: ImagePoster 컴포넌트 리팩토링
    상위 컨테이너에서 데이터 조작하도록 수정

--- component/ImagePosterComponent.js
+++ component/ImagePosterComponent.js
@@ -1,29 +1,27 @@
 import { createEl } from "../js/utils.js";
 
-export const ImagePosterComponent = (contents) => {
-    const wrapper = createEl("div");
+export const ImagePosterComponent = (content) => {
+    const wrapper = createEl("a");
     wrapper.classList.add('image-poster__wrapper');
 
-    for(let i = 0; i < contents.length; i++) {
-        const ImagePoster = createEl("div");
-        const Image = createEl("img");
-        const ImageTitle = createEl("h2");
-        const ImageSubscription = createEl("span");
+    const ImagePoster = createEl("div");
+    const Image = createEl("img");
+    const ImageTitle = createEl("h2");
+    const ImageSubscription = createEl("span");
 
-        ImagePoster.classList.add("image-poster");
-        Image.classList.add("image-poster__img");
-        ImageTitle.classList.add("image-poster__title");
-        ImageSubscription.classList.add("image-poster__subscription");
+    ImagePoster.classList.add("image-poster");
+    Image.classList.add("image-poster__img");
+    ImageTitle.classList.add("image-poster__title");
+    ImageSubscription.classList.add("image-poster__subscription");
 
-        Image.style.backgroundImage = `url(${contents[i].img})`;
-        ImageTitle.textContent = contents[i].title;
-        ImageSubscription.textContent = contents[i].subscriptions;
+    Image.style.backgroundImage = `url(${content.img})`;
+    ImageTitle.textContent = content.title;
+    ImageSubscription.textContent = content.subscriptions;
 
-        ImagePoster.appendChild(Image);
-        ImagePoster.appendChild(ImageTitle);
-        ImagePoster.appendChild(ImageSubscription);
-        wrapper.appendChild(ImagePoster);
-    }
+    ImagePoster.appendChild(Image);
+    ImagePoster.appendChild(ImageTitle);
+    ImagePoster.appendChild(ImageSubscription);
+    wrapper.appendChild(ImagePoster);
 
-    return wrapper.outerHTML;
+    return wrapper;
 }
