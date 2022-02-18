a2b8c0bcb697dc195655cb0986d324d31ad68a68
healtheloper <58503584+healtheloper@users.noreply.github.com>
2022-02-18 11:45:23 +0900

    refactor: Main banner image selector

--- screens/Webtoon/genre/DaysGenre.js
+++ screens/Webtoon/genre/DaysGenre.js
@@ -1,5 +1,13 @@
-const DaysGenre = () => {
-  return `<div>Days</div>`;
+import DaysTop from "../../Components/DaysTop.js";
+import MainBanner from "../../Components/MainBanner.js";
+
+const DaysGenre = (webtoons) => {
+  const today = new Date().getDay() - 1;
+
+  return `
+    ${MainBanner(webtoons.filter((wt) => wt.isDaysMain))}
+    ${DaysTop(today, webtoons)}
+`;
 };
 
 export default DaysGenre;
