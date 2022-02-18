b29b29d79f1c26344497341b74fd260812798545
jm <jm.dev.study@gmail.com>
2022-02-19 04:09:20 +0900

    [refactor] main.js: clickGenresList, setGenreFocus 함수 리팩토링 - clickGenresList: 기존에는 ul(에 이벤트리스너를 주었지만 li(.genres__item)에 이벤트리스너를 주는 방식으로 변경했다. - setGenreFocus: 쿼리셀렉터로 .genres__item이 아닌 .genres--focused를 찾는 방식으로 변경했다. - renderMain: WEEKEND_ITEMS 변수명을 DAILY_ITEMS로 변경했다.

--- JS/main.js
+++ JS/main.js
@@ -6,32 +6,33 @@ const $ = (select) => document.querySelector(select);
 const $all = (select) => document.querySelectorAll(select);
 
 const clickGenresList = () => {
-  const $genres = $('.genres__list');
-  $genres.addEventListener('click', (event) => {
-    if (event.target.className === 'genres__item') {
+  const genresItem = $all('.genres__item');
+  genresItem.forEach(item => {
+    item.addEventListener('click', (event) => {
       setGenreFocus(event);
-    }
-  })
-}
-
-function setGenreFocus (event) {
-  const $genresItem = $all('.genres__item');
-  $genresItem.forEach(item => {
-    if (item.classList.contains('genres--focused')) {
-      item.classList.remove('genres--focused');
-      return event.target.classList.add('genres--focused');
-    } 
+    })
   });
 }
 
-window.addEventListener('load', () => {
-  const WEEKEND_ITEMS = 10;
+function setGenreFocus (event) {
+  const focused = $('.genres--focused');
+  if (focused) {
+    focused.classList.remove('genres--focused');
+  }
+  event.target.classList.add('genres--focused');
+}
+
+function renderMain() {
+  const DAILY_ITEMS = 10;
   const TOP_ITEMS = 5;
 
   clickGenresList();
-  renderContainer('daily__top', '요일 연재 TOP', WEEKEND_ITEMS, 'cardLayout');
+  renderContainer('daily__top', '요일 연재 TOP', DAILY_ITEMS, 'cardLayout');
   renderContainer('romance__top', '로맨스 TOP', TOP_ITEMS, 'cardLayout');
   renderContainer('daily__ranking', '일간랭킹 TOP', TOP_ITEMS, 'ranking');
   renderContainer('event', '추천 이벤트', null, 'event')
   renderMoveApp('move-app');
-})
+}
+
+window.addEventListener('load', renderMain);
+
