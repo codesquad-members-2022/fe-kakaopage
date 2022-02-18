91afd480ebf2899851d3a356572d493e43ee2207
jm <jm.dev.study@gmail.com>
2022-02-18 11:49:20 +0900

    [feat] data/scrape.js : 웹페이지의 데이터를 스크랩하는 스크립트 추가 - npm의 axios, cheerio 라이브러리를 이용해서 데이터를 스크랩해서 json 형식으로 가져오며, 가져온 데이터를 fs_writeFile를 이용해서 새로운 js파일의 내용으로 추가한다. - dailyTop.js : 홈-웹툰 카테고리의 '요일 연재 top' 등의 작은 카드 레이아웃을 기준으로 스크랩되었다. - dailyRanking.js 홈-웹툰 카테고리의 '일간 랭킹 top'의 랭킹 레이아웃을 기준으로 스크랩되었다.

---
+++ data/scrape.js
@@ -0,0 +1,114 @@
+const axios = require('axios');
+const cheerio = require('cheerio');
+const fs = require('fs');
+const util = require('util');
+
+const fs_writeFile = util.promisify(fs.writeFile);
+
+const getHTML = async (url) => {
+  try {
+    return await axios.get(url)
+  } catch(err) {
+    console.log(err);
+  }
+}
+
+const parsingMainHome = async () => {
+  const url = 'https://page.kakao.com/main';
+  const html = await getHTML(url);
+
+  const $ = cheerio.load(html.data);
+  const $item = $('.css-d582dq');
+  let result = [];
+
+  $item.each((idx, node) => {
+    result.push({
+      title: $(node).find('.css-6mi0ws > span').text(),
+      desc: $(node).find('.css-1nd90yn').text(),
+      viewer: $(node).find('.css-1c1ahuy').text(),
+      author: $(node).find('.css-1pysja1').text(),
+      img: 'https:' + $(node).find('.css-1iw29bw > img').attr('data-src')
+    })
+  });
+
+  console.log(result);
+}
+
+// parsingMainHome()
+const parsingMainBanner = async () => {
+  const url = 'https://page.kakao.com/main?categoryUid=10&subCategoryUid=1002';
+  const html = await getHTML(url);
+
+  const $ = cheerio.load(html.data);
+  const $mainBanner = $('div.topBannerWrap')
+
+  let mainBanner = [];
+  $mainBanner.each((idx, node) => {
+    mainBanner.push({
+      title: $(node).find('div.css-1tqt666').text(),
+      viewer: $(node).find('div.css-13yoxln').text(),
+      img: 'https:' + $(node).find('img').attr('data-src'),
+      genre: $(node).find('div.css-10tchww').text(),
+      desc: $(node).find('div.css-11164fm > span').text(),
+    })
+  });
+
+  console.log(mainBanner);
+}
+
+// parsingMainBanner();
+
+const parsingDailyRanking = async () => {
+  const url = 'https://page.kakao.com/main?categoryUid=10&subCategoryUid=10000';
+  const html = await getHTML(url);
+
+  const $ = cheerio.load(html.data);
+  const $dailyRanking = $('.css-sknpnp');
+  let result = [];
+  $dailyRanking.each((idx, node) => {
+    result.push({
+      rank: $(node).find('.css-80le51').text(),
+      title: $(node).find('.css-6mi0ws > span').text(),
+      viewer: $(node).find('.css-1c1ahuy').text(),
+      author: $(node).find('.css-ifcwub > .css-1pysja1').text(),
+      img_url: $(node).find('.css-gvuv0v > img').attr('data-src'),
+      img_alt: $(node).find('.css-gvuv0v > img').attr('alt'),
+      genre: $(node).find('.css-ifcwub > .css-vurnku').text(),
+      day: $(node).find('.css-wdpbmy').text(),
+    })
+  });
+
+  return result;
+}
+
+parsingDailyRanking()
+.then((value) => {
+  fs_writeFile('dailyRanking.js', `export const dailyRankingData = 
+  ${JSON.stringify(value)}`)
+})
+
+const parsingHomeTop = async () => {
+  const url = 'https://page.kakao.com/main?categoryUid=10&subCategoryUid=10000';
+  const html = await getHTML(url);
+  
+  const $ = cheerio.load(html.data);
+  const $item = $('div.css-19y0ur2').children('a');
+  
+  let result = [];
+  $item.each((idx, node) => {
+    result.push({
+      title: $(node).find('div.css-6mi0ws > span').text(),
+      review: $(node).find('div.css-nfxgqr').text(),
+      viewer: $(node).find('div.css-1c1ahuy').text(),
+      img_url: $(node).find('img').attr('data-src'),
+      img_alt: $(node).find('img').attr('alt')
+    })
+  });
+  return result
+}
+
+parsingHomeTop()
+.then((value) => {
+  fs_writeFile('dailyTop.js', `export const dailyTopData = 
+  ${JSON.stringify(value)}`)
+})
