38753f015719dfdbf0d6ea3b07fe8cc04b99a31b
youryu0212 <youryu0212@naver.com>
2022-02-18 11:28:15 +0900

    update : add selectedPositionChange function

--- handler.js
+++ handler.js
@@ -1,13 +1,21 @@
+import { changeCirclePosition } from './change_circle_position.js';
 import {changeImageBoxInfo} from './change_image_box.js';
 import {changeImageCardInfo} from './change_image_card.js';
 import {weeklyDataInfo} from './data.js';
-
-const handler = (boxInfo,cardInfo) => {
-  changeImageBoxInfo(boxInfo);
-  changeImageCardInfo(cardInfo);
-}
+import { selectedPositionChange } from './selected_position_change.js';
 
 const el = document.querySelectorAll(".weekly-nav");
+
+const handler = (el,boxInfo,cardInfo,dayOfTheWeek,idx) => {
+  changeImageBoxInfo(boxInfo);
+  changeImageCardInfo(cardInfo);
+  changeCirclePosition(dayOfTheWeek);
+  selectedPositionChange(el,idx);
+}
+
 for (let i = 0 ; i<el.length ; i++){
-  el[i].addEventListener("click", ()=>{handler(weeklyDataInfo[i]["box"],weeklyDataInfo[i]["card"])},false);
+  const boxInfo = weeklyDataInfo[i]["box"]
+  const cardInfo = weeklyDataInfo[i]["card"]
+  const dayOfTheWeek = weeklyDataInfo[i]["dayOfTheWeek"]
+  el[i].addEventListener("click", ()=>{handler(el,boxInfo,cardInfo,dayOfTheWeek,i)},false);
 }
