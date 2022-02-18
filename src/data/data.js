fdd60db953fb63428aea688bb77635a27d519a36
Sangbeom Heo <mansaout@gmail.com>
2022-02-18 11:48:54 +0900

    Feat: "웹툰 탭 별로 메인배너 데이터 추가 (이미지 제외)"

--- src/data/data.js
+++ src/data/data.js
@@ -34,25 +34,158 @@ const webtoonData = {
   },
 
   // 1: 요일연재
-  1: {},
+  1: {
+    mainBanner: [
+      {
+        title: '화산전쟁',
+        caption: '나도 그들처럼 영웅이 되고 싶었다',
+        tag: 'event',
+        type: '웹툰',
+        view: '183.2',
+        path: '',
+      },
+      {
+        title: '톱스타, 그 자체',
+        caption: '다시 톱스타가 될 수 있을까?',
+        tag: 'event',
+        type: '웹툰',
+        view: '5',
+        path: '',
+      },
+    ],
+  },
 
   // 2: 소년
-  2: {},
+  2: {
+    mainBanner: [
+      {
+        title: '도굴왕',
+        caption: '한시한편 + 무료증량 + 캐시까지?!',
+        tag: 'event',
+        type: '웹툰',
+        view: '201',
+        path: '',
+      },
+      {
+        title: 'BJ대마도사',
+        caption: '왜 게임 내 정보들이 보이는 거지?',
+        tag: 'event',
+        type: '웹툰',
+        view: '26.1',
+        path: '',
+      },
+    ],
+  },
 
   // 3: 드라마
-  3: {},
+  3: {
+    mainBanner: [
+      {
+        title: '미슐랭스타',
+        caption: '최고의 셰프가 되는 건 나야!',
+        tag: 'event',
+        type: '웹툰',
+        view: '59.1',
+        path: '',
+      },
+      {
+        title: '톱스타, 그 자체',
+        caption: '다시 톱스타가 될 수 있을까?',
+        tag: 'event',
+        type: '웹툰',
+        view: '5',
+        path: '',
+      },
+    ],
+  },
 
   // 4: 로맨스
-  4: {},
+  4: {
+    mainBanner: [
+      {
+        title: '화홍',
+        caption: '엇갈린 사랑, 알 수 없는 마음',
+        tag: 'up',
+        type: '웹툰',
+        view: '113.4',
+        path: '',
+      },
+      {
+        title: '별이 빈 소원',
+        caption: '기다무 런칭기념, 캐시 이벤트!',
+        tag: 'event',
+        type: '웹툰',
+        view: '12.7',
+        path: '',
+      },
+    ],
+  },
 
   // 5: 로판
-  5: {},
+  5: {
+    mainBanner: [
+      {
+        title: '프린세스',
+        caption: '기간한정, 1시간마다 무료!',
+        tag: 'event',
+        type: '웹툰',
+        view: '30.7',
+        path: '',
+      },
+      {
+        title: '명랑자매백서',
+        caption: '기간한정, 10화까지 무료!',
+        tag: 'event',
+        type: '웹툰',
+        view: '12.8',
+        path: '',
+      },
+    ],
+  },
 
   // 6: 액션무협
-  6: {},
+  6: {
+    mainBanner: [
+      {
+        title: '무림제왕의 은퇴생활',
+        caption: '무림 맹주 같은 거, 너나 해!',
+        tag: 'event',
+        type: '웹툰',
+        view: '9.8',
+        path: '',
+      },
+      {
+        title: '생사결: 여래의 복수',
+        caption: '여래 현현! 다 무릎을 꿇어라!',
+        tag: 'up',
+        type: '웹툰',
+        view: '58.1',
+        path: '',
+      },
+    ],
+  },
 
   // 7. BL
-  7: {},
+  7: {
+    mainBanner: [
+      {
+        title: '낯선 농담',
+        caption: '또 시작해버렸다, 짝사랑',
+        tag: 'event',
+        type: '웹툰',
+        view: '12.1',
+        path: '',
+      },
+      {
+        title: '일상다정사',
+        caption: '내 형 말고, 나랑 사귀자',
+        tag: 'up',
+        type: '웹툰',
+        view: '55.5',
+        path: '',
+      },
+    ],
+  },
 };
 
 export { webtoonData };
