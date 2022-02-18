e49ed18ee3601985b3559689eb4240475dffe6af
suuzin <93182666+suuzin@users.noreply.github.com>
2022-02-18 15:38:08 +0900

    [jinnie] Step2 (#50)
    * test commit
    
    * 너비 변수선언
    
    * refactor:중복되는 색 변수선언
    
    * refactor:중복되는 배경색 변수선언
    
    * refactor:중복되는 글자색 변수선언
    
    * refactor:중복되는 마진값 변수선언
    
    * build js파일
    
    * refactor:html a링크 삭제
    
    * feat:웹툰 슬라이드 사진
    
    * feat:요일웹툰 슬라이드
    
    * refactor:html 요일별 링크삭제
    
    * style:요일별active속성추가
    
    * feat:웹툰클릭시 요일만 나오도록한다.
    
    * style:불필요한span태그삭제
    
    * style:변수명 수정

---
+++ kakao.js
@@ -0,0 +1,36 @@
+const preCategoryMenu = document.querySelector(".main_category ul li:nth-child(2)")
+const mainCategoryMenu = document.querySelector(".main_category ul li:nth-child(3)");
+const weekend = document.querySelector(".underline ul li:nth-child(2)");
+
+const webtoonTitle = document.querySelector(".webtoon_title");
+const slideShow = document.querySelector(".slideshow_slide");
+const slideshowInfo = document.querySelector(".slideshow_info");
+
+const underLine = document.querySelector(".underline");
+const allWeb = document.querySelector(".all_web")
+
+
+function webtoonContent(){
+    mainCategoryMenu.classList.add("sub_active");
+    preCategoryMenu.classList.remove("sub_active");
+
+    slideShow.style.backgroundImage ="url('img/extream.jpg')";
+    webtoonTitle.innerText="더 익스트림";
+    slideshowInfo.innerText = "외발소년의 스포츠 성장드라마";
+
+    allWeb.style.display = "none";
+}
+
+function originalContent(){
+    preCategoryMenu.classList.add("sub_active");
+    mainCategoryMenu.classList.remove("sub_active");
+
+    webtoonTitle.innerText="로드 오브 머니";
+    slideShow.style.backgroundImage ="url('img/roadofmoney.jpg')";
+    slideshowInfo.innerText = "세상에 없던 꼴통 재벌이 되다.";
+
+    allWeb.style.display = "flex";
+}
+
+preCategoryMenu.addEventListener("click",originalContent);
+mainCategoryMenu.addEventListener("click",webtoonContent);
