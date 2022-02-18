f5dee2e3e65086fade3843207add04dfb8d2b9be
suuzin <8oozin@gmail.com>
2022-02-18 11:22:34 +0900

    feat:웹툰클릭시 요일만 나오도록한다.

--- kakao.js
+++ kakao.js
@@ -1,5 +1,3 @@
-
-//웹툰을 클릭한다.
 const preCategoryMenu = document.querySelector(".main_category ul li:nth-child(2)")
 const mainCategoryMenu = document.querySelector(".main_category ul li:nth-child(3)");
 
@@ -7,14 +5,19 @@ const webtoonTitle = document.querySelector(".webtoon_title");
 const slideShow = document.querySelector(".slideshow_slide");
 const slideshowInfo = document.querySelector(".slideshow_info");
 
+const underLine = document.querySelector(".underline");
+const allWeb = document.querySelector(".all_web")
 
-function changeContent(){
+
+function webtoonContent(){
     mainCategoryMenu.classList.add("sub_active");
     preCategoryMenu.classList.remove("sub_active");
 
     slideShow.style.backgroundImage ="url('img/extream.jpg')";
     webtoonTitle.innerText="더 익스트림";
     slideshowInfo.innerText = "외발소년의 스포츠 성장드라마";
+
+    allWeb.style.display = "none";
 }
 
 function originalContent(){
@@ -24,6 +27,9 @@ function originalContent(){
     webtoonTitle.innerText="로드 오브 머니";
     slideShow.style.backgroundImage ="url('img/roadofmoney.jpg')";
     slideshowInfo.innerText = "세상에 없던 꼴통 재벌이 되다.";
+
+    allWeb.style.display = "flex";
 }
+
 preCategoryMenu.addEventListener("click",originalContent);
-mainCategoryMenu.addEventListener("click",changeContent);
+mainCategoryMenu.addEventListener("click",webtoonContent);
