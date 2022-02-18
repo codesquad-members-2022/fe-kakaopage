5206e292fc874c56fb1c9b9f5d2de1b9a608173b
BB-choi <78826879+BB-choi@users.noreply.github.com>
2022-02-18 15:20:36 +0900

    [BB] step1-2 DOM, Event, Templating 기본 (#49)
    * refactor: step1 코드 리팩토링
    
    - 자주 사용되는 font size, color css 변수 선언
    - global에 어울리지 않는 css들 main으로 이동
    
    * feat: dom조작을 위한 dom.js 추가
    
    * chore: .gitignore 추가
    
    * feat: 커버이미지란 변경(페이지전환) 구현
    
    * feat: 컨텐츠영역 페이지 전환 기본 구현
    
    * design: 웹툰-요일연재 날짜 선택 메뉴
    
    * test: insertAdjacentHTML로 요소 추가 테스트
    
    - 일단 하드코딩으로 테스트중..
    
    * feat: 컨텐츠 영역 부모요소만 추가하게 변경(초기화)
    
    - 페이지 전환시 부모 요소 추가 후 부모요소에 자식요소 추가하는 방법으로
      하기 위해 초기화
    
    * comment: TODO 주석 추가
    
    * refactor: 가운데 정렬용 공통 class 지정
    
    * feat: 웹툰-홈 컨텐츠 변경 - 로맨스TOP만 적용
    
    - 테스트 완료: 로맨스 TOP 적용
    1. 웹툰-홈 클릭시 초기화된 부모요소가 추가
    2. 부모요소 추가 확인(mutationObserver)후에 자식 노드를 추가
    
    * comment: 수정 필요한 부분 주석 추가
    
    * fix: Mixed Content 오류 해결
    
    - GitHub Pages로 발행 후 생기던 Mixed Content 오류 해결
    
    * fix: 기대신작 TOP 이벤트명 정렬 class 누락 수정
    
    * style: 불필요한 console.log() 제거
    
    - /js/mutationObserver.js의 console.log()제거
    
    * fix: 웹툰-홈 로맨스TOP 중복 추가 오류 해결
    
    - childList 관찰시 mutations가 여러번 일어나는 것으로 판단한다
    참고 :
    https://stackoverflow.com/questions/35640501/why-does-mutationobserver-fire-twice-for-childlist-but-never-for-characterdata
    
    * chore: .gitignore에 사용하지 않는 파일 추가
    
    - mutationObserver.js 추가
    
    * refactor: mutationObserver제거
    
    * feat: 요일연재 메뉴 클릭이벤트, 색상 효과

---
+++ js/index.js
@@ -0,0 +1,180 @@
+import { $ } from "./dom.js";
+import { nodes } from "./nodes.js";
+import { romanceTop } from "./genreItems.js";
+
+// FIXME:파일분리
+const webtoonMenu = {
+    홈: [
+        "category",
+        "slide-banner",
+        "weekday-serial-best",
+        "new-best",
+        "genre-best",
+        "daily-best",
+        "recommend-best",
+    ],
+    요일연재: ["weekday-serial"],
+    // FIXME:무의미한 임시 값, 수정필요
+    웹툰: 3,
+    소년: 4,
+    드라마: 5,
+    로맨스: 6,
+    로판: 7,
+    액션무협: 8,
+    BL: 9,
+};
+
+// FIXME:파일분리
+const coverImg = {
+    img: {
+        홈: "./images/img_cover.png",
+        요일연재:
+            "//dn-img-page.kakao.com/download/resource?kid=cl0NGc/hzacezTSJp/rNn75CBqttbKDY0Cgpierk",
+        웹툰: "//dn-img-page.kakao.com/download/resource?kid=bk7A6r/hzhOmqqVvd/3GO9mPkES4m1GdjxnAqhW0",
+        소년: "//dn-img-page.kakao.com/download/resource?kid=crcdq/hzb7xD95BW/CrwhRk7eAC9dtQV5Oi3Crk",
+        드라마: "//dn-img-page.kakao.com/download/resource?kid=bTBWMG/hzp2ifulZF/YkpvjY3a5Dl89wX9RtnxDk",
+        로맨스: "//dn-img-page.kakao.com/download/resource?kid=gVCvh/hzhOgRNPsz/a3PU9dhXVYCsbBncuWU9tK",
+        로판: "//dn-img-page.kakao.com/download/resource?kid=lZEeK/hzp2gIFRTI/SzC5Z3nFktXKSjBj9l0ZUk",
+        액션무협:
+            "//dn-img-page.kakao.com/download/resource?kid=b1G6sz/hzp2e5hBDs/rZxJLKfnt79jknCnuLJ83k",
+        BL: "//dn-img-page.kakao.com/download/resource?kid=bDvkGC/hzacbW2cAQ/ZS1Z5mX4Mpk9yN6A82B8q0",
+    },
+    title: {
+        홈: "웹툰-홈",
+        요일연재: "웹툰-요일연재",
+        웹툰: "웹툰-웹툰",
+        소년: "웹툰-소년",
+        드라마: "웹툰-드라마",
+        로맨스: "웹툰-로맨스",
+        로판: "웹툰-로판",
+        액션무협: "웹툰-액션무협",
+        BL: "웹툰-BL",
+    },
+};
+
+// FIXME:테스트중: 홈 클릭시(로 가정하고), 로맨스TOP 정보를 genreItems.js에서 가져와서 화면에 표시
+const createGenreItems = () => {
+    // FIXME: 홈(.genre-best 존재)에서만 적용하기 위한 라인
+    if (!$(".genre-best")) return;
+
+    const parentNode = $(".genre-best").querySelector("ol.webtoon-card-wrap");
+    let romanceTopChildren = "";
+    romanceTop.forEach((curItem) => {
+        romanceTopChildren += `<li class="webtoon-card">
+        <div class="thumbnail-wrap">
+            <img
+                src="${curItem.img}"
+                alt="${curItem.title}"
+                class="thumbnail"
+            />
+            <div class="group-flex rank-info">
+                <div>
+                    <span>★</span>
+                    <span>${curItem.score}</span>
+                </div>
+                <img
+                    src="./images/bmbadge_waitfree.svg"
+                    alt="기다리면 무료 아이콘"
+                />
+            </div>
+        </div>
+        <p class="title">${curItem.title}</p>
+        <div class="reader-info">
+            <span>UP</span>
+            <span>아이콘</span>
+            <span>인원수</span>
+        </div>
+    </li>`;
+    });
+    parentNode.insertAdjacentHTML("afterbegin", romanceTopChildren);
+};
+
+const changeCoverImg = (subMenu) => {
+    const coverImgSection = $(".cover-image");
+    const imgSrc = coverImg.img[subMenu];
+    const title = coverImg.title[subMenu];
+
+    coverImgSection.querySelector("img").src = imgSrc;
+    coverImgSection.querySelector(".title").innerText = title;
+};
+
+const toggleClass = (curEl, className) => {
+    const parentNode = curEl.parentNode;
+    Array.from(parentNode.children).forEach((el) =>
+        el.classList.remove(className)
+    );
+    curEl.classList.add(className);
+};
+
+const getElements = (targetPage) => {
+    const nodeNames = webtoonMenu[targetPage];
+    let tag = "";
+    for (let i = 0; i < nodeNames.length; i++) {
+        tag += nodes[nodeNames[i]];
+    }
+    return tag;
+};
+
+const preventDefaults = () => {
+    document.querySelectorAll("a").forEach((anchor) => {
+        anchor.addEventListener("click", (e) => e.preventDefault());
+    });
+};
+
+const clearMainContents = () => {
+    $(".contents-wrap").innerHTML = "";
+};
+
+const bindSubMenuEvent = () => {
+    // TODO: 콜백함수 따로 빼기
+    // TODO: 서브메뉴 외 다른 메뉴에도 적용되도록
+    $(".sub-menu").addEventListener("click", (e) => {
+        const curEl = e.target.parentNode;
+        const targetPage = e.target.innerText;
+
+        // 서브메뉴 active 클래스 토글
+        toggleClass(curEl, "active");
+        // 커버이미지란 변경
+        changeCoverImg(targetPage);
+        // 메인컨텐츠 비우기
+        clearMainContents();
+        // 필요한 노드들 추가하기(부모)
+        $(".contents-wrap").insertAdjacentHTML(
+            "afterbegin",
+            getElements(targetPage)
+        );
+        // TODO: 새로 추가되는 아이템이 있을때마다 bindEvent를 그 아이템들에도 해줘야함
+        bindEventListener();
+
+        //TODO: 추가한 노드들에 자식노드 추가
+        // FIXME: 로맨스TOP만 추가 테스트
+        // 테스트중: .genre-best가 만들어지면, createGenreItems()를 실행
+        // mutationObserver(.genre-best가 추가되었는지 감지하기 위함)를 사용했었으나, 사용없이도 createGenreItems()를 통해 문제없이 추가되므로 일단 제거
+        createGenreItems();
+
+        // 새로 생긴 노드에도 preventDefaults()적용
+        preventDefaults();
+    });
+};
+
+const bindWeekDayMenuEvent = () => {
+    if (!$(".week-day-menu")) return;
+
+    $(".week-day-menu").addEventListener("click", (e) => {
+        e.preventDefault();
+        const curEl = e.target.parentNode;
+        toggleClass(curEl, "active");
+    });
+};
+
+const bindEventListener = () => {
+    bindSubMenuEvent();
+    bindWeekDayMenuEvent();
+};
+
+const init = () => {
+    preventDefaults();
+    bindEventListener();
+};
+
+init();
---
+++ js/nodes.js
@@ -0,0 +1,106 @@
+export const nodes = {
+    category: `<div class="category bg-white margin-top-default padding-default">
+    <div class="category-links-wrap group-flex">
+    </div></div>`,
+    "slide-banner": `<div
+    class="slide-banner group-flex bg-white margin-top-default"
+>
+    <div class="arrow group-flex flex-center">
+        <img src="./images/paging_back.svg" alt="이전" />
+    </div>
+    
+    <div class="arrow group-flex flex-center">
+        <img src="./images/paging_next.svg" alt="다음" />
+    </div>
+</div>`,
+    "weekday-serial-best": `<div
+    class="weekday-serial-best bg-white margin-top-default padding-default"
+>
+    <div class="section-header group-flex">
+        <div class="group-flex">
+            <h3>요일 연재 TOP</h3>
+            <span class="count group-flex flex-center">(갯수)</span>
+        </div>
+        <div>
+            <span class="detail">더보기 ></span>
+        </div>
+    </div>
+
+    <ol class="week-list group-flex">
+        <li>월</li>
+        <li>화</li>
+        <li class="active">수</li>
+        <li>목</li>
+        <li>금</li>
+        <li>토</li>
+        <li>일</li>
+        <li>완결</li>
+    </ol>
+
+    <ol class="webtoon-card-wrap group-flex">
+        
+    </ol>
+</div>`,
+    "new-best": `<div
+    class="new-best bg-white margin-top-default padding-default"
+>
+    <div class="section-header group-flex">
+        <h3>기대신작 TOP</h3>
+        <span class="detail">더보기></span>
+    </div>
+
+    <div class="new-best-img-wrap group-flex">
+        
+    </div>
+</div>`,
+    "genre-best": `<div
+    class="genre-best margin-top-default bg-white padding-default"
+>
+    <div class="section-header group-flex">
+        <h3>로맨스 TOP</h3>
+        <span class="detail">더보기 ></span>
+    </div>
+
+    <ol class="webtoon-card-wrap group-flex">
+        
+    </ol>
+</div>`,
+    "daily-best": `<div
+    class="daily-best bg-white margin-top-default padding-default"
+>
+    <div class="section-header group-flex">
+        <h3>일간 랭킹 TOP</h3>
+        <span class="detail">더보기 ></span>
+    </div>
+
+    <ol>
+        
+    </ol>
+</div>`,
+    "recommend-best": `<div
+    class="recommend-event bg-white margin-top-default padding-default"
+>
+    <div class="section-header group-flex">
+        <h3>추천 이벤트</h3>
+        <span class="detail">더보기 ></span>
+    </div>
+
+    <div class="img-wrap">
+        
+    </div>
+</div>`,
+    "weekday-serial": `<div class="weekday-serial bg-white margin-top-default">
+                        <ul class="week-day-menu group-flex bg-white">
+                            <li class="active"><a href="">월</a></li>
+                            <li><a href="">화</a></li>
+                            <li><a href="">수</a></li>
+                            <li><a href="">목</a></li>
+                            <li><a href="">금</a></li>
+                            <li><a href="">토</a></li>
+                            <li><a href="">일</a></li>
+                            <li><a href="">완결</a></li>
+                        </ul>
+
+                            <ol class="webtoon-card-wrap group-flex padding-default"></ol>
+                            </div>`,
+};
