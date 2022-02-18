118ddf10b6a1812fc3b35c2cb9735f7c77a729bb
otterp012 <87624756+otterp012@users.noreply.github.com>
2022-02-18 15:28:20 +0900

    [otter] step2 (#45)
    * docs: README 수정
    
    * feat: add  article__book5
    
    * feat: 랭킹 페이지 완성
    
    * fix: 랭킹3 수정
    
    * feat: recommend-banner 추가
    
    * fix: main.html 인덴팅 수정
    
    * fix: mid-button
    
    * feat: add footer
    
    * fix: 메인 nav부분 수정 및 js nav부분 구현
    
    * fix: section 부분 css 수정
    
    * feat: weekdays.js 추가 및 동적생성 기능 추가
    
    * feat: home탭누르면 변화하는 기능 추가
    
    * docs: 이번 파일들 로컬에 따로 빼서 관리
    
    * docs: README
    
    * feat: common.js파일에 재사용 가능한 컴포넌트 정리하고, 바디부분에서 동적으로 활용될 수 있는 기능 추가
    
    * docs: 문서 수정

---
+++ common.js
@@ -0,0 +1,155 @@
+
+function renderHeader() {
+    return `<header class="header">
+    <div class="header__tittle-wrapper">
+        <h1>Kakaopage</h1>
+        <div class="header__button-wrapper">
+            <div class="header__search-wrapper">
+                <input type="text" class="header__search">
+                <i class="fas fa-search"></i>
+            </div>
+            <button class="header__button">Stage</button>
+            <button class="header__button">캐시충전</button>
+            <button class="header__button">로그인</button>
+        </div>
+    </div>
+    <nav class="header__nav">
+        <ul class="header__nav-lists">
+            <li><a href="#">홈</a></li>
+            <li><a href="#">웹툰 ・
+                <i class="fas fa-solid fa-clock"></i>
+            </a></li>
+            <li><a href="#">웹소설 ・
+                <i class="fas fa-solid fa-clock"></i>
+            </a></li>
+            <li><a href="#">영화</a></li>
+            <li><a href="#">방송</a></li>
+            <li><a href="#">책</a></li>
+        </ul>
+    </nav>
+</header>`
+}
+
+
+function renderNav() {
+    return `    <nav class="nav">
+    <ul class="nav__ul">
+        <li class="nav__list active">홈</li>
+        <li class="nav__list">요일</li>
+        <li class="nav__list">웹툰</li>
+        <li class="nav__list">소년</li>
+        <li class="nav__list">드라마</li>
+        <li class="nav__list">로맨스</li>
+        <li class="nav__list">로판</li>
+        <li class="nav__list">액션</li>
+        <li class="nav__list">BL</li>
+    </ul>
+</nav>`
+}
+// 수정할필요가 없고, 재사용할 필요가 없는 부분.
+
+
+function renderBanner() {
+    return `    <div class="aside__banner-wrapper">
+    <aside class="aside__banner">
+        <i class="fas fa-chevron-circle-left" id="left-icon"></i>
+        <i class="fas fa-chevron-circle-right" id="right-icon"></i>
+        <img src="./useimages/웹툰 _ 롱리브더킹 7타 _ 10-21.png" alt="image">
+        <div class="aside__banner-tittle-wrapper">
+            <br>
+            <h3 class="aside__banner-tittle">롱리브더킹</h3>
+        </div>
+        <div class="aside__banner-lists-wrapper">
+            <ul class="aside__banner-lists">
+                <li><i class="fas fa-solid fa-clock">웹툰</i></li>
+                <li><i class="fas fa-user-alt">30.5만명</i></li>
+            </ul>
+        </div>
+        <div class="aside__banner-footer"><p>어쩌고 저쩌고</p></div>
+    </aside>
+</div>`
+}
+
+// 배너는 추후에 수정이 필요할 수 있음..
+
+function renderMain() {
+    return `<main>
+    </main>`
+}
+
+// main에 내용 변화를 주려고 하므로 필요한 부분.
+
+function renderFooter() {
+    return `    <footer class="footer">
+    <button class="footer__toApp-button">
+        카카오페이지 앱으로 보기 >
+    </button>
+
+    <div class="footer__infor-wrapper">
+        <div class="footer__infor-text-wrapper">
+            <div class="footer__infor-text">
+                회사소개 이용약관 개인정보처리방침 청소년보호정책 공지사항
+            </div>
+            <div class="footer__infor-text">
+                (주)카카오엔터테인먼트 사업자 정보 <u>사업자 정보 확인</u>
+            </div>
+            <div class="footer__infor-text">
+                Kakao Entertainment Corp.
+            </div>
+        </div>
+        <div class="footer__infor-image-wrapper">
+            <img src="./useimages/ok mark.png" alt="image">
+        </div>
+    </div>
+</footer>`
+}
+
+function renderBase() {
+    return renderHeader() + renderNav() + renderBanner() + renderMain() + renderFooter();
+}
+
+
+function renderArticle(className) {
+    return `<article class="${className}">
+    </article>`
+}
+
+function renderArticleHeader() {
+    return `    <header class="article__header">
+    <div class="article__header-wrapper">
+        <div class="article__header-text-wrapper">
+            <h2>요일 연재 TOP</h2>
+            <span>(1,622)</span>
+        </div>
+        <button class="article__header-button">더보기 ></button>
+    </div>
+    </header>`
+}
+
+function renderArticleSections(num) {
+    const sections = renderSection(num);
+    return `<div class="section__book-wrapper">${sections}
+    </div>`
+}
+
+function renderSection(num) {
+    let sections = '';
+    for(let i = 0; i < num; i++) {
+        sections += `               <section class="section__book">
+        <div class="section__book-image-wrapper">
+            <img src="./useimages/학사재생.png" alt="images">
+            <div class="section__book-image-infor">
+                <span>1위</span>
+                <i class="fas fa-solid fa-clock"></i>
+            </div>
+        </div>
+        <div class="section__book-text-wrapper">
+            <span class="section__book-text">어쩌고 저쩌고</span>
+            <span class="section__book-text"><i class="fas fa-user-alt"></i> 28.5만명</span>
+        </div>
+    </section>`
+    }
+    return sections;
+}
+
+export { renderBase, renderArticle, renderArticleHeader, renderArticleSections };
---
+++ home.js
@@ -0,0 +1,92 @@
+// 함수 네이밍 
+//  - 특정 페이지에서 작동할 것 render + 페이지 + 정보
+//  - 공통으로 사용하는 것 render + 컴포넌트 이름 
+
+function renderHome_base() {
+    return renderHome_midButton()
+}
+
+function renderHome_midButton() {
+    return `<div class="mid-button__wrapper">
+    <button class="mid-button">오늘 UP</button>
+    <button class="mid-button">오늘 신작</button>
+    <button class="mid-button">오리지널</button>
+    <button class="mid-button">완결까지 정주행</button>
+    <button class="mid-button">독립운동가 웹툰</button>
+    <button class="mid-button">오늘 랭킹</button>
+    </div>`
+}
+
+function renderHome_ArticelNav() {
+    return `        <nav class="article__nav">
+    <ul class="article__nav-lists">
+        <li><a href="#">월</a></li>
+        <li><a href="#">화</a></li>
+        <li><a href="#">수</a></li>
+        <li><a href="#">목</a></li>
+        <li><a href="#">금</a></li>
+        <li><a href="#">토</a></li>
+        <li><a href="#">일</a></li>
+        <li><a href="#">완결</a></li>
+    </ul>
+</nav>`
+}
+
+// 공통적으로 사용되는 부분
+
+function renderArticle(className) {
+    return `<article class="${className}">
+    </article>`
+}
+
+function renderArticleHeader() {
+    return `    <header class="article__header">
+    <div class="article__header-wrapper">
+        <div class="article__header-text-wrapper">
+            <h2>요일 연재 TOP</h2>
+            <span>(1,622)</span>
+        </div>
+        <button class="article__header-button">더보기 ></button>
+    </div>
+    </header>`
+}
+
+function renderArticleSections(num) {
+    const sections = renderSection(num);
+    return `<div class="section__book-wrapper">${sections}
+    </div>`
+}
+
+function renderSection(num) {
+    let sections = '';
+    for(let i = 0; i < num; i++) {
+        sections += `               <section class="section__book">
+        <div class="section__book-image-wrapper">
+            <img src="./useimages/학사재생.png" alt="images">
+            <div class="section__book-image-infor">
+                <span>1위</span>
+                <i class="fas fa-solid fa-clock"></i>
+            </div>
+        </div>
+        <div class="section__book-text-wrapper">
+            <span class="section__book-text">어쩌고 저쩌고</span>
+            <span class="section__book-text"><i class="fas fa-user-alt"></i> 28.5만명</span>
+        </div>
+    </section>`
+    }
+    return sections;
+}
+// section 관련은 모든 페이지에서 똑같이 사용한다 -> 이함수들로 그대로 재사용할 수 있다.
+
+// function renderHome_midButton(num, list) {
+//     let buttons = ``;
+//     for(let i = 0; i < num; i++) {
+//         buttons +=  `<button class="mid-button">오늘 UP</button>`
+//     }
+//     return `<div class="mid-button__wrapper">
+//     ${buttons}
+//     </div>`
+// }
+// button도 list들어가면 이름 자동으로 될 수 있게 변경할 예정
+
+export { renderHome_base }
