7625324944cd7d8086a104561b3287bb1c45b070
Millie <85419343+jaypedia@users.noreply.github.com>
2022-02-19 21:56:33 +0900

    [Millie] Step2 (#76)
    * HTML & CSS: modify classname (feedback by crong)
    
    * Create mock webtoon data in data.json file
    
    * Add package.json file to set type as module
    
    * HTML & CSS: remove markup in HTML since data is processed in JS
    
    * Feat: load webtoon data asynchronously & tab switching
    
    * Add promotion information to data.json file
    
    * Feat: display promotion webtoons
    
    * Feat: remove little circle when clicking nav header
    
    * CSS: custom reset css and minor changes
    
    * HTML: modify class & id name
    
    * Feat: nav header - filter contents by subject
    
    * HTML & CSS: add banner images and delete local image files
    
    * HTML & CSS: refactor html tag structures and css styling
    
    * Update readme.md
    
    * HTML & CSS: refactor header and add shortcut icon
    
    * HTML & CSS & JS: refactor header nav alarm using pseudo element

---
+++ src/js/util.js
@@ -0,0 +1,10 @@
+export const DATA_URL = 'data/data.json';
+
+export const $ = selector => document.querySelector(selector);
+
+export const CL = {
+  SELECTED: 'selected',
+  WHOLE: 'whole',
+  DAY_NAME: '.day__name',
+  NAV_SUBJECT: 'nav__subject',
+};
