4ecc4217038c0f10686be8e60ac7ad2a9a27ad6f
JinJeon <nanyong01@gmail.com>
2022-02-20 01:28:53 +0900

    🔍 navigator 자식 요소 변수 제거
    app.js: navigator의 자식 요소들을 모두 선언해 함수에 적용하던 방식을 줄였다.

--- JS/app.js
+++ JS/app.js
@@ -4,18 +4,6 @@ const days = document.querySelector(".days");
 const headerNav = document.querySelector(".header__nav");
 const mainNav = document.querySelector(".main__nav");
 const daysNav = document.querySelector(".days__nav");
-const headerNavChildren = headerNav.querySelectorAll("span");
-const mainNavChildren = mainNav.querySelectorAll("span");
-const daysNavChildren = daysNav.querySelectorAll("span");
-
-const markSelectedNav = (event) => {
-  event.preventDefault();
-  const { parentNode } = event.target;
-  const previousSelectedTarget = parentNode.querySelector(".selected");
-  if (previousSelectedTarget)
-    previousSelectedTarget.classList.remove("selected");
-  event.target.classList.add("selected");
-};
 
 const showSelectedNav = (event) => {
   let type = "";
@@ -35,17 +23,27 @@ const showSelectedNav = (event) => {
   });
 };
 
-const markSelectedNavEvent = (children) => {
+const markSelectedNav = (event) => {
+  event.preventDefault();
+  const { parentNode } = event.target;
+  const previousSelectedTarget = parentNode.querySelector(".selected");
+  if (previousSelectedTarget)
+    previousSelectedTarget.classList.remove("selected");
+  event.target.classList.add("selected");
+};
+
+const markSelectedNavEvent = (nav) => {
+  const children = nav.querySelectorAll("span");
   children.forEach((child) => {
     child.addEventListener("click", markSelectedNav);
   });
 };
 
-headerNavChildren.forEach((child) => {
+headerNav.querySelectorAll("span").forEach((child) => {
   child.addEventListener("click", showSelectedNav);
 });
 
-markSelectedNavEvent(headerNavChildren);
-markSelectedNavEvent(mainNavChildren);
-markSelectedNavEvent(daysNavChildren);
+markSelectedNavEvent(headerNav);
+markSelectedNavEvent(mainNav);
+markSelectedNavEvent(daysNav);
 showSelectedNav("홈");
