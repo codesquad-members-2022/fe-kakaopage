90e9e4d773963ebed5e0fa03d5d0d9e489fac892
bukim0329 <bukim0329@gmail.com>
2022-02-21 00:13:19 +0900

    fix: Fix href=# 기능 없애기

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
 import { todayTapSet, tabMoving } from "./tabSetting.mjs";
 
+document.addEventListener("click", (e) => {
+  e.preventDefault();
+});
+
 todayTapSet(".dailyTop__tab");
 tabMoving(".dailyTop__tab");
