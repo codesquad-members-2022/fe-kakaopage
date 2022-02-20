3cc2933c0caa57fea7e8c74fd560ce1e9e77fa55
bukim0329 <bukim0329@gmail.com>
2022-02-21 01:41:34 +0900

    feat: Add 요일 연재 TOP 탭 정보 변화

--- tabSetting.mjs
+++ tabSetting.mjs
@@ -1,3 +1,7 @@
+import { weekMap } from "./variable.mjs";
+import { info } from "./info.js";
+import { changeContent } from "./changeContent.mjs";
+
 const todayTapSet = (className) => {
   const today = new Date();
   const $tab = [...document.querySelectorAll(className)];
@@ -11,10 +15,13 @@ const tabMoving = (className) => {
   const $tab = [...document.querySelectorAll(className)];
   $tab.forEach((elem) => {
     elem.addEventListener("click", () => {
+      const dayInfo = info[weekMap.get(elem.firstChild.nodeValue)];
+      const items = document.querySelectorAll(".top__inner .itemBox");
       $tab.forEach((val) => {
         val.classList.remove("selected-color", "selected-box");
       });
       elem.classList.add("selected-color", "selected-box");
+      changeContent(items, dayInfo);
     });
   });
 };
