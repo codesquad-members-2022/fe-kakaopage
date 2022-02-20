e6efe19384c77e16a201989af080ed1fdd3813a3
Muffin <jinlog9@gmail.com>
2022-02-20 14:15:15 +0900

    refactor: 이미지 포스터 컴포넌트를 여러곳에서 사용하여서 파라미터에 따라서 다르게 렌더링 해주려했는데~..
    >> 다시 카카오 웹페이지를 들여다보니 더 작게 컴포넌트를 나눠보는것도 좋겠다고 생각하였고 width값만 관리하는 css도 따로 빼는게 더 효율적으로 짜야겠다고 생각이 들었다.
    
    코딩을 잠시 멈추고 다시 설계 작업을 해볼 생각!

--- component/ImagePosterComponent.js
+++ component/ImagePosterComponent.js
@@ -1,9 +1,19 @@
-export const ImagePosterComponent = (content) => {
-    return `<a class="image-poster__wrapper">
-                <div class="image-poster">
-                    <img src="${content.img}" class="image-poster__img" alt="이미지 포스터" />
+export const ImagePosterComponent = (menu = 'default', content) => {
+    return /* html */`<a class="image-poster__${menu}-wrapper">
+                <div class="image-poster__${menu}">
+                    <div class="image-poster__${menu}-item">
+                        <img src="${content.img}" class="image-poster__${menu}_img" alt="이미지 포스터" />
+                        <div class="item__inner">
+                            <span class="item__inner_rank">1위</span>
+                            <img class="item__inner_wait_img" src="images/bmbadge_waitfree.svg" alt="시계 이미지" />
+                        </div>
+                    </div>
                     <h2 class="image-poster__title">${content.title}</h2>
-                    <span class="image-poster__subscription">${content.subscriptions}</span>
+                    <div class="image-poster__detail">
+                        <img class="image-poster__detail_subscription" src="${content.status}" alt="UP 표시" />
+                        <img class="image-poster__detail_subscription" src="images/icon_read_count.png" alt="읽은 사람수 이미지" />
+                        <span class="image-poster__detail_subscription">${content.subscriptions}</span>
+                    </div>
                 </div>
             </a>`;
 }
