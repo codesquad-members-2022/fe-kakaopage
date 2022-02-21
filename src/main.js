ee4eae91f05d1095754f8eb527ed6b68eb6ec953
dlwnstjrzz <95525638+dlwnstjrzz@users.noreply.github.com>
2022-02-22 00:40:05 +0900

    FEAT: createElement를 활용해서 dom객체를 만드는 함수를 만들었는데 활용이 너무 어려워서 일단 template literal 문법으로 click했을때 웹툰 리스트를 요일별로 보여주는 함수를 작성함

--- src/main.js
+++ src/main.js
@@ -1,7 +1,40 @@
 import todayData from '../data/webtoonData.js';
+import createDom from './createDOM.js';
+function makeTodayList(day) {
+  return todayData[`${day}`].forEach((contentData, idx) => {
+    `<a href="${contentData["link"]}" class="webtoon-content">
+      <div class="webtoon-img">
+        <div class="webtoon-img-1">
+          <img src="${contentData["imgUrl"]}">
+        </div>
+        <div class="webtoon-ranking-description">
+          <div class="ranking">${contentData["rank"]}</div>
+          <div class="ranking-logo">
+            <img src="https://static-page.kakao.com/static/common/bmbadge_waitfree.svg?53cf25c84253dee8d32e66da7524dbaf" class="ranking-logo">
+          </div>
+        </div>
+      </div>
+      <div class="webtoon-list-description">
+        <span>${contentData["title"]}</span>
+      </div>
+      <div class="viewers-num">
+        <div class="viewers-logo">
+          <img src="https://static-page.kakao.com/static/common/icon_read_count.png?817b1f83aa0dd8de232a68ac82efd871" class="viewers-logo">
+        </div>
+        <div class="num">${contentData["readerNum"]}</div>
+      </div>
+    </a>`
+  });
+}
 
-// makeTodayList(day) {
-//   todayData.day
-// }
-
-console.log(todayData.mon)
+function makeTodayEvent(dayIdx) {
+  const dayArr = ["mon", "tue", "wed", "thu", "fri", "sat", "sun", "end"];
+  const day = dayArr[dayIdx];
+  const toonList = document.querySelector(`.${day}`);
+  const webtoons = document.querySelector(".webtoon-list-1");
+  toonList.addEventListener('mousedown', function (el) {
+    toonList.classList.add("makeBold", "makeBackgroundImg");
+    webtoons.innerHTML = makeTodayList(day);
+  });
+};
+console.log(todayData["mon"][0]["imgUrl"])
