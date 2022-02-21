db32f124e3151c0a66f94ba63dc9ef5e25793e7d
Muffin <jinlog9@gmail.com>
2022-02-21 23:26:10 +0900

    feat, refactor: 각 메뉴에 컴포넌트가 렌더되도록 로직 구현
    1. if(menu !== '0') return; 의 의미는 클릭한 메뉴가 홈이 아닌경우 해당 컴포넌트가 그려지지 않도록 바로 리턴!
    
    2. 현재 영역에 보여줄 데이터를 제한하기 위해 splice 함수로 원본 객체를 잘라 파라미터로 넘겼다.

--- js/ContentListInfos.js
+++ js/ContentListInfos.js
@@ -1,8 +1,7 @@
-import { $, removeElement } from "./utils.js";
 import { IssueComponent } from "../component/IssueComponent.js";
 import { AdvertiseComponent } from "../component/AdvertiseComponent.js";
 import { issueObj, rowListTitleObj } from "./constants.js";
-import { recommendationObj, dayofWeekObj } from "./dummy.js";
+import { WebtoonData } from "./dummy.js";
 import { RowListContainer } from "../container/RowListContainer.js";
 import { WeekListContainer } from "../container/WeekListContainer.js";
 
@@ -10,32 +9,33 @@ export const ContentListInfos = {
     addAdvertiseComponent() {
         return AdvertiseComponent();
     },
-    removeAdvertiseComponent() {
-        const advertiseElement = $(".advertise");
-        removeElement(advertiseElement);
-    },
     addIssueComponent(menu) {
         if(Array.isArray(issueObj[menu]) && issueObj[menu].length === 0) return;
         return IssueComponent(issueObj[menu]);
     },
-    removeIssueComponent() {
-        const issueElement = $(".issue");
-        removeElement(issueElement);
-    },
     addRecommendationComponent(menu) {
-        if(Array.isArray(recommendationObj[menu]) && recommendationObj[menu].length === 0) return;
-        return RowListContainer(rowListTitleObj.recommendation, recommendationObj[menu]);
-    },
-    removeRecommendationComponent() {
-        const recommendElement = $(".recommend");
-        removeElement(recommendElement);
+        if(menu === '0' || menu === '1' || menu === '2') return;
+        const recommendData = WebtoonData.filter(data => data.genre === menu);
+        return RowListContainer(rowListTitleObj.recommendation, recommendData.splice(0, 4));
     },
     addWeeksComponent(menu) {
-        if(+menu !== 2) return;
-        return WeekListContainer(rowListTitleObj.weeks, dayofWeekObj);
+        if(menu !== '1') return;
+        const weeksData = WebtoonData.filter(data => data.week.includes(2));
+        return WeekListContainer(rowListTitleObj.weeks, weeksData);
     },
-    removeWeeksComponent() {
-        const weeksElement = $(".weeks");
-        removeElement(weeksElement);
+    addRomanceComponent(menu) {
+        if(menu !== '0') return;
+        const romanceData = WebtoonData.filter(data => data.genre === '4');
+        return RowListContainer(rowListTitleObj.romanceTop, romanceData.splice(0, 5));
+    },
+    addNobilityComponent(menu) {
+        if(menu !== '0') return;
+        const nobilityData = WebtoonData.filter(data => data.genre === '6');
+        return RowListContainer(rowListTitleObj.nobilityTop, nobilityData.splice(0, 5));
+    },
+    addDramaComponent(menu) {
+        if(menu !== '0') return;
+        const dramaData = WebtoonData.filter(data => data.genre === '4');
+        return RowListContainer(rowListTitleObj.dramaTop, dramaData.splice(0, 5));
     }
 }
