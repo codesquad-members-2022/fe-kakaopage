78c64deec213c9af8ac3b56277e3c5a18baba6d2
mina-gwak <mina.gwak@kakao.com>
2022-02-22 01:33:47 +0900

    feat: Add categories data

--- src/data/index.js
+++ src/data/index.js
@@ -1,5 +1,6 @@
 import { MainNavItems, ContentsNavItems } from './navItems.js';
 import Webtoons from './webtoons.js';
 import MainBanners from './mainBanners.js';
+import Categories from './categories.js';
 
-export { MainNavItems, ContentsNavItems, Webtoons, MainBanners };
+export { MainNavItems, ContentsNavItems, Webtoons, MainBanners, Categories };
