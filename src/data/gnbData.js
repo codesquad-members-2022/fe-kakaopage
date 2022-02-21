31492f08a4e58ca5dcf93f6cddd10aeaea6ae669
happyGyu <happyboyto.dev@gmail.com>
2022-02-21 22:43:04 +0900

    feat: gnb components로 분리, 데이터를 HTML에서 분리

---
+++ src/data/gnbData.js
@@ -0,0 +1,35 @@
+const gnbData = [
+    {
+        link: '#', 
+        src:'https://static-page.kakao.com/static/pc/menu_home.svg?156813c20605e12311c90bae39f38c31',
+        alt:'홈',
+    },
+    {
+        link: '#', 
+        src:'https://static-page.kakao.com/static/pc/menu_toon.svg?fd6837bff2e823e13c693320961cc5a8',
+        alt:'웹툰',
+    },
+    {
+        link: '#', 
+        src:'https://static-page.kakao.com/static/pc/menu_vod.svg?549a6831d63d4b27a463c6cc2be7044f',
+        alt:'웹소설',
+    },
+    {
+        link: '#', 
+        src:'https://static-page.kakao.com/static/pc/menu_vod.svg?549a6831d63d4b27a463c6cc2be7044f',
+        alt:'영화',
+    },
+    {
+        link: '#', 
+        src:'https://static-page.kakao.com/static/pc/menu_broadcast.svg?549cf0553505cda3d418aa684f358c18',
+        alt:'방송',
+    },
+    {
+        link: '#', 
+        src:'https://static-page.kakao.com/static/pc/menu_book.svg?eb9b97c2528955f1e6cf788c6fe7e504',
+        alt:'책',
+    }
+]
+
+
+export {gnbData};
