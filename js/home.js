46d81827a60aa4b68c2310a92543665ee238db70
seungchanHan <seungchanhan00@gmail.com>
2022-02-21 20:55:37 +0900

    [MOD] 비슷한 클래스 하나로 합치기

--- js/home.js
+++ js/home.js
@@ -2,13 +2,11 @@ import { homeData } from './data.js';
 import { makeImageSection } from './contents.js';
 import { makeMenu } from './contents.js';
 import { makeAdvert } from './contents.js';
-import { makeDate } from './contents.js';
 import { makeTitle } from './contents.js';
+import { makeDate } from './contents.js';
 import { addTag } from './contents.js';
-import { makeTodayRanking } from './contents.js';
-import { makeTodayWebtoon } from './contents.js';
 import { makeNewThings } from './contents.js';
-import { genreTop } from './contents.js';
+import { makeFiveWebtoon } from './contents.js';
 import { makeRanking } from './contents.js';
 import { makeEvent } from './contents.js';
 
@@ -17,57 +15,56 @@ export default function addHTML() {
   ${makeImageSection(homeData[0])}
   ${makeMenu(homeData[1])}
   ${makeAdvert(homeData[2])}
-  <section class="today_top">
-  ${makeDate(homeData[3])}
-  ${makeTitle(homeData[4])}
+  <section class="section dp-flex mg-t-20">
+  ${makeTitle(homeData[3])}
+  ${makeDate(homeData[4])}
   ${addTag(homeData[5])}
-  ${makeTodayRanking(homeData[6])}
-  ${makeTodayRanking(homeData[7])}
-  ${makeTodayRanking(homeData[8])}
-  ${makeTodayRanking(homeData[9])}
-  ${makeTodayRanking(homeData[10])}
-  ${makeTodayWebtoon(homeData[11])}
-  ${makeTodayWebtoon(homeData[12])} 
-  ${makeTodayWebtoon(homeData[13])} 
-  ${makeTodayWebtoon(homeData[14])}
-  ${makeTodayWebtoon(homeData[15])}
+  ${makeFiveWebtoon(homeData[6])}
+  ${makeFiveWebtoon(homeData[7])}
+  ${makeFiveWebtoon(homeData[8])}
+  ${makeFiveWebtoon(homeData[9])}
+  ${makeFiveWebtoon(homeData[10])}
+  ${makeFiveWebtoon(homeData[11])}
+  ${makeFiveWebtoon(homeData[12])} 
+  ${makeFiveWebtoon(homeData[13])} 
+  ${makeFiveWebtoon(homeData[14])}
+  ${makeFiveWebtoon(homeData[15])}
   </section>
 
-  <section class="new_things">
+  <section class="section dp-flex mg-t-20">
   ${makeTitle(homeData[16])}
   ${makeNewThings(homeData[17])}
   ${makeNewThings(homeData[18])}
   </section>
 
-  <section class="drama_section"> 
+  <section class="section dp-flex mg-t-20"> 
   ${makeTitle(homeData[19])}
-  ${genreTop(homeData[20])}  
-  ${genreTop(homeData[21])}  
-  ${genreTop(homeData[22])}  
-  ${genreTop(homeData[23])}  
-  ${genreTop(homeData[24])}  
+  ${makeFiveWebtoon(homeData[20])}  
+  ${makeFiveWebtoon(homeData[21])}  
+  ${makeFiveWebtoon(homeData[22])}  
+  ${makeFiveWebtoon(homeData[23])}  
+  ${makeFiveWebtoon(homeData[24])}  
   </section>
 
-  <section class="boy_section"> 
+  <section class="section dp-flex mg-t-20"> 
   ${makeTitle(homeData[25])}
-  ${genreTop(homeData[26])}  
-  ${genreTop(homeData[27])}  
-  ${genreTop(homeData[28])}  
-  ${genreTop(homeData[29])}  
-  ${genreTop(homeData[30])}
+  ${makeFiveWebtoon(homeData[26])}  
+  ${makeFiveWebtoon(homeData[27])}  
+  ${makeFiveWebtoon(homeData[28])}  
+  ${makeFiveWebtoon(homeData[29])}  
+  ${makeFiveWebtoon(homeData[30])}
   </section>
 
-  <section class="ranking_section">
+  <section class="section dp-flex mg-t-20">
   ${makeTitle(homeData[31])}
   ${makeRanking(homeData[32])}
   ${makeRanking(homeData[33])}
   ${makeRanking(homeData[34])}
   </section>
 
-  <section class="event">
+  <section class="section dp-flex mg-t-20">
   ${makeTitle(homeData[35])}
   ${makeEvent(homeData[36])}
-
   </section>
   `;
 }
