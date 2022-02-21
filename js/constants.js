1303c618d8c5d3059d5e8f8f99dfd046fefc9112
Muffin <jinlog9@gmail.com>
2022-02-21 23:16:00 +0900

    refactor: 카테고리 메뉴 가리키는 key 값 0 ~ 8 문자열로 수정

--- js/constants.js
+++ js/constants.js
@@ -1,10 +1,10 @@
 /**
  * Documentaion
  * key : menu ( 1 ~ 9 )
- * value : 1: 홈, 2: 요일연재, 3: 웹툰, 4:소년, 5: 드라마, 6: 로맨스, 7: 로판, 8: 액션무협, 9: BL
+ * value : 0: 홈, 1: 요일연재, 2: 웹툰, 3: 소년, 4: 드라마, 5: 로맨스, 6: 로판, 7: 액션무협, 8: BL
  */
 export const issueObj = {
-  1: [
+  0: [
     "오늘 UP",
     "오늘 신작",
     "오리지널",
@@ -12,14 +12,14 @@ export const issueObj = {
     "독립운동가 웹툰",
     "오늘 랭킹",
   ],
+  1: [],
   2: [],
-  3: [],
-  4: ["독점 신작", "이벤트로 달리자", "ㄸㅂㅇㄹ ㄹㅂㅇ"],
-  5: ["월간 베스트#20", "#이벤트로달리자", "완결까지#정주행"],
-  6: ["완결까지 정주행", "인기완결작", "기다무플러스"],
-  7: ["월간 베스트", "스테디셀러정주행", "기다무플러스"],
-  8: ["독점 신작", "이벤트로 달리자", "기다무 플러스"],
-  9: [
+  3: ["독점 신작", "이벤트로 달리자", "ㄸㅂㅇㄹ ㄹㅂㅇ"],
+  4: ["월간 베스트#20", "#이벤트로달리자", "완결까지#정주행"],
+  5: ["완결까지 정주행", "인기완결작", "기다무플러스"],
+  6: ["월간 베스트", "스테디셀러정주행", "기다무플러스"],
+  7: ["독점 신작", "이벤트로 달리자", "기다무 플러스"],
+  8: [
     "지난달의 BL",
     "인기 완결작",
     "기다무 플러스",
@@ -32,5 +32,7 @@ export const issueObj = {
 export const rowListTitleObj = {
   weeks: {name: "weeks", imageSize:"small", imageStatus: 'grade' },
   recommendation: { name: "recommend", title: "오늘의 추천", imageSize: "large", imageStatus: "clock" },
-  romanceTop: { name: "romance", title: "로맨스 TOP", imageSize: "small", imageStatus: "garde"},
+  romanceTop: { name: "romance", title: "로맨스 TOP", imageSize: "small", imageStatus: "grade"},
+  nobilityTop: { name: "nobility", title: "로판 TOP", imageSize: "small", imageStatus: "grade"},
+  dramaTop: { name: "drama", title: "드라마 TOP", imageSize: "small", imageStatus: "grade"}
 };
