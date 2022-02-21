a0a7539fa95d18aeda3c5769b0d91d4137ae2b9b
Cola <bcad1591@gmail.com>
2022-02-21 17:08:28 +0900

    Feat: 웹툰 Cover크기별 템플릿 추가

---
+++ js/webtoonComponent/cover.js
@@ -0,0 +1,120 @@
+export const wideCover = (webtoonData) => {
+  const { title, state, subscriberCount, badge, summary, tags } = webtoonData;
+  let tagElements = '';
+  tags.forEach(
+    (tag) => (tagElements += `<span class="webtoon-tag">${tag}</span>`)
+  );
+
+  return `
+  <div class="webtoon-cover wide-cover">
+    <img class="webtoon-badge" src="./data/contents/${badge}.png" alt="" />
+  </div>
+  <div class="webtoon-info">
+    <div class="webtoon-title">
+      <img
+        class="webtoon-state"
+        src="./data/contents/${state}.svg"
+        alt=""
+      />${title}
+    </div>
+    <div class="webtoon-summary small-font">${summary}</div>
+    <div class="webtoon-etc">
+      <img class="webtoon-subs" src="./data/contents/subscriber.png" alt="" />
+      <span class="webtoon-subs-count"
+        >${(subscriberCount / 10000).toFixed(0) + '만명'}</span
+      >
+      ${tagElements}
+    </div>
+  </div>
+`;
+};
+export const bigCover = (webtoonData) => {
+  const { title, badge, state, subscriberCount } = webtoonData;
+
+  return `
+  <div class="webtoon-cover">
+    <img class="webtoon-badge" src="./data/contents/${badge}.png" />
+  </div>
+  <div class="webtoon-info">
+    <div class="webtoon-title">
+      <img
+        class="webtoon-state"
+        src="./data/contents/${state}.svg"
+        alt=""
+      />${title}
+    </div>
+
+    <div class="webtoon-etc">
+      <img class="webtoon-subs" src="./data/contents/subscriber.png" />
+      <span class="webtoon-subs-count">${
+        (subscriberCount / 10000).toFixed(0) + '만명'
+      }</span>
+    </div>
+  </div>
+  `;
+};
+export const normalCover = (webtoonData) => {
+  const { title, ranking, score, state, subscriberCount } = webtoonData;
+  const under6th = ranking < 6;
+  return `
+  <div class="webtoon-cover relative">
+    <div class="webtoon-label">
+      <div class="webtoon-label-row">
+        ${
+          under6th
+            ? `<div class="webtoon-ranking">${ranking}위</div>`
+            : `<img class="webtoon-score-img" src="./svg/star.svg" alt="" />
+            <div class="webtoon-score">${score}</div>
+            `
+        }        
+      </div>
+      <div class="webtoon-label-row">
+        <img src="./data/state/waitfree.svg" />
+      </div>
+    </div>
+  </div>
+  <div class="webtoon-info">
+    <div class="webtoon-title">${title}</div>
+    <div class="webtoon-etc">
+      <img class="webtoon-state" src="./data/contents/${state}.svg" alt="" />
+      <img class="webtoon-subs" src="./data/contents/subscriber.png" alt="" />
+      <span class="webtoon-subs-count">${
+        (subscriberCount / 10000).toFixed(0) + '만명'
+      }</span>
+    </div>
+  </div>
+  `;
+};
+export const smallCover = (webtoonData) => {
+  const { ranking, state, title, subscriberCount, tags, days } = webtoonData;
+  let tagElements = '';
+  tags.forEach(
+    (tag) => (tagElements += `<span class="webtoon-tag">${tag}</span>`)
+  );
+  return ` 
+  <div class="webtoon-ranking-number">${ranking}</div>
+  <div class="webtoon-cover small-cover">
+    <img
+      class="webtoon-badge"
+      src="./data/contents/waitfree-badge.png"
+      alt=""
+    />
+  </div>
+  <div class="webtoon-info">
+    <div class="webtoon-title">
+      <img
+        class="webtoon-state"
+        src="./data/contents/${state}.svg"
+        alt=""
+      />${title}
+    </div>
+    <div class="webtoon-etc">
+      <img class="webtoon-subs" src="./data/contents/subscriber.png" alt="" />
+      <span class="webtoon-subs-count">${
+        (subscriberCount / 10000).toFixed(0) + '만명'
+      }</span>
+      ${tagElements}
+    </div>
+    <div class="webtoon-day">${days.join(', ')} 연재</div>
+  </div>`;
+};
