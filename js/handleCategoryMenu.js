4e7b14618f104da481775c7bee363146816c6cb5
Muffin <jinlog9@gmail.com>
2022-02-21 23:18:09 +0900

    refactor: 메뉴 클릭시 contents 영역 지운다음 공통으로 그려지는 부분은 빼서 그려주고 각 메뉴 마다 렌더링 되는 부분이 달라 switch-case 문으로 분리

--- js/handleCategoryMenu.js
+++ js/handleCategoryMenu.js
@@ -2,14 +2,35 @@ import { $ } from "./utils.js";
 import { ContentListInfos } from "./ContentListInfos.js";
 
 export const handleCategoryMenu = (menu) => {
-    const carousel = $(".carousel");
-    ContentListInfos.removeAdvertiseComponent();
-    ContentListInfos.removeRecommendationComponent();
-    ContentListInfos.removeIssueComponent();
-    ContentListInfos.removeWeeksComponent();
-
-    carousel.insertAdjacentHTML("afterend", ContentListInfos.addAdvertiseComponent(menu) ? ContentListInfos.addAdvertiseComponent(menu) : "");
-    carousel.insertAdjacentHTML("afterend", ContentListInfos.addRecommendationComponent(menu) ? ContentListInfos.addRecommendationComponent(menu) : "");
-    carousel.insertAdjacentHTML("afterend", ContentListInfos.addIssueComponent(menu) ? ContentListInfos.addIssueComponent(menu) : "");
-    carousel.insertAdjacentHTML("afterend", ContentListInfos.addWeeksComponent(menu) ? ContentListInfos.addWeeksComponent(menu) : "");
+    const contents = $(".main__contents");
+    contents.innerHTML = "";
+    contents.insertAdjacentHTML("beforeend", ContentListInfos.addIssueComponent(menu) ? ContentListInfos.addIssueComponent(menu) : "");
+    contents.insertAdjacentHTML("beforeend", ContentListInfos.addAdvertiseComponent(menu) ? ContentListInfos.addAdvertiseComponent(menu) : "");
+    contents.insertAdjacentHTML("beforeend", ContentListInfos.addRecommendationComponent(menu) ? ContentListInfos.addRecommendationComponent(menu) : "");
+    switch(menu) {
+        case '0':
+            contents.insertAdjacentHTML("beforeend", ContentListInfos.addRomanceComponent(menu));
+            contents.insertAdjacentHTML("beforeend", ContentListInfos.addNobilityComponent(menu));
+            contents.insertAdjacentHTML("beforeend", ContentListInfos.addDramaComponent(menu));
+            break;
+        case '1':
+            contents.insertAdjacentHTML("beforeend", ContentListInfos.addWeeksComponent(menu));
+            break;
+        case '2':
+            break;
+        case '3':
+            break;
+        case '4':
+            break;
+        case '5':
+            break;
+        case '6':
+            break;
+        case '7':
+            break;
+        case '8':
+            break;
+        default:
+            console.log(`${menu} is not valid.`);
+    }
 }
