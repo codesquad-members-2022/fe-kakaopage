dad360c04b277d59f9ef50bd675df7b1b2fe7b26
mina-gwak <mina.gwak@kakao.com>
2022-02-22 02:46:35 +0900

    feat: Add sub banner data

--- src/data/index.js
+++ src/data/index.js
@@ -2,5 +2,13 @@ import { MainNavItems, ContentsNavItems } from './navItems.js';
 import Webtoons from './webtoons.js';
 import MainBanners from './mainBanners.js';
 import Categories from './categories.js';
+import SubBanners from './subBanners.js';
 
-export { MainNavItems, ContentsNavItems, Webtoons, MainBanners, Categories };
+export {
+  MainNavItems,
+  ContentsNavItems,
+  Webtoons,
+  MainBanners,
+  Categories,
+  SubBanners,
+};
---
+++ src/data/subBanners.js
@@ -0,0 +1,14 @@
+const SubBanners = [
+  {
+    title: '롱리브더킹',
+    bannerImage: 'src/img/sub-banner.png',
+    link: '#',
+  },
+  {
+    title: '롱리브더킹',
+    bannerImage: 'src/img/sub-banner.png',
+    link: '#',
+  },
+];
+
+export default SubBanners;
