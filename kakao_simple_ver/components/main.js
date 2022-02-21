7a99a0e2819a9e4f129d11f99b6f529acdfb78d7
onlinekkabchi <onlinekkabchi@gmail.com>
2022-02-22 02:49:40 +0900

    addEventListener mdn 페이지 읽음 & html 뼈대 싹 갈아엎음

--- kakao_simple_ver/components/main.js
+++ kakao_simple_ver/components/main.js
@@ -1,13 +1,27 @@
 // Get the element with id="defaultOpen" and click on it
 // const landingPage = document.getElementById("defaultOpen").click();
-const mainMenu = document.getElementsByClassName("main-li");
-
-mainMenu.addEventListener('click', event => {
-  // GamepadButton.textContent = `Click count: ${event.detail}`;
-
-});
 
 
+console.log("hello world");
+const mainNodeList = document.querySelectorAll(".main-li");
+mainNodeList.forEach(test);
+
+
+function openPage() {
+  const mainContents = document.getElementsByClassName("main-contents");
+  const contentDiv = document.createElement('div');
+  const ttx = document.createTextNode('wow!');
+  
+  contentDiv.appendChild(ttx);
+  mainContents.appendChild(contentDiv);
+
+}
+
+function test() {
+  console.log("test sucess!");
+}
+
+// 참고 function
 // function openPage(pageName,elmnt,color) {
 //     var i, tabcontent, tablinks;
 //     tabcontent = document.getElementsByClassName("tabcontent");
