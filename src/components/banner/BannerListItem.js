2b9c0b64642d68ea3720a655e23811e21a320e92
mina-gwak <mina.gwak@kakao.com>
2022-02-22 01:04:50 +0900

    style: Create mixins and apply css to banner

--- src/components/banner/BannerListItem.js
+++ src/components/banner/BannerListItem.js
@@ -3,11 +3,11 @@ const BannerListItem = ({ webtoonInfo, bannerImage, bannerText, bannerBadge, lin
             <a href="${link}">
               <img src="${bannerImage}" alt="${webtoonInfo.title}" />
               <div class="banner-info">
-                <h2>${webtoonInfo.title}</h2>
+                <h2 class="title">${webtoonInfo.title}</h2>
                 <div>
                   <span class="badge">${bannerBadge}</span>
-                  <span class="genre pseudo-img">웹툰</span>
-                  <span class="reader pseudo-img">${webtoonInfo.readers}</span>
+                  <span class="genre">웹툰</span>
+                  <span class="reader">${webtoonInfo.readers}</span>
                 </div>
               </div>
               <p class="banner-text">${bannerText}</p>
--- src/components/section/MainBanner.js
+++ src/components/section/MainBanner.js
@@ -6,8 +6,8 @@ const MainBanner = () => {
             ${MainBannerList(MainBanners)}
             <span class="banner-count">1 / 3</span>
             <div class="banner-btn-group">
-              <button type="button" class="back pseudo-img">이전 배너</button>
-              <button type="button" class="next pseudo-img">이후 배너</button>
+              <button type="button" class="back">이전 배너</button>
+              <button type="button" class="next">이후 배너</button>
             </div>
           </section>`
 }
