b0b007e2b11d89e76fe37a3f689ab5a27fa931fa
Muffin <jinlog9@gmail.com>
2022-02-22 01:16:44 +0900

    refactor: Column, Row 컨테이너에서 ImagePosterComponent를 재사용하기 위해서... 리팩토링
    >>>> 뭔가.. ImagePosterComponent를 억지로 재사용하는 느낌이 들어서 코드가 지저분해지고 있다. 컴포넌트 분리작업은 여기까지 진행하도록 하자

--- component/ImagePosterComponent.js
+++ component/ImagePosterComponent.js
@@ -1,23 +1,27 @@
 import { ImagePosterDetailComponent } from "./ImagePosterDetailComponent.js";
 
-export const ImagePosterComponent = (obj, content) => {
+export const ImagePosterComponent = (axis = 'col', obj, content) => {
     return /* html */`
     <a class="image-poster__${obj.name}-wrapper">
-        <div class="image-poster__${obj.imageSize}">
-            <div class="image-poster__${obj.imageSize}-item">
+    ${axis === 'col' ?
+    `<div class="image-poster__${obj.imageSize} flex-${axis}">` :
+    `<div class="image-poster flex-${axis}">`}
+            <div class="image-poster__${obj.imageSize}-item flex-${axis}_mr20">
                 <div class="${obj.imageSize}-item__inner-wrapper">
-                    ${obj.imageStatus === 'clock' ? `<img class="image-poster_status" src="https://static-page.kakao.com/static/common/badge_time.png?4f8e2321988f4826b438a4ab2509e82a" />` : ''}
+                    ${obj.imageStatus === 'clock' ? `<img class="image-poster_status" alt="상태 이미지" />` : ''}
                     <img src="${content.img}" class="image-poster__${obj.imageSize}_img" alt="이미지 포스터" />
                 </div>
                 ${obj.imageStatus === 'grade' ?
                 `<div class="item__inner">
-                    <img class="item__inner_star" src="https://static-page.kakao.com/static/common/badge-thumbnail-star.svg?c4d2181b65253b0259cfa219fe4506ac" />
+                    <img class="item__inner_star" alt="별 이미지" />
                     <span class="item__inner_rank">${content.rating}</span>
                     <img class="item__inner_wait" src="images/bmbadge_waitfree.svg" alt="시계 이미지" />
                 </div>`
                 : ''}
             </div>
-            ${ImagePosterDetailComponent(content)}
+            <div class="image-poster__detail-wrapper">
+                ${ImagePosterDetailComponent(content)}
+            </div>
         </div>
     </a>`;
 }
--- container/rowListContainer.js
+++ container/rowListContainer.js
@@ -2,10 +2,11 @@ import { TopicComponent } from "../component/TopicComponent.js";
 import { ImagePosterComponent } from "../component/ImagePosterComponent.js";
 
 export const RowListContainer = (menuObj, contents) => {
-    return /* html */ `<div class="rowList ${menuObj.name}">
-                ${TopicComponent(menuObj.title)}
-                <div class="rowList__inner">
-                    ${contents.length ? contents.reduce((prev, cur) => prev += ImagePosterComponent(menuObj, cur), '') : ''}
-                </div>
-            </div>`;
+    return /* html */ `
+        <div class="rowList ${menuObj.name}">
+            ${TopicComponent(menuObj.title)}
+            <div class="rowList__inner">
+                ${contents.length ? contents.reduce((prev, cur) => prev += ImagePosterComponent('col', menuObj, cur), '') : ''}
+            </div>
+        </div>`;
 }
