b0e4d83050aa45703e45d9036fd51893c9917b65
nanjeong <whdwkxj7@naver.com>
2022-02-21 17:22:34 +0900

    feat: js로 main 그리기

---
+++ index.js
@@ -0,0 +1,528 @@
+const $ = (selector) => {
+  return document.querySelector(selector);
+};
+
+const insertIntoMain = (component) => {
+  $("main").insertAdjacentHTML("beforeend", component);
+};
+
+const createCateNav = () => {
+  const cateNav = `<nav class="center container webtoon-category">
+  <ul class="nav-item-sort text-color--light-gray">
+    <li class="text-color--black">홈</li>
+    <li>요일연재</li>
+    <li>웹툰</li>
+    <li>소년</li>
+    <li>드라마</li>
+    <li>로맨스</li>
+    <li>로판</li>
+    <li>액션무협</li>
+    <li>BL</li>
+  </ul>
+</nav>`;
+
+  insertIntoMain(cateNav);
+};
+
+const createBanner = () => {
+  const banner = `<div class="center container banner-container">
+  <img
+    class="banner-img"
+    src="https://dn-img-page.kakao.com/download/resource?kid=boPfot/hzacdN1xHr/Erv1JHofHUV7eK46ofjynk"
+    alt="도사강호"
+  />
+  <svg
+    class="arrow-icon arrow--round-border arrow-left arrow--white"
+    xmlns="http://www.w3.org/2000/svg"
+    class="h-6 w-6"
+    fill="none"
+    viewBox="0 0 24 24"
+    stroke="currentColor"
+  >
+    <path
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      stroke-width="2"
+      d="M15 19l-7-7 7-7"
+    />
+  </svg>
+  <svg
+    class="arrow-icon arrow--round-border arrow-right arrow--white"
+    xmlns="http://www.w3.org/2000/svg"
+    class="h-6 w-6"
+    fill="none"
+    viewBox="0 0 24 24"
+    stroke="currentColor"
+  >
+    <path
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      stroke-width="2"
+      d="M9 5l7 7-7 7"
+    />
+  </svg>
+  <div class="banner-detail-container text-color--white">
+    <h3 class="banner-title">도사강호</h3>
+    <div class="layout-center banner-detail-text-container">
+      <img
+        class="banner-detail-img"
+        src="https://static-page.kakao.com/static/pc/badge-bigthum-event.svg?2c00fc6eb18517e8f006adfaf464530b"
+      />
+      <span class="layout-center banner-detail-text"
+        ><img
+          class="banner-detail-icon"
+          src="https://static-page.kakao.com/static/pc/ico-bigthum-wait.svg?aeb2837e99c7d1055cbc3444433f4858"
+        />웹툰</span
+      >
+      <span>|</span>
+      <span class="layout-center banner-detail-text">
+        <img
+          class="banner-detail-icon"
+          src="https://static-page.kakao.com/static/pc/ico-bigthum-person.svg?100328455b1454b0ffff555caf02e71e"
+        />39.1만명</span
+      >
+    </div>
+  </div>
+  <div class="banner-count-container">
+    <span class="banner-count text-color--light-gray">1/3</span>
+  </div>
+  <p class="banner-bottom-text">시각이 다르면 무공도 다르다</p>
+</div>`;
+
+  insertIntoMain(banner);
+};
+
+const createCateBtnBlock = () => {
+  const cateBtnBlock = `<div class="center container category-buttons-container">
+  <div class="round-container grid-3col">
+    <button class="category-button">오늘 UP</button>
+    <button class="category-button">오늘 신작</button>
+    <button class="category-button">오리지널</button>
+    <button class="category-button">완결까지 정주행</button>
+    <button class="category-button">독립운동가 웹툰</button>
+    <button class="category-button">오늘 랭킹</button>
+  </div>
+</div>`;
+
+  insertIntoMain(cateBtnBlock);
+};
+
+const createPromotionBlock = () => {
+  const promotion = `<div class="center container promotion-container">
+  <svg
+    class="arrow-icon arrow--round-border arrow--gray"
+    xmlns="http://www.w3.org/2000/svg"
+    class="h-6 w-6"
+    fill="none"
+    viewBox="0 0 24 24"
+    stroke="currentColor"
+  >
+    <path
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      stroke-width="2"
+      d="M15 19l-7-7 7-7"
+    />
+  </svg>
+  <img
+    src="https://dn-img-page.kakao.com/download/resource?kid=gPEB1/hzhOklLHwx/b9atKBc9aVXCF3Gu9KKKHK"
+  />
+  <svg
+    class="arrow-icon arrow--round-border arrow--gray"
+    xmlns="http://www.w3.org/2000/svg"
+    class="h-6 w-6"
+    fill="none"
+    viewBox="0 0 24 24"
+    stroke="currentColor"
+  >
+    <path
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      stroke-width="2"
+      d="M9 5l7 7-7 7"
+    />
+  </svg>
+</div>`;
+
+  insertIntoMain(promotion);
+};
+
+const createDaysBlock = () => {
+  const daysBlock = `<div class="center container contents-container">
+  <header class="header-container">
+    <h2 class="mr--auto">요일 연재 TOP</h2>
+    <span class="text-color--gray">더보기</span>
+    <svg
+      class="arrow-with-text arrow--light-gray"
+      xmlns="http://www.w3.org/2000/svg"
+      class="h-6 w-6"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
+    >
+      <path
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        stroke-width="2"
+        d="M9 5l7 7-7 7"
+      />
+    </svg>
+  </header>
+  <nav class="nav-days">
+    <ul class="nav-item-sort text-color--light-gray">
+      <li>월</li>
+      <li>화</li>
+      <li>수</li>
+      <li>목</li>
+      <li class="current-tab underline-thin text-color--black">금</li>
+      <li>토</li>
+      <li>일</li>
+      <li>완결</li>
+    </ul>
+  </nav>
+  <div class="grid-5col">
+    <div class="webtoon-container">
+      <div class="webtoon-img-container round-container">
+        <img
+          class="webtoon-img"
+          src="https://dn-img-page.kakao.com/download/resource?kid=c9BidG/hzhOhJV5W3/ApkqR0bIKHmF8faTtNCm60&filename=th2"
+          alt="남편님, 다시 결혼해주세요!"
+        />
+        <div class="layout-center webtoon-img-bar">
+          <span class="mr--auto ml--small">1위</span>
+          <img
+            class="watch-img"
+            src="https://static-page.kakao.com/static/common/bmbadge_waitfree.svg?53cf25c84253dee8d32e66da7524dbaf"
+          />
+        </div>
+      </div>
+      <h3 class="webtoon-title">남편님, 다시 결혼해주세요!</h3>
+      <div class="layout-center">
+        <img
+          class="webtoon-state-img"
+          src="https://static-page.kakao.com/static/common/icon_up.svg?51cfaf512283ca0e1eaca53414e35a3f"
+          alt="업데이트"
+        />
+        <img
+          class="people-img"
+          src="https://static-page.kakao.com/static/common/icon_read_count.png?817b1f83aa0dd8de232a68ac82efd871"
+        />
+        <span class="text-color--gray webtoon-details-text">46.1만명</span>
+      </div>
+    </div>
+    <div class="webtoon-container">
+      <div class="webtoon-img-container round-container">
+        <img
+          class="webtoon-img"
+          src="https://dn-img-page.kakao.com/download/resource?kid=c9BidG/hzhOhJV5W3/ApkqR0bIKHmF8faTtNCm60&filename=th2"
+          alt="남편님, 다시 결혼해주세요!"
+        />
+        <div class="layout-center webtoon-img-bar">
+          <span class="mr--auto ml--small">2위</span>
+          <img
+            class="watch-img"
+            src="https://static-page.kakao.com/static/common/bmbadge_waitfree.svg?53cf25c84253dee8d32e66da7524dbaf"
+          />
+        </div>
+      </div>
+      <h3 class="webtoon-title">남편님, 다시 결혼해주세요!</h3>
+      <div class="layout-center">
+        <img
+          class="webtoon-state-img"
+          src="https://static-page.kakao.com/static/common/icon_up.svg?51cfaf512283ca0e1eaca53414e35a3f"
+          alt="업데이트"
+        />
+        <img
+          class="people-img"
+          src="https://static-page.kakao.com/static/common/icon_read_count.png?817b1f83aa0dd8de232a68ac82efd871"
+        />
+        <span class="text-color--gray webtoon-details-text">46.1만명</span>
+      </div>
+    </div>
+    <div class="webtoon-container">
+      <div class="webtoon-img-container round-container">
+        <img
+          class="webtoon-img"
+          src="https://dn-img-page.kakao.com/download/resource?kid=c9BidG/hzhOhJV5W3/ApkqR0bIKHmF8faTtNCm60&filename=th2"
+          alt="남편님, 다시 결혼해주세요!"
+        />
+        <div class="layout-center webtoon-img-bar">
+          <span class="mr--auto ml--small">3위</span>
+          <img
+            class="watch-img"
+            src="https://static-page.kakao.com/static/common/bmbadge_waitfree.svg?53cf25c84253dee8d32e66da7524dbaf"
+          />
+        </div>
+      </div>
+      <h3 class="webtoon-title">남편님, 다시 결혼해주세요!</h3>
+      <div class="layout-center">
+        <img
+          class="webtoon-state-img"
+          src="https://static-page.kakao.com/static/common/icon_up.svg?51cfaf512283ca0e1eaca53414e35a3f"
+          alt="업데이트"
+        />
+        <img
+          class="people-img"
+          src="https://static-page.kakao.com/static/common/icon_read_count.png?817b1f83aa0dd8de232a68ac82efd871"
+        />
+        <span class="text-color--gray webtoon-details-text">46.1만명</span>
+      </div>
+    </div>
+  </div>
+</div>`;
+  insertIntoMain(daysBlock);
+};
+
+const createSmallBannerBlock = () => {
+  const smallBannerBlock = `<div class="center container contents-container">
+  <header class="header-container">
+    <h2 class="mr--auto">기대신작 TOP</h2>
+    <span class="text-color--gray">더보기</span>
+    <svg
+      class="arrow-with-text arrow--light-gray"
+      xmlns="http://www.w3.org/2000/svg"
+      class="h-6 w-6"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
+    >
+      <path
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        stroke-width="2"
+        d="M9 5l7 7-7 7"
+      />
+    </svg>
+  </header>
+  <div class="grid-2col">
+    <div class="banner-container round-container">
+      <img
+        class="banner-img"
+        src="https://dn-img-page.kakao.com/download/resource?kid=bu1cDf/hzp2e5hDx9/kk12ZeaZln5VkyfoC4dfak"
+        alt="내가 죽였다"
+      />
+      <div class="banner-detail-container--small text-color--white">
+        <h3 class="banner-title--small">내가 죽였다</h3>
+        <div class="layout-center banner-detail-text-container">
+          <img
+            class="banner-detail-img--small"
+            src="https://static-page.kakao.com/static/pc/badge-bigthum-event.svg?2c00fc6eb18517e8f006adfaf464530b"
+          />
+          <span
+            class="layout-center banner-detail-text banner-detail-text--small"
+            ><img
+              class="banner-detail-icon banner-detail-icon--small"
+              src="https://static-page.kakao.com/static/pc/ico-bigthum-wait.svg?aeb2837e99c7d1055cbc3444433f4858"
+            />웹툰</span
+          >
+          <span class="banner-detail-text--small">|</span>
+          <span
+            class="layout-center banner-detail-text banner-detail-text--small"
+          >
+            <img
+              class="banner-detail-icon banner-detail-icon--small"
+              src="https://static-page.kakao.com/static/pc/ico-bigthum-person.svg?100328455b1454b0ffff555caf02e71e"
+            />22.1만명</span
+          >
+        </div>
+      </div>
+      <p class="banner-bottom-text banner-bottom-text--small">
+        누가 아군이고, 누가 적인가.
+      </p>
+    </div>
+  </div>
+</div>`;
+  insertIntoMain(smallBannerBlock);
+};
+
+const createGenreBlock = () => {
+  const genreBlock = `<div class="center container contents-container">
+  <header class="header-container">
+    <h2 class="mr--auto">드라마 TOP</h2>
+    <span class="text-color--gray">더보기</span>
+    <svg
+      class="arrow-with-text arrow--light-gray"
+      xmlns="http://www.w3.org/2000/svg"
+      class="h-6 w-6"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
+    >
+      <path
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        stroke-width="2"
+        d="M9 5l7 7-7 7"
+      />
+    </svg>
+  </header>
+  <div class="grid-5col">
+    <div class="webtoon-container">
+      <div class="webtoon-img-container round-container">
+        <img
+          class="webtoon-img"
+          src="https://dn-img-page.kakao.com/download/resource?kid=CLJBa/hyATqn0Q3p/xPvmPoA8nEKeYkZKdMlyy0&filename=th2"
+          alt="은동은동+옹동스"
+        />
+        <div class="layout-center webtoon-img-bar">
+          <img
+            class="star-img"
+            src="https://static-page.kakao.com/static/common/badge-thumbnail-star.svg?c4d2181b65253b0259cfa219fe4506ac"
+          />
+          <span class="mr--auto">10.0</span>
+          <img
+            class="watch-img"
+            src="https://static-page.kakao.com/static/common/bmbadge_waitfree.svg?53cf25c84253dee8d32e66da7524dbaf"
+          />
+        </div>
+      </div>
+      <h3 class="webtoon-title">은동은동+옹동스</h3>
+      <div class="layout-center">
+        <img
+          class="webtoon-state-img"
+          src="https://static-page.kakao.com/static/common/icon_up.svg?51cfaf512283ca0e1eaca53414e35a3f"
+          alt="업데이트"
+        />
+        <img
+          class="people-img"
+          src="https://static-page.kakao.com/static/common/icon_read_count.png?817b1f83aa0dd8de232a68ac82efd871"
+        />
+        <span class="text-color--gray webtoon-details-text">46.1만명</span>
+      </div>
+    </div>
+  </div>
+</div>`;
+  insertIntoMain(genreBlock);
+};
+
+const createDailyRankBlock = () => {
+  const dailyRankBlock = `<div class="center container contents-container">
+  <header class="header-container">
+    <h2 class="mr--auto">일간 랭킹 TOP</h2>
+    <span class="text-color--gray">더보기</span>
+    <svg
+      class="arrow-with-text arrow--light-gray"
+      xmlns="http://www.w3.org/2000/svg"
+      class="h-6 w-6"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
+    >
+      <path
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        stroke-width="2"
+        d="M9 5l7 7-7 7"
+      />
+    </svg>
+  </header>
+  <div>
+    <ol class="daily-rank-list">
+      <li class="layout-center">
+        <p class="daily-ranking">1</p>
+        <div class="daily-rank-img-container round-container">
+          <img
+            class="daily-rank-img"
+            src="https://dn-img-page.kakao.com/download/resource?kid=bxZgfW/hzhOjAWIkc/W6ngPs1TUSSfuOQ1MzpH70&filename=th2"
+            alt="막내 황녀님"
+          />
+          <img
+            class="clock-img--yellow"
+            src="https://static-page.kakao.com/static/common/badge_time.png?4f8e2321988f4826b438a4ab2509e82a"
+          />
+        </div>
+        <div class="daily-rank-details">
+          <div>
+            <h3 class="webtoon-title daily-rank-title">막내 황녀님</h3>
+            <span class="webtoon-details-text text-color--gray"
+              ><img
+                class="people-img"
+                src="https://static-page.kakao.com/static/common/icon_read_count.png?817b1f83aa0dd8de232a68ac82efd871"
+              />118만명 | 기다무웹툰 | 로판 | 돌대 [악어스튜디오],사하</span
+            >
+          </div>
+          <p class="text-color--gray webtoon-details-text">화 연재</p>
+        </div>
+      </li>
+    </ol>
+  </div>
+</div>`;
+  insertIntoMain(dailyRankBlock);
+};
+
+const createEventBlock = () => {
+  const eventBlock = `<div class="center container contents-container">
+  <header class="header-container">
+    <h2 class="mr--auto">추천 이벤트</h2>
+    <span class="text-color--gray">더보기</span>
+    <svg
+      class="arrow-with-text arrow--light-gray"
+      xmlns="http://www.w3.org/2000/svg"
+      class="h-6 w-6"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
+    >
+      <path
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        stroke-width="2"
+        d="M9 5l7 7-7 7"
+      />
+    </svg>
+  </header>
+  <div class="event-img-container">
+    <img
+      class="event-img round-container"
+      src="https://dn-img-page.kakao.com/download/resource?kid=FIep1/hzmU3sGhNi/ltEgI1VyEMT2RwQZYdXhqk"
+      alt="왕실교사 하이네 기다무 런칭 기념, 경품 이벤트!"
+    />
+    <div
+      class="layout-center event-count-container text-color--transparent-white"
+    >
+      <svg
+        class="arrow-icon"
+        xmlns="http://www.w3.org/2000/svg"
+        class="h-6 w-6"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+          d="M15 19l-7-7 7-7"
+        />
+      </svg>
+      <span class="event-count">1/8</span>
+      <svg
+        class="arrow-icon"
+        xmlns="http://www.w3.org/2000/svg"
+        class="h-6 w-6"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+          d="M9 5l7 7-7 7"
+        />
+      </svg>
+    </div>
+  </div>
+</div>`;
+  insertIntoMain(eventBlock);
+};
+
+createCateNav();
+createBanner();
+createCateBtnBlock();
+createPromotionBlock();
+createDaysBlock();
+createSmallBannerBlock();
+createGenreBlock();
+createDailyRankBlock();
+createEventBlock();
