7e6163bd651109a9bd41a2e37ca3e389124ff7b5
bangdler <zbthz90@gmail.com>
2022-02-21 22:26:16 +0900

    update : daily Content 요일별 목록 변경
    webtoonData 의 요일에 따라 해당 요일 click 시 목록 변경,
    rank 5 이하일 경우에만 순위로 표기

--- js/webtoon.js
+++ js/webtoon.js
@@ -1,4 +1,6 @@
 
+import { webtoonData } from "./webtoonData.js"
+
 function select(selector) {
     return document.querySelector(selector)
 }
@@ -14,15 +16,16 @@ function createDailyNav() {
 // 이벤트 버블링 사용해보기.
 function listenEvent() {
     const $daily__nav = select('.daily__nav')
-    $daily__nav.addEventListener('click', (e) => (addOn(e)))
+    $daily__nav.addEventListener('click', (e) => (addOn(e)));
+    $daily__nav.addEventListener('click', (e) => (createDailyContent(e)));
 }
 
 // 기존에 on 은 모두 삭제하고 클릭된 노드만 on
 function addOn(e) {
-    const clickedDay = e.target
+    const $daily__button = e.target
     removeOn(e)
-    if(clickedDay.className === 'daily__button') {
-        clickedDay.className += ' daily__button-on' // 한칸 띄고 넣어야함.
+    if($daily__button.className === 'daily__button') {
+        $daily__button.className += ' daily__button-on' // 한칸 띄고 넣어야함.
     }
 }
 
@@ -31,6 +34,52 @@ function removeOn(e) {
     const $$daily__button = document.querySelectorAll(`.${$daily__button.className}`)
     $$daily__button.forEach((node) => node.classList.remove('daily__button-on'))
 }
+
+function createDailyContent(e) {
+    const clickedDay = e.target.innerText;
+    if(e.target.className === 'daily__button daily__button-on') {
+        let template = getDailyTemplate(clickedDay)
+        const $dailyContents = select('.daily__contents')
+        template = template.repeat(5)
+        $dailyContents.innerHTML = template
+    }
+}
+
+function getDailyTemplate(day) {
+    let template = ''
+    webtoonData.forEach(function(data) {
+        if(day === '전체' || data.day.includes(day)) {
+            if(data.rank < 6) {
+                template +=
+                    `<div class="daily__content">
+                <div class="content__inner">
+                    <img class="daily__img" src="${data.src}" alt="${data.title}">
+                    <div class="daily__bar">${data.rank}위</div>
+                </div>
+                <div class="daily__title">${data.title}</div>
+                <div class="daily__views">${data.views}만명</div>
+            </div>`
+            }
+            else{
+                template +=
+                    `<div class="daily__content">
+                <div class="content__inner">
+                    <img class="daily__img" src="${data.src}" alt="${data.title}">
+                    <div class="daily__bar">
+                        <img src="./images/ico-thumbnail-star.svg" alt="별">
+                        ${data.rating}
+                    </div>
+                </div>
+                <div class="daily__title">${data.title}</div>
+                <div class="daily__views">${data.views}만명</div>
+            </div>`
+            }
+
+        }
+    })
+    return template;
+}
+
 
 createDailyNav()
 listenEvent()
---
+++ js/webtoonData.js
@@ -0,0 +1,15 @@
+
+export const webtoonData = [
+    {day: ['월', '금'], src:"./images/daily-img1.png", title:"악당의 아빠를 꼬셔라", rank:1, rating:4.8, views:159.3 },
+
+    {day: ['월', '화'], src:"./images/daily-img2.png", title:"나 혼자만 레벨업", rank:2, rating:4.8, views:417.4 },
+
+    {day: ['수', '목'], src:"./images/daily-img3.png", title:"이번 생은 가주가 되겠습니다", rank:3, rating:4.8, views:122.7 },
+
+    {day: ['토', '목'], src:"./images/daily-img4.png", title:"쪽쪽이를 주세요", rank:8, rating:4.8, views:50.4 },
+
+    {day: ['화', '일'], src:"./images/daily-img5.png", title:"베이비드래곤", rank:6, rating:4.7, views:33.3 },
+
+    {day: ['토', '일'], src:"./images/daily-img6.png", title:"다정한 개새끼의 목을 비틀겠습니다", rank:7, rating:4.5, views:20.4 },
+
+];
