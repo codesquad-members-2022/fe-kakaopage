6f2b5a1590d1203831ba0035915c34585179a368
Cola <bcad1591@gmail.com>
2022-02-21 17:09:15 +0900

    Feat: 웹툰 컴포넌트 생성 함수 추가
    데이터에 따라 종류에 맞는 Element를 리턴하도록 변경

--- js/webtoonComponent/webtoonComponent.js
+++ js/webtoonComponent/webtoonComponent.js
@@ -1,50 +1,29 @@
-const webtoonComponent = (webtoonData) => {
+import { wideCover, bigCover, normalCover, smallCover } from './cover.js';
+
+const getComponentType = (coverType) => {
+  if (coverType === 'big') return 'big-component';
+  if (coverType === 'small' || coverType === 'wide')
+    return 'horizontal-component';
+  return '';
+};
+
+const getHTML = (coverType, webtoonData) => {
+  if (coverType === 'big') return bigCover(webtoonData);
+  if (coverType === 'normal') return normalCover(webtoonData);
+  if (coverType === 'wide') return wideCover(webtoonData);
+  if (coverType === 'small') return smallCover(webtoonData);
+};
+
+const webtoonComponent = (coverType, webtoonData) => {
   const article = document.createElement('article');
-  const under6th = webtoonData.webtoonRanking < 6;
-  const HTML = `
-  <article class="webtoon-component">
-    <div class="webtoon-cover relative">
-      <div class="webtoon-label">
-        <div class="webtoon-label-row">
-          <div class="webtoon-ranking">${
-            under6th ? webtoonData.webtoonRanking + '위' : ''
-          }</div>
-          ${
-            under6th
-              ? ''
-              : `<img class="webtoon-score-img" src="./svg/star.svg" alt="">`
-          }
-          ${
-            under6th
-              ? ''
-              : `<div class="webtoon-score">${webtoonData.webtoonScore}</div>`
-          }
-        </div>
-        <div class="webtoon-label-row">
-          <img src="./data/state/${
-            webtoonData.waitfree ? 'waitfree' : 'free'
-          }.svg">
-        </div>
-      </div>
-    </div>
-    <div class="webtoon-info">
-      <div class="webtoon-title">${webtoonData.webtoonTitle}</div>
-      <div class="webtoon-etc">
-        <img class="webtoon-state" src="./data/contents/${
-          webtoonData.webtoonState
-        }.svg" alt="">
-        <img class="webtoon-subs" src="./data/contents/subscriber.png" alt="">
-        <span class="webtoon-subs-count">${
-          (webtoonData.subscriberCount / 10000).toFixed(0) + '만명'
-        }</span>
-      </div>
-    </div>
-  </article>
-`;
+
+  article.className = `webtoon-component ${getComponentType(coverType)}`;
+  const HTML = getHTML(coverType, webtoonData);
   article.innerHTML = HTML;
   article.querySelector(
     '.webtoon-cover'
   ).style.backgroundImage = `url(${webtoonData.coverSrc})`;
+
   return article;
 };
 
