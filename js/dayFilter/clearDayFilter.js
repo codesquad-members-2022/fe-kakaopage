989c15b993a36174bcefa9dd849326b2f921dba2
Cola <bcad1591@gmail.com>
2022-02-22 10:23:24 +0900

    FIX: 메인탭 전환시 filter엘리먼트가 계속 생성되는 문제점 수정

--- js/dayFilter/clearDayFilter.js
+++ js/dayFilter/clearDayFilter.js
@@ -1,5 +1,5 @@
 const clearDayFilter = () => {
-  const dayFilter = document.querySelector('.day-filter');
+  const dayFilter = document.querySelector('.filter');
   if (!dayFilter) return;
   dayFilter.parentElement.removeChild(dayFilter);
 };
