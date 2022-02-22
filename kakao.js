48679fa701c2bdcbac5188603edd1df6e9564b41
suuzin <8oozin@gmail.com>
2022-02-22 15:59:43 +0900

    feat:요일class추가

--- kakao.js
+++ kakao.js
@@ -4,11 +4,12 @@ function showContent(event){
     if(event.target.tagName !== "LI") return;
     else{
         console.log(event.target);
+        
+        weekCategory.querySelector(".weekend_check").classList.remove("weekend_check");
+        weekCategory.querySelector(".sub_active").classList.remove("sub_active");
         event.target.classList.add("weekend_check");
         event.target.classList.add("sub_active");  
     }
-
-
 }
 
 weekCategory.addEventListener("click",showContent);
