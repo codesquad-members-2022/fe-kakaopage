3a89606f0bc928af2e2c9124021143a6a7104d2f
happyGyu <happyboyto.dev@gmail.com>
2022-02-23 03:07:24 +0900

    refactor: 렌더링 하는 방식 수정. render함수 하나로 처리하도록

---
+++ src/data/renderData.js
@@ -0,0 +1,11 @@
+export const genreRenderList = {
+    'home' : ['mainBanner', 'categoryGrid', 'eventBanner'],
+    'daily' :  ['mainBanner'],
+    'webtoon' :  ['mainBanner'],
+    'boy' :  ['mainBanner', 'categoryGrid', 'eventBanner'],
+    'drama' :  ['mainBanner', 'categoryGrid', 'eventBanner'],
+    'romance' :  ['mainBanner', 'categoryGrid', 'eventBanner'],
+    'rofan' :  ['mainBanner', 'categoryGrid', 'eventBanner'],
+    'action' :  ['mainBanner', 'categoryGrid', 'eventBanner'],
+    'BL' :  ['mainBanner', 'categoryGrid'] 
+}
--- src/render.js
+++ src/render.js
@@ -1,21 +1,41 @@
+import {DEFAULT_PAGE, DEFAULT_GENRE} from "./constant.js";
+import { genreRenderList } from "./data/renderData.js";
 import { renderHeader } from "./components/header.js";
 import { renderGnb , setGnbEvent} from "./components/gnb.js";
-import {renderGenreTab, setGenreTabEvent} from "./components/genreTab.js";
-import {DEFAULT_PAGE} from "./constant.js";
+import { renderGenreTab, setGenreTabEvent } from "./components/genreTab.js";
+import { renderMainBanner } from "./components/mainBanner.js";
+import { renderCategoryGrid } from "./components/categoryGrid.js";
+import { renderEventBanner } from "./components/eventBanner.js";
+
 
 export const initRender = () => {
     renderHeader();
     renderGnb();
-    const currPage = document.querySelector('.gnb .tab__item--selected').dataset.tab;
     renderGenreTab(DEFAULT_PAGE);
+    renderByGenre(DEFAULT_GENRE); 
 
     setGnbEvent();
     setGenreTabEvent();
-
-    setReRenderEvt();
+    activateGnb();
+    activateGenreTab();
 }
 
-const setReRenderEvt = () => {  
+const renderByGenre = (genre) => {
+    const renderList = genreRenderList[genre];
+    renderList.forEach(component => render(component, genre));
+}
+
+const render = (component, genre) => {
+    const funcs = {
+        'mainBanner': renderMainBanner,
+        'categoryGrid': renderCategoryGrid,
+        'eventBanner': renderEventBanner,
+    }
+    funcs[component](genre);
+}
+
+//GNB는 시간상 제대로 구현하지 않음.
+const activateGnb = () => {  
     const gnb = document.querySelector('.gnb');
     gnb.addEventListener('click', (e) => {
         const selected = e.target.closest('li');
@@ -24,14 +44,11 @@ const setReRenderEvt = () => {
     })
 }
 
+const activateGenreTab = () => {
+    const genreTab = document.querySelector('.genre-tab');
+    genreTab.addEventListener('click', (e) => {
+        const selected = e.target.closest('li');
+        renderByGenre(selected.dataset.genre);
+    })
+}
 
-
-
-// renderHeader();
-// renderGnb();
-// setGnbEvent();
-// const currPage = document.querySelector('.gnb .tab__item--selected').dataset.tab;
-// console.log(currPage);
-// renderGenreTab(currPage);
-// setGenreTabEvent();
-// setEventLinstener();
