52d83b9c85f07fa1ab69fd4eba19e66a6ae7152a
Hemudi <ksum1205@naver.com>
2022-02-23 03:32:32 +0900

    Feat: top 컨텐츠 리스트 HTML 코드 생성 함수 구현

---
+++ src/data/IconSrc.js
@@ -0,0 +1,8 @@
+const IconSrc = {
+    'thumb_start' : 'resources/images/icon/badge-thumbnail-star.svg',
+    'thumb_wait' : 'resources/images/icon/bmbadge_waitfree.svg',
+    'thumb_up' : 'resources/images/icon/icon_up.svg',
+    'thumb_reader' : 'resources/images/icon/icon_read_count.png'
+};
+
+export { IconSrc };
