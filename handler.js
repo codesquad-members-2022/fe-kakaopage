48ef010334b5fc2f43f6e218ac29c53268f83ed6
youryu0212 <youryu0212@naver.com>
2022-02-22 15:20:54 +0900

    refactoring : change event-handling method to bubbling

--- handler.js
+++ handler.js
@@ -4,18 +4,25 @@ import {changeImageCardInfo} from './change_image_card.js';
 import {weeklyDataInfo} from './data.js';
 import { selectedPositionChange } from './selected_position_change.js';
 
-const el = document.querySelectorAll(".weekly-nav");
-
-const handler = (el,boxInfo,cardInfo,dayOfTheWeek,idx) => {
-  changeImageBoxInfo(boxInfo);
-  changeImageCardInfo(cardInfo);
-  changeCirclePosition(dayOfTheWeek);
-  selectedPositionChange(el,idx);
+const el = document.querySelector(".weekly-webtoon-ul");
+const weeklyIdxChanger = {
+  "월" : 0,
+  "화" : 1,
+  "수" : 2,
+  "목" : 3,
+  "금" : 4,
+  "토" : 5,
+  "일" : 6,
 }
 
-for (let i = 0 ; i<el.length ; i++){
+const handler = (el,weekly) => {
+  const i = weeklyIdxChanger[weekly.innerHTML];
   const boxInfo = weeklyDataInfo[i]["box"]
   const cardInfo = weeklyDataInfo[i]["card"]
   const dayOfTheWeek = weeklyDataInfo[i]["dayOfTheWeek"]
-  el[i].addEventListener("click", ()=>{handler(el,boxInfo,cardInfo,dayOfTheWeek,i)},false);
+  changeImageBoxInfo(boxInfo);
+  changeImageCardInfo(cardInfo);
+  changeCirclePosition(dayOfTheWeek);
+  selectedPositionChange(el.querySelectorAll("li"),i);
 }
+el.addEventListener("click", (evt) => {if (evt.target.nodeName === "LI") handler(el,evt.target)});
