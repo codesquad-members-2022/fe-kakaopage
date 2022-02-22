5454e05201f5f3bafa0971c1c31b0b55c8137e8a
moonyerim <moonyerim2@gmail.com>
2022-02-23 03:20:38 +0900

    docs: 구현했는 목록 체크

---
+++ src/js/index.js
@@ -0,0 +1,10 @@
+import { get_webtoonData } from './fetchToonData.js';
+import { jsonData } from './data.js';
+
+const $ = selector => document.querySelector(selector);
+
+// const platform = 'kakao-page';
+// const day = new Date().getDay();
+// const kakaoToday = get_webtoonData(`${platform}/week?day=${day}`);
+
+console.log(jsonData);
