b394b68e2692f0e065a2ca6fddb14dcca665d868
jm <jm.dev.study@gmail.com>
2022-02-22 11:18:26 +0900

    [feat] render.js: renderMainBanner와 관련된 함수 생성 - makeMainBannerItems()에 삽입하는 데이터는 스크랩한 mainBannerData.js를 사용한다.

--- JS/render.js
+++ JS/render.js
@@ -1,5 +1,6 @@
 import {dailyTopData} from './data/scrape/dailyTop.js'
 import {dailyRankingData} from './data/scrape/dailyRanking.js'
+import {mainBannerData} from './data/scrape/mainBannerData.js'
 import {promotionBannerData} from './data/scrape/promotionBannerData.js'
 import {$, $all} from './utility.js'
 
@@ -217,6 +218,57 @@ function makeBannerBtns() {
   return newDiv;
 }
 
+// ========== mainBanner ==========
+function renderMainBanner() {
+  const main = $('.main');
+  const newDiv = makeMainBannerLayout(focus);
+  main.appendChild(newDiv);
+}
+
+function makeMainBannerLayout() {
+  const newDiv = document.createElement('article');
+  newDiv.classList.add('main__banner');
+  newDiv.classList.add('banner');
+
+  const btns = makeBannerBtns();
+  const newList = makeMainBannerList(focus);
+  newDiv.appendChild(btns);
+  newDiv.appendChild(newList);
+
+  return newDiv
+}
+
+function makeMainBannerList() {
+  const newList = document.createElement("ul");
+  newList.classList.add('banner__list');
+  makeMainBannerItems(newList, focus);
+  
+  return newList;
+}
+
+function makeMainBannerItems(list) {
+  const data = mainBannerData;
+  data.forEach((el, index) => {
+    const item = document.createElement('li');
+    item.classList.add('banner__item');
+    item.dataset.index = index + 1;
+    item.innerHTML = `<a href="#"><img src="${data[index].img}" alt="${data[index].img_alt}" class="banner__img"/></a>
+    <div class="banner__info">
+      <h3 class="banner__title">${data[index].title}</h3>
+      <div class="banner__viewer">
+        <p><i class="fas fa-clock"></i> 웹툰</p>
+        <div class="divider"></div>
+        <p><i class="fas fa-user"></i> ${data[index].viewer}</p>
+      </div>
+      <div class="banner__summary">
+        <span>${data[index].desc}</span>
+      </div>
+    </div>`
+
+    list.appendChild(item);
+  });
+}
+
 // ========== promotionBanner ==========
 function renderPromotionBanner(focus) {
   const main = $('.main');
@@ -263,4 +315,4 @@ function makePromotionItem(list, focus) {
   });
 }
 
-export {renderContainer, renderPromotionBanner, renderMoveApp, makeWebtoonList}
+export {renderContainer, renderPromotionBanner, renderMoveApp, renderMainBanner}
