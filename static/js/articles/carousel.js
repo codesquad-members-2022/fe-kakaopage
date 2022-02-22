e16e3d39377160292a0b46d67925cc092a6e885e
yunho <kimyouknow@naver.com>
2022-02-22 21:00:30 +0900

    Refactor: carousel 동적으로 변하는 부분과 고정된 버튼 분리
    html태그 변경 및 css변경

--- static/js/articles/carousel.js
+++ static/js/articles/carousel.js
@@ -1,42 +1,28 @@
-const mock_carouselElementArr = [
-  {
-    title: '홈 서브 카테고리',
-    views: 25.6,
-    genre: '웹툰',
-    subTitle: '홈 서브 카테고리: 서브 타이틀 입니다',
-    imgUrl:
-      'https://dn-img-page.kakao.com/download/resource?kid=c0XBz/hzp2jFzCR9/JCvx5dagQ345yKhVf2m5SK',
-  },
-  {
-    title: '요일연재 서브 카테고리',
-    views: 30.6,
-    genre: '웹툰',
-    subTitle: '요일연재 서브 카테고리: 서브 타이틀 입니다',
-    imgUrl:
-      'https://dn-img-page.kakao.com/download/resource?kid=bJ0Aku/hzp2moMU6e/cEihkdBNG70ikK3gfKUK81',
-  },
-  {
-    title: '웹툰 서브 카테고리',
-    views: 10.2,
-    genre: '웹툰',
-    subTitle: '웹툰 서브 카테고리: 서브 타이틀 입니다',
-    imgUrl:
-      'https://dn-img-page.kakao.com/download/resource?kid=b1GVlh/hzp2nOLuZJ/qq3oH7NLIRt6ApNh6TadUK',
-  },
-];
-export function renderCarousel(subCategoryUid) {
-  const carouselElement = mock_carouselElementArr[subCategoryUid];
-  const mockDataLength = mock_carouselElementArr.length;
+export function renderCarousel(carouselsArr) {
+  const carouselElement = carouselsArr[0];
+  const carouselsArrLength = carouselsArr.length;
   const { title, views, genre, subTitle, imgUrl } = carouselElement;
   const newDiv = document.createElement('div');
   newDiv.classList.add('c-carousel');
   newDiv.innerHTML = `
-  <img
-  class="c-carousel__img"
-  src=${imgUrl}
-  alt="carousel image"
-/>
-<div class="l-carousel__content">
+  <div class="l-carousel__content">
+    <img  class="c-carousel__img" src=${imgUrl} alt="carousel image" />
+    <div class="c-carousel__content">
+      <h3 class="c-carousel__title">${title}</h3>
+      <div class="c-carousel__footer">
+        <div class="c-carousel__indicator indicator">
+          <span class="c-carousel__new indicator__new">UP</span>
+          <span class="c-carousel__genre"><i class="fas fa-clock"></i> ${genre}</span>
+          <div class="divider"></div>
+          <span class="c-carousel__views indicator__view"><i class="fas fa-user"></i> ${views}</span>
+        </div>
+        <div class="c-carousel__index">${1}/${carouselsArrLength}</div>
+      </div>
+      <div class="c-carousel__sub-title">
+        ${subTitle}
+      </div>
+    </div>
+  </div>
   <div class="c-carousel__buttons">
     <button type="button" class="c-carousel__button move-button">
       <i class="fas fa-angle-left"></i>
@@ -44,26 +30,6 @@ export function renderCarousel(subCategoryUid) {
     <button type="button" class="c-carousel__button move-button">
       <i class="fas fa-angle-right"></i>
     </button>
-  </div>
-  <div class="c-carousel__content">
-    <h3 class="c-carousel__title">${title}</h3>
-    <div class="c-carousel__footer">
-      <div class="c-carousel__indicator indicator">
-        <span class="c-carousel__new indicator__new">UP</span>
-        <span class="c-carousel__genre"
-          ><i class="fas fa-clock"></i> ${genre}</span
-        >
-        <div class="divider"></div>
-        <span class="c-carousel__views indicator__view"
-          ><i class="fas fa-user"></i> ${views}</span
-        >
-      </div>
-      <div class="c-carousel__index">${1}/${mockDataLength}</div>
-    </div>
-  </div>
-  </div>
-  <div class="c-carousel__sub-title">
-    ${subTitle}
   </div>
   `;
   return newDiv;
--- static/js/category/Webtoon.js
+++ static/js/category/Webtoon.js
@@ -2,6 +2,21 @@ import { renderCarousel } from '../articles/carousel.js';
 import { renderSubCategory } from '../articles/subCategory.js';
 import { renderMainChildDOM } from '../utils.js';
 
+export const Webtoon = async (subCategoryUid) => {
+  const subCategory = renderSubCategory(subCategoryIndexArr);
+  const carousel = renderCarousel(mock_carouselElementArr);
+  const eventBox = `Webtoon`;
+  const evenCarouel = `Webtoon`;
+  const mainContent = `Webtoon`;
+  return renderMainChildDOM({
+    subCategory,
+    carousel,
+    eventBox,
+    evenCarouel,
+    mainContent,
+  });
+};
+
 // mock: 임시데이터 - categoryUid에 해당하는 데이터를 서버에서 불러올 예정
 const subCategoryIndexArr = [
   '홈',
@@ -15,17 +30,29 @@ const subCategoryIndexArr = [
   'BL',
 ];
 
-export const Webtoon = async (subCategoryUid) => {
-  const subCategory = renderSubCategory(subCategoryIndexArr);
-  const carousel = renderCarousel(subCategoryUid);
-  const eventBox = `Webtoon`;
-  const evenCarouel = `Webtoon`;
-  const mainContent = `Webtoon`;
-  return renderMainChildDOM({
-    subCategory,
-    carousel,
-    eventBox,
-    evenCarouel,
-    mainContent,
-  });
-};
+const mock_carouselElementArr = [
+  {
+    title: '홈 서브 카테고리',
+    views: 25.6,
+    genre: '웹툰',
+    subTitle: '홈 서브 카테고리: 서브 타이틀 입니다',
+    imgUrl:
+      'https://dn-img-page.kakao.com/download/resource?kid=c0XBz/hzp2jFzCR9/JCvx5dagQ345yKhVf2m5SK',
+  },
+  {
+    title: '요일연재 서브 카테고리',
+    views: 30.6,
+    genre: '웹툰',
+    subTitle: '요일연재 서브 카테고리: 서브 타이틀 입니다',
+    imgUrl:
+      'https://dn-img-page.kakao.com/download/resource?kid=bJ0Aku/hzp2moMU6e/cEihkdBNG70ikK3gfKUK81',
+  },
+  {
+    title: '웹툰 서브 카테고리',
+    views: 10.2,
+    genre: '웹툰',
+    subTitle: '웹툰 서브 카테고리: 서브 타이틀 입니다',
+    imgUrl:
+      'https://dn-img-page.kakao.com/download/resource?kid=b1GVlh/hzp2nOLuZJ/qq3oH7NLIRt6ApNh6TadUK',
+  },
+];
