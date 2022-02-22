9d4250a739baefd23497df1c92be86c4a6114668
jm <jm.dev.study@gmail.com>
2022-02-23 03:59:01 +0900

    [feat] main.js: 메인배너가 자동으로 슬라이드 되도록 이벤트 추가 - makeCloneNode: 첫번째 노드와 마지막 노드를 클론해서 .banner__list 에 추가한다. - transitionend: 클래스가 cloneFirstNode인 노드가 마지막에 온다면 transition을 ‘none’으로 설정하고 counter를 1로 초기화한 뒤 transform으로 이동한다.

--- JS/main.js
+++ JS/main.js
@@ -52,6 +52,49 @@ function resetMain() {
   }
 }
 
+function makeCloneNode() {
+  const list = $('.main__banner .banner__list');
+  const items = $all('.main__banner .banner__item');
+  const firstNode = items[0].cloneNode(true);
+  const lastNode = items[items.length - 1].cloneNode(true);
+  firstNode.classList.add('cloneFirstNode');
+  lastNode.classList.add('cloneLastNode');
+
+  list.appendChild(firstNode);
+  list.insertBefore(lastNode, items[0]);
+
+  return list
+}
+
+function eventMainBanner() {
+  const list = makeCloneNode();
+  const items = $all('.main__banner .banner__item');
+  const width = $('.main__banner .banner__item').offsetWidth;
+
+  let counter = 1;
+  list.style.transform = `translate(${-width * counter}px)`;
+  slideMainBanner(list, items, width, counter);
+}
+
+const slideMainBanner = (list, items, width, counter) => {
+  setInterval(() => {
+    if (counter >= items.length - 1) return;
+    counter++;
+    list.style.transform = `translate(${-width * counter}px)`;
+    list.style.transition = 'transform 0.5s';
+
+    $('.banner__count .now').innerText = items[counter].dataset.index;
+  }, 3000);
+
+  list.addEventListener('transitionend', () => {
+    if (items[counter].classList.contains("cloneFirstNode"))  {
+      list.style.transition = "none";
+      counter = items.length - counter;
+      list.style.transform = `translate(${-width * counter}px)`;
+    }
+  })
+}
+
 function clickBannerPrevBtn() {
   const focused = $('.focused');
   const prevItem = focused.previousElementSibling;
@@ -98,9 +141,11 @@ function eventHome() {
 
 function loadHome() {
   renderHome('홈', containerInfo, 'firstRender');
-  clickGenresList();
   eventHome();
 }
 
-window.addEventListener('load', loadHome);
-
+window.addEventListener('load', () => {
+  loadHome();
+  clickGenresList();
+  eventMainBanner();
+});
