98646f579179bed62ecd2da8eb344a75981b2a42
Hemudi <ksum1205@naver.com>
2022-02-23 04:53:49 +0900

    Feat: 요일 연재 TOP 요일 Tab 클릭 이벤트 구현

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import { renderDailyTop, renderGenreTop } from "./Render.js"
+import { setWeekNavEvent } from "./NavEventController.js"
 
 function init(){
     renderDailyTop();
     renderGenreTop();
+    setWeekNavEvent();
 }
 
 init();
