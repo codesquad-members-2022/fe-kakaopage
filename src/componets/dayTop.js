bcac15c16c4150848ada0eec54b0b70f1698fe02
dony <rsefa94@gmail.com>
2022-02-23 07:34:27 +0900

    Fix: import 경로 수정, dayIdx 매개 변수 삭제,  getSectionTop 함수 파라미터 수정

--- src/componets/dayTop.js
+++ src/componets/dayTop.js
@@ -1,14 +1,14 @@
-import { getSectionTop } from '../componets/sectionTop';
+import { getSectionTop } from '../componets/sectionTop.js';
 import { getGradeListTemp } from '../componets/gradeList.js';
 
-const getDayTop = (data) => {
+const getDayTop = (data, dayIdx) => {
     const rankRowData = data['rankRow'][dayIdx];
     const rankList = getGradeListTemp(rankRowData);
     const gradeRowData = data['gradeRow'][dayIdx];
     const gradeList = getGradeListTemp(gradeRowData);
     return (
         `<section class="section">
-            ${getSectionTop(data.sectionTitle)}
+            ${getSectionTop(data)}
             <ul class="section__day-top-tabs">
                 <li class="section__day-top-tab"><button type="button" class="day-top-tab__button">월</button></li>
                 <li class="section__day-top-tab"><button type="button" class="day-top-tab__button">화</button></li>
