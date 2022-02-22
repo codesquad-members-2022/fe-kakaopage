28a5b967f007d973ad6800646eecff2440618561
mina-gwak <mina.gwak@kakao.com>
2022-02-22 12:23:08 +0900

    fix: Change style to show contents header's pseudo element

--- src/components/contentsList/ContentsHeader.js
+++ src/components/contentsList/ContentsHeader.js
@@ -1,7 +1,7 @@
 const ContentsHeader = (title) => {
   return `<div class="contents-header">
             <h3 class="contents-title">${title}</h3>
-            <a href="#" class="pseudo-img">더보기</a>
+            <a href="#">더보기</a>
           </div>`;
 };
 
