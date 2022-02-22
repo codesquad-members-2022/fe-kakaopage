629042ae006aea156422c26e70e49fe6bae73f18
Muffin <jinlog9@gmail.com>
2022-02-22 16:58:25 +0900

    refactor: 공통으로 보여주는 컴포넌트 따로분리

--- js/handleCategoryMenu.js
+++ js/handleCategoryMenu.js
@@ -1,5 +1,11 @@
-import { $ } from "./utils.js";
+import { $, today } from "./utils.js";
 import { ContentListInfos } from "./ContentListInfos.js";
+
+const commonGenreRender = (element, menu) => {
+    element.insertAdjacentHTML("beforeend", ContentListInfos.addExclusiveComponent(menu));
+    element.insertAdjacentHTML("beforeend", ContentListInfos.addTodayGiftComponent(menu));
+    element.insertAdjacentHTML("beforeend", ContentListInfos.addPopularityComponent(menu));
+}
 
 export const handleCategoryMenu = (menu) => {
     const contents = $(".main__contents");
@@ -12,23 +18,34 @@ export const handleCategoryMenu = (menu) => {
             contents.insertAdjacentHTML("beforeend", ContentListInfos.addRomanceComponent(menu));
             contents.insertAdjacentHTML("beforeend", ContentListInfos.addNobilityComponent(menu));
             contents.insertAdjacentHTML("beforeend", ContentListInfos.addDramaComponent(menu));
+            contents.insertAdjacentHTML("beforeend", ContentListInfos.addDailyRankingComponent(menu));
             break;
         case '1':
-            contents.insertAdjacentHTML("beforeend", ContentListInfos.addWeeksComponent(menu));
+            contents.insertAdjacentHTML("beforeend", ContentListInfos.addWeekComponent(menu));
+            contents.insertAdjacentHTML("beforeend", ContentListInfos.addFilterComponent(menu));
+            contents.insertAdjacentHTML("beforeend", ContentListInfos.addWeeksComponent(menu, today));
             break;
         case '2':
+            contents.insertAdjacentHTML("beforeend", ContentListInfos.addWebToonComponent(menu));
             break;
         case '3':
+            contents.insertAdjacentHTML("beforeend", ContentListInfos.addBoyComponent(menu));
+            commonGenreRender(contents, menu);
             break;
         case '4':
+            commonGenreRender(contents, menu);
             break;
         case '5':
+            commonGenreRender(contents, menu);
             break;
         case '6':
+            commonGenreRender(contents, menu);
             break;
         case '7':
+            commonGenreRender(contents, menu);
             break;
         case '8':
+            commonGenreRender(contents, menu);
             break;
         default:
             console.log(`${menu} is not valid.`);
