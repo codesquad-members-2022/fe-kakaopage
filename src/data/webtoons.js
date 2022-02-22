f1873e3877dac62a21d92d23aed3d1c855f3ea97
mina-gwak <mina.gwak@kakao.com>
2022-02-22 12:10:18 +0900

    feat: Add anticipated works data

--- src/data/webtoons.js
+++ src/data/webtoons.js
@@ -106,7 +106,23 @@ const Webtoons = [
     shortDesc: '"내가 원했던 참회란, 모든 것을 되돌려놓는 것."',
     rating: '10.0',
     image: 'https://dn-img-page.kakao.com/download/resource?kid=bhe9o7/hzmU3zpvL3/DWRcsmPxJAlrjDw7MkGWwK&filename=th2',
-  }
+  },
+  {
+    title: '화타가 된 외과 의사',
+    writer: {
+      writing: '샤이멜',
+      drawing: '최병익',
+      original: '박동신',
+    },
+    readers: '27.3만명',
+    genre: '액션',
+    waitFree: true,
+    ageLimit: 'All',
+    serialDate: ['화'],
+    shortDesc: '<삼국지연의>속 세상에 떨어진 젊은 외과의사가 펼치는 기상천외한 모험이 시작된다!',
+    rating: '10.0',
+    image: 'https://dn-img-page.kakao.com/download/resource?kid=dwMIuC/hzhOgD8UFe/J6wGhV1I0sWJ4jwJvUYun1&filename=th2',
+  },
 ];
 
 export default Webtoons;
