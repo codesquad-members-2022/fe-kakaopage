23786f339db06dc1656b32d9478604e2108aa895
suuzin <8oozin@gmail.com>
2022-02-22 16:02:58 +0900

    feat:메인카테고리 클릭시 클래스 주기

--- kakao.js
+++ kakao.js
@@ -1,4 +1,15 @@
 const weekCategory = document.querySelector(".tab .main_category ul");
+const mainCategory = document.querySelector(".main_category ul");
+
+function mainContent(event){
+    if(event.target.tagName !=="LI")return;
+    else{
+        console.log(event.target);
+
+        mainCategory.querySelector(".sub_active").classList.remove("sub_active");
+        event.target.classList.add("sub_active");
+    }
+}
 
 function showContent(event){
     if(event.target.tagName !== "LI") return;
@@ -11,5 +22,5 @@ function showContent(event){
         event.target.classList.add("sub_active");  
     }
 }
-
+mainCategory.addEventListener("click",mainContent);
 weekCategory.addEventListener("click",showContent);
