1f79efa4005527885d79c99c69a1fa8e4be9ef9b
suuzin <8oozin@gmail.com>
2022-02-23 00:44:16 +0900

    feat:filter로 요일 구분

--- data.js
+++ data.js
@@ -11,35 +11,35 @@ const webtoonData = [
             rank:"2위",
             title:"도굴왕",
             view:"201.9만명",
-            day:"월"
+            day:"화"
         },
         {
             image:"https://dn-img-page.kakao.com/download/resource?kid=cppzaC/hzp2ifvC1J/IAy6e8p17VpMjWaIfr3bkK&filename=th2",
             rank:"3위",
             title:"막내 황녀님",
             view:"118.2만명",
-            day:"월"
+            day:"수"
         },
         {
             image:"https://dn-img-page.kakao.com/download/resource?kid=hczXh/hzmU2fMnub/cOpubbT9K34zgxegMOSmhk&filename=th2",
             rank:"9.9",
             title:"로열 셰프 영애님",
             view:"90.4만명",
-            day:"화"
+            day:"목"
         },
         {
             image:"https://dn-img-page.kakao.com/download/resource?kid=cEXgMa/hyATr1CZmD/YoDlK2IoUrelLh02Ria3f1&filename=th2",
             rank:"9.9",
             title:"던전 리셋",
             view:"71.5만명",
-            day:"화"
+            day:"금"
         },
         {
             image:"https://dn-img-page.kakao.com/download/resource?kid=4pT3F/hyLi76skGR/u3m5gW9VYP1mP6xNTTAafk&filename=th2",
             rank:"9.9",
             title:"로드오브머니",
             view:"49.5만명",
-            day:"화"
+            day:"토"
         }
 ];
 
--- kakao.js
+++ kakao.js
@@ -1,26 +1,39 @@
+import { webtoonData } from "./data.js";
+import { webtoonCard } from "./webtoon.js";
+
 const weekCategory = document.querySelector(".tab .main_category ul");
 const mainCategory = document.querySelector(".main_category ul");
 
 function mainContent(event){
     if(event.target.tagName !=="LI")return;
     else{
-        console.log(event.target);
-
         mainCategory.querySelector(".sub_active").classList.remove("sub_active");
         event.target.classList.add("sub_active");
     }
 }
 
-function showContent(event){
+function getDay(event){
     if(event.target.tagName !== "LI") return;
     else{
-        console.log(event.target);
-        
         weekCategory.querySelector(".weekend_check").classList.remove("weekend_check");
         weekCategory.querySelector(".sub_active").classList.remove("sub_active");
         event.target.classList.add("weekend_check");
-        event.target.classList.add("sub_active");  
+        event.target.classList.add("sub_active"); 
+        
+        showWebtoon(event);
     }
 }
+
+function showWebtoon(event){
+    const dayWebToonData = webtoonData.filter(function(web){
+        return event.target.dataset.value === web.day
+    });
+    
+    // document.querySelector('.webtoon_list ul').innerHTML = "";
+    console.log(dayWebToonData);
+    makeHtml(dayWebToonData);
+}
+
+
 mainCategory.addEventListener("click",mainContent);
-weekCategory.addEventListener("click",showContent);
+weekCategory.addEventListener("click",getDay);
