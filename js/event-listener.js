5cbfec2d69471d071b9623a31baf6a6087953426
연 <mogoo22@naver.com>
2022-02-22 21:41:59 +0900

    feat: 스크롤시 여백에 따라 웹툰 한줄씩 추가

--- js/event-listener.js
+++ js/event-listener.js
@@ -1,9 +1,12 @@
 import { handleCategory } from "./handle-cateogry.js";
+import { scrollDowSerialization } from "./blocks/dow-serialization.js";
 
 const initEventListener = () => {
   document.querySelectorAll(".nav__list").forEach((e) => {
     e.addEventListener("click", handleCategory);
   });
+
+  window.addEventListener("scroll", scrollDowSerialization);
 };
 
 export { initEventListener };
