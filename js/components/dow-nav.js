8cf5de8ecbfa9b63cfd91f8fdc9609166976b574
연 <mogoo22@naver.com>
2022-02-22 20:19:53 +0900

    feat: 메인 내비게이터 클릭시 오늘 요일에 맞는 탭 색상 변경

--- js/components/dow-nav.js
+++ js/components/dow-nav.js
@@ -1,9 +1,15 @@
+import { getToday } from "../utils.js";
+
 const dowNavContents = ["월", "화", "수", "목", "금", "토", "일", "완결"];
 
 export const dowNav = () => {
   return `<div class="dow__nav nav">
   <ul class="nav__list">${dowNavContents.reduce(
-    (acc, cur) => acc + `<li class="nav__item" data-value="${cur}">${cur}</li>`,
+    (acc, cur) =>
+      acc +
+      `<li class="nav__item" ${
+        cur === getToday() ? "id = selected" : ""
+      } data-value="${cur}">${cur}</li>`,
     ""
   )}
   </ul></div>`;
