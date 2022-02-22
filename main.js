76568e420a298b56d7625f6f2b620d0a7933c29e
khj <hongjung.kim.dev@gmail.com>
2022-02-22 21:13:21 +0900

    feat: 홈과 요일연재를 누르면 해당 하위 컨텐츠들 렌더링, 번갈아가면서 누르면 요일연재 하위 컨텐츠만 나오는 문제 미해결

--- main.js
+++ main.js
@@ -1,13 +1,28 @@
 import { $mondayToon } from "./mondayToon.js";
+import $weekWebtoons from "./weekWebtoons.js";
 
+const $main__container = document.querySelector(".main__container");
+const $headerNav = document.querySelector(".header__navigation__menu");
 const element = document.querySelector(".main__week__menu");
 const toonsBox = document.querySelector(".toons__box");
+const $main__container__copy = $main__container.cloneNode(true);
+
+$headerNav.addEventListener("click", (event) => {
+  const selectedImg = event.target.closest("img");
+  const alt = selectedImg ? selectedImg.alt : null;
+  const $main = document.querySelector(".main");
+  const $current__main__container = document.querySelector(".main__container");
+
+  if (alt === "menu home") {
+    $main.replaceChild($main__container__copy, $current__main__container);
+  } else if (alt === "menu toon") {
+    $current__main__container.innerHTML = $weekWebtoons;
+  }
+});
 
 element.addEventListener("click", (event) => {
   const className = event.target.className;
   if (className === "day-월") {
-    console.log("월요일");
-    console.log(toonsBox.childNodes);
     toonsBox.innerHTML = $mondayToon;
   }
 });
---
+++ weekWebtoons.js
@@ -0,0 +1,205 @@
+const $weekWebtoons = `
+<section>
+<nav>
+          <ul class="main__navigation__menu main__week__menu interval">
+            <li>월</li>
+            <li>화</li>
+            <li>수</li>
+            <li>목</li>
+            <li>금</li>
+            <li>토</li>
+            <li>일</li>
+            <li>완결</li>
+          </ul>
+        </nav>
+
+        <section
+          class="header__container main__toon__category container__space__between"
+        >
+          <div>
+            <button>전체</button>
+            <span>|</span>
+            <button>웹툰</button>
+            <span>|</span>
+            <button><span>clock</span>웹툰</button>
+          </div>
+
+          <div>
+            <span>전체 (180)</span>
+            <span>▼</span>
+          </div>
+        </section>
+
+<div class="main__toon__category toon__grid toons__box">
+  <div>
+    <div>
+      <div class="toon__box">
+        <img
+          class="toon"
+          src="https://dn-img-page.kakao.com/download/resource?kid=rOfMN/hyEdeEtJFz/63XlMqYRjhokso828Ki6k0&filename=th2"
+          alt="toon"
+        />
+      </div>
+      <img
+        src="https://static-page.kakao.com/static/common/badge-thumbnail-star.svg?c4d2181b65253b0259cfa219fe4506ac"
+      />
+      <span>9.3</span>
+      <img
+        src="https://static-page.kakao.com/static/common/bmbadge_waitfree.svg?53cf25c84253dee8d32e66da7524dbaf"
+      />
+    </div>
+    <h2 class="title">나 혼자만 레벨업</h2>
+    <div header__container>
+      <img
+        class="read__icon"
+        src="https://static-page.kakao.com/static/common/icon_read_count.png?817b1f83aa0dd8de232a68ac82efd871"
+        alt="read count"
+      />
+      <span>11.5만명</span>
+    </div>
+  </div>
+
+  <div>
+    <div>
+      <div class="toon__box">
+        <img
+          class="toon"
+          src="https://dn-img-page.kakao.com/download/resource?kid=rOfMN/hyEdeEtJFz/63XlMqYRjhokso828Ki6k0&filename=th2"
+          alt="toon"
+        />
+      </div>
+      <img
+        src="https://static-page.kakao.com/static/common/badge-thumbnail-star.svg?c4d2181b65253b0259cfa219fe4506ac"
+      />
+      <span>9.3</span>
+      <img
+        src="https://static-page.kakao.com/static/common/bmbadge_waitfree.svg?53cf25c84253dee8d32e66da7524dbaf"
+      />
+    </div>
+    <h2 class="title">나 혼자만 레벨업</h2>
+    <div header__container>
+      <img
+        class="read__icon"
+        src="https://static-page.kakao.com/static/common/icon_read_count.png?817b1f83aa0dd8de232a68ac82efd871"
+        alt="read count"
+      />
+      <span>11.5만명</span>
+    </div>
+  </div>
+
+  <div>
+    <div>
+      <div class="toon__box">
+        <img
+          class="toon"
+          src="https://dn-img-page.kakao.com/download/resource?kid=rOfMN/hyEdeEtJFz/63XlMqYRjhokso828Ki6k0&filename=th2"
+          alt="toon"
+        />
+      </div>
+      <img
+        src="https://static-page.kakao.com/static/common/badge-thumbnail-star.svg?c4d2181b65253b0259cfa219fe4506ac"
+      />
+      <span>9.3</span>
+      <img
+        src="https://static-page.kakao.com/static/common/bmbadge_waitfree.svg?53cf25c84253dee8d32e66da7524dbaf"
+      />
+    </div>
+    <h2 class="title">나 혼자만 레벨업</h2>
+    <div header__container>
+      <img
+        class="read__icon"
+        src="https://static-page.kakao.com/static/common/icon_read_count.png?817b1f83aa0dd8de232a68ac82efd871"
+        alt="read count"
+      />
+      <span>11.5만명</span>
+    </div>
+  </div>
+
+  <div>
+    <div>
+      <div class="toon__box">
+        <img
+          class="toon"
+          src="https://dn-img-page.kakao.com/download/resource?kid=rOfMN/hyEdeEtJFz/63XlMqYRjhokso828Ki6k0&filename=th2"
+          alt="toon"
+        />
+      </div>
+      <img
+        src="https://static-page.kakao.com/static/common/badge-thumbnail-star.svg?c4d2181b65253b0259cfa219fe4506ac"
+      />
+      <span>9.3</span>
+      <img
+        src="https://static-page.kakao.com/static/common/bmbadge_waitfree.svg?53cf25c84253dee8d32e66da7524dbaf"
+      />
+    </div>
+    <h2 class="title">나 혼자만 레벨업</h2>
+    <div header__container>
+      <img
+        class="read__icon"
+        src="https://static-page.kakao.com/static/common/icon_read_count.png?817b1f83aa0dd8de232a68ac82efd871"
+        alt="read count"
+      />
+      <span>11.5만명</span>
+    </div>
+  </div>
+
+  <div>
+    <div>
+      <div class="toon__box">
+        <img
+          class="toon"
+          src="https://dn-img-page.kakao.com/download/resource?kid=rOfMN/hyEdeEtJFz/63XlMqYRjhokso828Ki6k0&filename=th2"
+          alt="toon"
+        />
+      </div>
+      <img
+        src="https://static-page.kakao.com/static/common/badge-thumbnail-star.svg?c4d2181b65253b0259cfa219fe4506ac"
+      />
+      <span>9.3</span>
+      <img
+        src="https://static-page.kakao.com/static/common/bmbadge_waitfree.svg?53cf25c84253dee8d32e66da7524dbaf"
+      />
+    </div>
+    <h2 class="title">나 혼자만 레벨업</h2>
+    <div header__container>
+      <img
+        class="read__icon"
+        src="https://static-page.kakao.com/static/common/icon_read_count.png?817b1f83aa0dd8de232a68ac82efd871"
+        alt="read count"
+      />
+      <span>11.5만명</span>
+    </div>
+  </div>
+
+  <div>
+    <div>
+      <div class="toon__box">
+        <img
+          class="toon"
+          src="https://dn-img-page.kakao.com/download/resource?kid=rOfMN/hyEdeEtJFz/63XlMqYRjhokso828Ki6k0&filename=th2"
+          alt="toon"
+        />
+      </div>
+      <img
+        src="https://static-page.kakao.com/static/common/badge-thumbnail-star.svg?c4d2181b65253b0259cfa219fe4506ac"
+      />
+      <span>9.3</span>
+      <img
+        src="https://static-page.kakao.com/static/common/bmbadge_waitfree.svg?53cf25c84253dee8d32e66da7524dbaf"
+      />
+    </div>
+    <h2 class="title">나 혼자만 레벨업</h2>
+    <div header__container>
+      <img
+        class="read__icon"
+        src="https://static-page.kakao.com/static/common/icon_read_count.png?817b1f83aa0dd8de232a68ac82efd871"
+        alt="read count"
+      />
+      <span>11.5만명</span>
+    </div>
+  </div>
+</div>
+</section>
+`;
+
+export default $weekWebtoons;
