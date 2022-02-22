fe88e6ddb0d9696b3cec1564ad769f94f5f0ffe5
youryu0212 <youryu0212@naver.com>
2022-02-23 02:17:14 +0900

    remove : rename file

--- change_circle_position.js
+++
@@ -1,7 +0,0 @@
-export const changeCirclePosition = (dayOfTheWeek) => {
-  const circle = document.querySelector(".circle");
-  const newClassName = `circle main-content__content__nav__circile-${dayOfTheWeek}`;
-  circle.className = newClassName;
-  console.log(dayOfTheWeek);
-  console.log(circle,circle.className);
-}
--- selected_position_change.js
+++
@@ -1,6 +0,0 @@
-export const selectedPositionChange = (el,idx) => {
-  for (let j = 0 ; j<el.length ; j++){
-    el[j].className = "nav-box__text weekly-nav";
-  }
-  el[idx].className = "nav-box__text weekly-nav selected-text";
-}
