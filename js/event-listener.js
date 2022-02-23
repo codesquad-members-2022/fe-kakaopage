c571694df7b52133b5a649abbff8786c918cf6ab
연 <mogoo22@naver.com>
2022-02-23 12:44:05 +0900

    feat: main-ad-banner에 무한슬라이드 구현

--- js/event-listener.js
+++ js/event-listener.js
@@ -1,5 +1,6 @@
 import { handleCategory } from "./handle-cateogry.js";
 import { scrollDowSerialization } from "./blocks/dow-serialization.js";
+import { slideShow } from "./event-slide.js";
 
 const initEventListener = () => {
   document.querySelectorAll(".nav__list").forEach((e) => {
@@ -7,6 +8,10 @@ const initEventListener = () => {
   });
 
   window.addEventListener("scroll", scrollDowSerialization);
+
+  document.querySelectorAll(".main-ad-banner button").forEach((e) => {
+    e.addEventListener("click", slideShow);
+  });
 };
 
 export { initEventListener };
