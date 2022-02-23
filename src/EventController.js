4f48f0b288aa19bc4a1f9095bb763036f0e80083
Hemudi <ksum1205@naver.com>
2022-02-23 17:15:38 +0900

    Refact: 파일 이름 변경, 재사용 가능한 형태로 리팩토링

---
+++ src/EventController.js
@@ -0,0 +1,29 @@
+import * as Render from "./Render.js"
+
+function setNavEvent(className){
+    const $nav = document.querySelector(`.${className}`);
+    const eventHandler = getNavEventHandler(className);
+    $nav.addEventListener('click', eventHandler);
+}
+
+function getNavEventHandler(className){
+    const handlers = {
+        'week-nav__list' : weekNavClickHandler
+    }
+    return handlers[className];
+}
+
+function weekNavClickHandler(event){
+    const selected = 'week-nav__item-selected';
+    const $nav_item = event.target;
+    const $nav_item_selected = document.querySelector(`.${selected}`);
+    const dataKey = $nav_item.dataset.dataKey;
+
+    if($nav_item === $nav_item_selected) return;
+
+    $nav_item.classList.add(selected);
+    $nav_item_selected.classList.remove(selected);
+    Render.renderTopList('week', dataKey);
+}
+
+export { setNavEvent };
