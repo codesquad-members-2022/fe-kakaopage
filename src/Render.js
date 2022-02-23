0dda48471d47d07ff4120fba17ae5297e6ddfdee
Hemudi <ksum1205@naver.com>
2022-02-23 17:24:13 +0900

    Feat: 홈 서브 메뉴 동적 생성 기능 구현

--- src/Render.js
+++ src/Render.js
@@ -6,7 +6,7 @@ const $body_main = document.querySelector('.custom');
 
 export function rendering(tabName){
     const renders = {
-        'home' : null,
+        'home' : renderHomePage,
         'daily' : null,
         'webToon' : null,
         'boy' : null,
@@ -18,6 +18,25 @@ export function rendering(tabName){
     }
 
     $body_main.innerHTML = '';
-    // renders[tabName](tabName);
+    renders[tabName](tabName);
     setNavEvent('main-nav__list');
+}
+
+function renderHomePage(tabName){
+    renderMenu(tabName);
+}
+
+function renderMenu(tabName){
+    const $menu_section = document.createElement('section');
+    const $menu_div = document.createElement('div');
+    $menu_div.classList.add('contents_menu__container', 'center');
+    $menu_section.classList.add('contents_menu', 'section');
+    $menu_section.appendChild($menu_div);
+    $body_main.appendChild($menu_section);
+    Menu[tabName].map(data => $menu_div.innerHTML += HTMLCreator.createMenuItemHTML(data));
+}
+
+export function renderTopList(listType, dataKey = null){
+    const comicList = document.getElementById(`list-${listType}`);
+    comicList.innerHTML = HTMLCreator.createTopListHTML(dataKey ? dataKey : listType);
 }
