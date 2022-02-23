6e62af1388a44836bb47d4deafea69d036fdbba0
happyGyu <happyboyto.dev@gmail.com>
2022-02-24 00:02:58 +0900

    refactor: render후 이벤트 붙이는 콜백함수를 따로 분리

--- src/render.js
+++ src/render.js
@@ -16,6 +16,7 @@ export const initRender = () => {
     activateAll();
 }
 
+
 const renderByGenre = (genre) => {
     const renderList = genreRenderList[genre];
     document.querySelector('.tab-contents').innerHTML = '';
@@ -23,16 +24,17 @@ const renderByGenre = (genre) => {
     activateDayTab();
 }
 
-
 const render = (component, genre) => {
     const funcs = {
         'mainBanner': renderMainBanner,
         'categoryGrid': renderCategoryGrid,
         'eventBanner': renderEventBanner,
-        'dayRanking': renderWorkSection.bind(null, "small", component)
+        'dayRanking': renderWorkSection.bind(null, "small", component),
+        신작 : renderWorkSection.bind(null, 'horizontal')
     }
     funcs[component](genre);
 }
+
 
 const activateAll = () => {
     activateGnb();
@@ -40,48 +42,98 @@ const activateAll = () => {
     activateDayTab();
 }
 
+//이하 중복 해결해야함
+
+const handleTabClick = (event, type) => {
+    const selectionMark = `.${type}-tab__item--selected`;
+    const prevSelected = document.querySelector(selectionMark);
+    const newSelected = event.target.closest('li');
+    if (prevSelected === newSelected) return;
+    moveSelectionMark(prevSelected, newSelected);
+    reRender(newSelected, type);
+}
+
+const moveSelectionMark = (prevSelected, newSelected, type) => {
+    const selectionMark = `.${type}-tab__item--selected`;
+    prevSelected.classList.remove(selectionMark);
+    newSelected.classList.add(selectionMark);
+}
+
+const reRender = (newSelected, type) => {
+    console.log(type);
+    console.log(newSelected.dataset)
+    const newTab = newSelected.dataset[type];
+    switch (type) {
+        case 'gnb': 
+            renderGenreTab(newTab);
+            break;
+        case 'genre':
+            renderByGenre(newTab);
+            break;
+        case 'day':
+            const dayRankSection = newSelected.closest('section');
+            changeContentsByDay(dayRankSection, newTab);
+    }
+}
+
+
 const activateGnb = () => {  
     const gnb = document.querySelector('.gnb');
-    gnb.addEventListener('click', (e) => {
-        const prevSelected = document.querySelector('.gnb .tab__item--selected');
-        const selected = e.target.closest('li');
-        if (prevSelected === selected) return;
-        prevSelected.classList.remove('tab__item--selected');
-        selected.classList.add('tab__item--selected');
-        
-        const currPage = selected.dataset.tab;
-        renderGenreTab(currPage);    
-    })
+    gnb.addEventListener('click', (e) => handleTabClick(e, 'gnb'));
 }
 
 const activateGenreTab = () => {
     const genreTab = document.querySelector('.genre-tab');
-    genreTab.addEventListener('click', (e) => {
-        const prevSelected = genreTab.querySelector('.genre-tab__item--selected');
-        const selected = e.target.closest('li');
-        if (prevSelected === selected) return;
-        prevSelected.classList.remove('genre-tab__item--selected');
-        selected.classList.add('genre-tab__item--selected');
-
-        renderByGenre(selected.dataset.genre);
-    })
+    genreTab.addEventListener('click', (e) => handleTabClick(e, 'genre'));
 }
 
 const activateDayTab = () => {
     const dayTab = document.querySelector('.day-tab__container');
-    dayTab.addEventListener('click', (e) => {
-        const prevSelected = dayTab.querySelector('.day-tab__item--selected');
-        const selected = e.target.closest('li');
-        if (prevSelected === selected) return;
-        prevSelected.classList.remove('day-tab__item--selected', 'tab__item--selected');
-        selected.classList.add('day-tab__item--selected', 'tab__item--selected');
-
-        const dayRankSection = dayTab.parentNode;
-        const oldContainer = dayRankSection.querySelector('.work-container');
-        changeContentsByDay(dayRankSection, selected.dataset.day);
-    })
+    dayTab.addEventListener('click', (e) => handleTabClick(e,'day'));
 }
 
 
 
 
+
+// const activateGnb = () => {  
+//     const gnb = document.querySelector('.gnb');
+//     gnb.addEventListener('click', (e) => {
+//         const prevSelected = document.querySelector('.gnb .tab__item--selected');
+//         const selected = e.target.closest('li');
+//         if (prevSelected === selected) return;
+//         prevSelected.classList.remove('tab__item--selected');
+//         selected.classList.add('tab__item--selected');
+        
+//         const currPage = selected.dataset.tab;
+//         renderGenreTab(currPage);    
+//     })
+// }
+
+// const activateGenreTab = () => {
+//     const genreTab = document.querySelector('.genre-tab');
+//     genreTab.addEventListener('click', (e) => {
+//         const prevSelected = genreTab.querySelector('.genre-tab__item--selected');
+//         const selected = e.target.closest('li');
+//         if (prevSelected === selected) return;
+//         prevSelected.classList.remove('genre-tab__item--selected');
+//         selected.classList.add('genre-tab__item--selected');
+
+//         renderByGenre(selected.dataset.genre);
+//     })
+// }
+
+// const activateDayTab = () => {
+//     const dayTab = document.querySelector('.day-tab__container');
+//     dayTab.addEventListener('click', (e) => {
+//         const prevSelected = dayTab.querySelector('.day-tab__item--selected');
+//         const selected = e.target.closest('li');
+//         if (prevSelected === selected) return;
+//         prevSelected.classList.remove('day-tab__item--selected', 'tab__item--selected');
+//         selected.classList.add('day-tab__item--selected', 'tab__item--selected');
+
+//         const dayRankSection = dayTab.parentNode;
+//         const oldContainer = dayRankSection.querySelector('.work-container');
+//         changeContentsByDay(dayRankSection, selected.dataset.day);
+//     })
+// }
