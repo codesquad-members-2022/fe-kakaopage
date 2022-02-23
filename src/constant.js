5cf7036bb90ec5aa956464474667783af58744c7
dony <rsefa94@gmail.com>
2022-02-23 12:36:38 +0900

    Fix: $CONTENTS 상수 삭제, DATA_URL 상수 추가

--- src/constant.js
+++ src/constant.js
@@ -1,7 +1,7 @@
 import { $ } from '/src/utility.js';
 
 const INITIAL_PAGE = "웹툰";
-const $CONTENTS = $(".contents");
 const SLIDE_LENGTH = 3;
+const DATA_URL = '/src/data/';
 
-export { INITIAL_PAGE, $CONTENTS, SLIDE_LENGTH };
+export { INITIAL_PAGE, SLIDE_LENGTH, DATA_URL };
--- src/data.js
+++ src/data.js
@@ -1,11 +1,13 @@
+import { DATA_URL } from "./constant.js"
+
 const getPageData = (page) => {
     const jsonUrl = {
-        '홈': '/src/data/home.json',
-        '웹툰': '/src/data/webtoon.json',
-        '웹소설': '/src/data/webFiction.json',
-        '영화': '/src/data/movie.json',
-        '방송': '/src/data/tv.json',
-        '책': '/src/data/book.json'
+        '홈': `${DATA_URL}home.json`,
+        '웹툰': `${DATA_URL}webtoon.json`,
+        '웹소설': `${DATA_URL}webFiction.json`,
+        '영화': `${DATA_URL}movie.json`,
+        '방송': `${DATA_URL}tv.json`,
+        '책': `${DATA_URL}book.json`
     }
     return jsonUrl[page];
 }
