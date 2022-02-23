a7579ae697a1b3d0bb57b802beacb219d5702873
youryu0212 <youryu0212@naver.com>
2022-02-23 11:36:42 +0900

    style : class name shorten from main-content__content to main-content and from main-content to main

--- change-circle-position.js
+++ change-circle-position.js
@@ -1,7 +1,7 @@
 import { $ } from './selector.js';
 export const changeCirclePosition = (dayOfTheWeek) => {
   const circle = $(".circle");
-  const newClassName = `circle main-content__content__nav__circile-${dayOfTheWeek}`;
+  const newClassName = `circle main-content__nav__circile-${dayOfTheWeek}`;
   circle.className = newClassName;
   // console.log(dayOfTheWeek);
   // console.log(circle,circle.className);
