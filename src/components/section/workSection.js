bef0e55bd5fa52ae210ca116d6b8221782d14bd2
happyGyu <happyboyto.dev@gmail.com>
2022-02-23 14:38:07 +0900

    feat: 요일 탭 기능 구현

--- src/components/section/workSection.js
+++ src/components/section/workSection.js
@@ -10,7 +10,12 @@ const dataDic = {
 export const renderWorkSection = (layout, contents, genre) => {
   const workSection = document.createElement('section');
   workSection.insertAdjacentHTML('beforeend', getHeaderTemplate());
-  workSection.insertAdjacentHTML('beforeend', getWorkContainerTemplate(layout, contents, genre));
+  if (contents === 'dayRanking') {
+    workSection.insertAdjacentHTML('beforeend', getDayTabTemplate());
+    setDefault(workSection.querySelector(`li[data-day='${DEFAULT_DAY}']`));
+  }
+  
+  workSection.insertAdjacentElement('beforeend', getWorkContainerTemplate(layout, contents, genre));
   document.querySelector('.tab-contents').appendChild(workSection);
 }
 
@@ -28,7 +33,34 @@ const getHeaderTemplate = () => {
         </div>`
 }
 
-const getWorkContainerTemplate = (layout, contents, genre) => {
-  const data = dataDic[contents][genre][DEFAULT_DAY];
-  return makeWorkContainer(data, layout);
-} 
+const getWorkContainerTemplate = (layout, contents, genre, day = DEFAULT_DAY) => {
+  const data = contents === 'dayRanking' ? dataDic[contents][genre][day] : dataDic[contents][genre];
+  return makeWorkContainer(genre, data, layout);
+}
+
+const getDayTabTemplate = () => {
+  return `
+    <ul class="day-tab__container tab__container">
+      <li class="day-tab__item center" data-day="mon">월</li>
+      <li class="day-tab__item center" data-day="tue">화</li>
+      <li class="day-tab__item center" data-day="wed">수</li>
+      <li class="day-tab__item center" data-day="thr">목</li>
+      <li class="day-tab__item center" data-day="fri">금</li>
+      <li class="day-tab__item center" data-day="sat">토</li>
+      <li class="day-tab__item center" data-day="sun">일</li>
+      <li class="day-tab__item center" data-day="end">완결</li>
+    </ul>
+  `
+}
+
+const setDefault = (item) => {
+  item.classList.add('day-tab__item--selected' ,'tab__item--selected');
+}
+
+export const changeContentsByDay = (dayRankSection, day) => {
+  const oldContainer = dayRankSection.querySelector('.work-container');
+  const currGenre = oldContainer.dataset.genre;
+  const newContainer = getWorkContainerTemplate('small', 'dayRanking', currGenre, day);
+  
+  dayRankSection.replaceChild(newContainer, oldContainer)
+}
