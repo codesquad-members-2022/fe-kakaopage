37894ee1cf5fdc4af7205478e33ce2548c2a6ab9
suuzin <8oozin@gmail.com>
2022-02-23 11:01:52 +0900

    refactor:불필요한 코드 삭제

--- kakao.js
+++ kakao.js
@@ -4,44 +4,47 @@ import { webtoonCard } from "./webtoon.js";
 const weekCategory = document.querySelector(".tab .main_category ul");
 const mainCategory = document.querySelector(".main_category ul");
 
-function mainContent(event){
-    if(event.target.tagName !=="LI")return;
-    else{
-        mainCategory.querySelector(".sub_active").classList.remove("sub_active");
-        event.target.classList.add("sub_active");
+function showMain(){
+    function mainContent(event){
+        if(event.target.tagName !=="LI")return;
+        else{
+            mainCategory.querySelector(".sub_active").classList.remove("sub_active");
+            event.target.classList.add("sub_active");
+        }
     }
+    mainCategory.addEventListener("click",mainContent);
 }
 
-function getDay(event){
-    if(event.target.tagName !== "LI") return;
-    else{
-        weekCategory.querySelector(".weekend_check").classList.remove("weekend_check");
-        weekCategory.querySelector(".sub_active").classList.remove("sub_active");
-        event.target.classList.add("weekend_check");
-        event.target.classList.add("sub_active"); 
-        
-        showWebtoon(event);
+function showDayWebtoon(){
+    function getDay(event){
+        if(event.target.tagName !== "LI") return;
+        else{
+            weekCategory.querySelector(".weekend_check").classList.remove("weekend_check");
+            weekCategory.querySelector(".sub_active").classList.remove("sub_active");
+            event.target.classList.add("weekend_check");
+            event.target.classList.add("sub_active"); 
+            showWebtoon(event);
+        }
     }
+    weekCategory.addEventListener("click",getDay);
 }
 
 function showWebtoon(event){
-    const dayWebToonData = webtoonData.filter(function(web){
-        return event.target.dataset.value === web.day
+    const dayWebToonData = webtoonData.filter(function(webtoon){
+        return event.target.dataset.value === webtoon.day
     });
-    
     // document.querySelector('.webtoon_list ul').innerHTML = "";
-    console.log(dayWebToonData);
     makeHtml(dayWebToonData);
 }
 
 function makeHtml(dayWebToonData){
-    
-    // const webtoonCardTemplate = dayWebToonData.map(e=>webtoonCard(e)).reduce((acc,cur)=>acc+cur,"");
-    const webtoonCardTemplate = dayWebToonData.reduce((acc,cur) => acc+webtoonCard(cur),"");
+    const webtoonCardTemplate = dayWebToonData.map(e => webtoonCard(e)).reduce((acc,cur)=>acc+cur,"");
+    // const webtoonCardTemplate = dayWebToonData.reduce((acc,cur) => acc + webtoonCard(cur),"");
     document.querySelector('.webtoon_list ul').innerHTML = webtoonCardTemplate;
 }
 
-mainCategory.addEventListener("click",mainContent);
-weekCategory.addEventListener("click",getDay);
-
-
+function init(){
+    showMain();
+    showDayWebtoon()
+}
+init();
