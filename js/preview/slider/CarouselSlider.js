867f37ad47057a43e51ea414f5d52d60a398244c
bcad1591 <bcad1591@gmail.com>
2022-02-24 03:20:06 +0900

    Feat: 버튼 이벤트 리스너 추가
    버튼 이벤트 리스너 구현에 필요한 인스턴스 프로퍼티들을 추가
    prevBtnEl, nextBtnEl -> 버튼 DOM 엘리먼트
    여러 번 클릭시 이벤트 리스너로의 진입을 잠시 막아주는 stopFlag 추가
    this 바인딩을 명시적으로 해준 이유는 익명함수로 이벤트 리스너 해제가 어렵기 때문에 기명함수를 사용하기 위해서이다. (아직까진 리스너를 해제할 필요는 없다)
    슬라이드 효과에서 사용자에게 보이는 사진은  html 마크업 구조상 항상 가운데에 위치하기 때문에, 사진이 1장인 경우에도 동일한 사진을 양옆에 끼워넣어서 마크업 구조상 사진 3개를 맞추었다.

--- js/preview/slider/CarouselSlider.js
+++ js/preview/slider/CarouselSlider.js
@@ -1,12 +1,66 @@
 import ItemSet from './ItemSet.js';
-
+import StopFlag from './StopFlag.js';
 export default class CarouselSlider {
-  constructor({ elementWidth, createItemElFunc }) {
+  constructor({ elementWidth, createItemElFunc, prevBtnEl, nextBtnEl, timer }) {
     this.sliderEl = document.querySelector('.slider');
     this.elementWidth = elementWidth;
-    this.state = null;
     this.createItemEl = createItemElFunc;
+    this.timer = timer;
+    this.state = null;
     this.itemSet = null;
+    this.prevBtnEl = prevBtnEl;
+    this.nextBtnEl = nextBtnEl;
+    this.stopFlag = new StopFlag();
+    this.prevBtnEl.addEventListener('click', this.onPrevBtn.bind(this));
+    this.nextBtnEl.addEventListener('click', this.onNextBtn.bind(this));
+    this.sliderEl.addEventListener(
+      'transitionend',
+      this.transitionEnd.bind(this)
+    );
+  }
+  onPrevBtn() {
+    if (this.stopFlag.isTrue()) return;
+    this.stopFlag.setTrue();
+    this.timer.clearTimer();
+    this.timer.setTimer(2);
+    const { itemSet } = this;
+    if (itemSet.getLength() === 1) return;
+    this.moveToPrevPage();
+    itemSet.decreaseIdx();
+    this.setStateFirst();
+  }
+  onNextBtn() {
+    if (this.stopFlag.isTrue()) return;
+    this.stopFlag.setTrue();
+    this.timer.clearTimer();
+    this.timer.setTimer(2);
+    const { itemSet } = this;
+    if (itemSet.getLength() === 1) return;
+    this.moveToNextPage();
+    itemSet.increaseIdx();
+    this.setStateLast();
+  }
+  transitionEnd() {
+    const { sliderEl, itemSet } = this;
+    this.offTransition();
+    if (this.isFirstPage()) {
+      console.log('firt');
+      const prevItem = itemSet.getPrevItem();
+      const prevItemEl = this.createItemEl(prevItem);
+      sliderEl.removeChild(sliderEl.lastElementChild);
+      sliderEl.insertBefore(prevItemEl, sliderEl.firstElementChild);
+    }
+    if (this.isLastPage()) {
+      console.log('last');
+      const nextItem = itemSet.getNextItem();
+      const nextItemEl = this.createItemEl(nextItem);
+      sliderEl.removeChild(sliderEl.firstElementChild);
+      sliderEl.appendChild(nextItemEl);
+    }
+    this.setPageMid();
+    setTimeout(() => {
+      this.stopFlag.setFalse();
+    }, 200);
   }
   initItemSet(itemSet) {
     this.itemSet = new ItemSet(itemSet);
@@ -16,18 +70,22 @@ export default class CarouselSlider {
   }
   initSlideList() {
     const { items, length } = this.itemSet;
-    console.log(length);
+    this.offTransition();
+    this.setPageMid();
+
     if (length === 1) {
-      this.setPageFirst();
       const firstItem = items[0];
+      this.sliderEl.appendChild(this.createItemEl(firstItem));
+      this.sliderEl.appendChild(this.createItemEl(firstItem));
       this.sliderEl.appendChild(this.createItemEl(firstItem));
       return;
     }
-    this.setPageMid();
+
     const targetItems = [items[length - 1], items[0], items[1]];
     targetItems.forEach((targetItem) => {
       this.sliderEl.appendChild(this.createItemEl(targetItem));
     });
+    this.stopFlag.setFalse(); // 다른 카테고리의 슬라이드를 넘기다가 카테고리 전환한 경우 stopFlag 초기화
     return;
   }
   setStateFirst() {
@@ -61,7 +119,7 @@ export default class CarouselSlider {
     return this.state === 'first';
   }
   isLastPage() {
-    return this.staet === 'last';
+    return this.state === 'last';
   }
   onTransition() {
     this.sliderEl.style.transition = 'transform 300ms';
