3009bae6ac89f5aef3c1dd902c0dd7268fa48a08
youryu0212 <youryu0212@naver.com>
2022-02-23 11:04:58 +0900

    style : change document.queryselector to $

--- handler.js
+++ handler.js
@@ -1,10 +1,11 @@
 import { changeCirclePosition } from './change-circle-position.js';
-import {changeImageBoxInfo} from './change-image-box.js';
+// import {changeImageBoxInfo} from './change-image-box.js';
 import {changeImageCardInfo} from './change-image-card.js';
 import {weeklyDataInfo} from './data.js';
 import { selectedPositionChange } from './selected-position-change.js';
+import { $ } from './selector.js';
 
-const el = document.querySelector(".weekly-webtoon-ul");
+const weeklyWebtoonUl = $(".weekly-webtoon-ul");
 const weeklyIdxChanger = {
   "월" : 0,
   "화" : 1,
@@ -15,14 +16,15 @@ const weeklyIdxChanger = {
   "일" : 6,
 }
 
-const handler = (el,weekly) => {
+const handler = (weeklyWebtoonUl,weekly) => {
   const i = weeklyIdxChanger[weekly.innerHTML];
   const boxInfo = weeklyDataInfo[i]["box"]
   const cardInfo = weeklyDataInfo[i]["card"]
   const dayOfTheWeek = weeklyDataInfo[i]["dayOfTheWeek"]
-  changeImageBoxInfo(boxInfo);
+  // changeImageBoxInfo : 추후 배너광고 이미지 박스 정보를 변경하고 싶을때 활용하기
+  // changeImageBoxInfo(boxInfo);
   changeImageCardInfo(cardInfo);
   changeCirclePosition(dayOfTheWeek);
-  selectedPositionChange(el.querySelectorAll("li"),i);
+  selectedPositionChange(weeklyWebtoonUl.querySelectorAll("li"),i);
 }
-el.addEventListener("click", (evt) => {if (evt.target.nodeName === "LI") handler(el,evt.target)});
+weeklyWebtoonUl.addEventListener("click", (evt) => {if (evt.target.nodeName === "LI") handler(weeklyWebtoonUl,evt.target)});
