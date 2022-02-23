ced4d0cca364de7b2bbfcff5cebafa87e8ad2bf3
yunho <kimyouknow@naver.com>
2022-02-23 11:32:06 +0900

    Chore: carousel mock데이터 서브별 데이터에서 해당 서브탭 하위 데이터로 변경

--- static/js/category/Webtoon.js
+++ static/js/category/Webtoon.js
@@ -32,26 +32,26 @@ const subCategoryIndexArr = [
 
 const mock_carouselElementArr = [
   {
-    title: '홈 서브 카테고리',
+    title: '프론트',
     views: 25.6,
     genre: '웹툰',
-    subTitle: '홈 서브 카테고리: 서브 타이틀 입니다',
+    subTitle: '프론트: 서브 타이틀 입니다',
     imgUrl:
       'https://dn-img-page.kakao.com/download/resource?kid=c0XBz/hzp2jFzCR9/JCvx5dagQ345yKhVf2m5SK',
   },
   {
-    title: '요일연재 서브 카테고리',
+    title: '백앤드',
     views: 30.6,
     genre: '웹툰',
-    subTitle: '요일연재 서브 카테고리: 서브 타이틀 입니다',
+    subTitle: '백앤드: 서브 타이틀 입니다',
     imgUrl:
       'https://dn-img-page.kakao.com/download/resource?kid=bJ0Aku/hzp2moMU6e/cEihkdBNG70ikK3gfKUK81',
   },
   {
-    title: '웹툰 서브 카테고리',
+    title: '디자인',
     views: 10.2,
     genre: '웹툰',
-    subTitle: '웹툰 서브 카테고리: 서브 타이틀 입니다',
+    subTitle: '디자인: 서브 타이틀 입니다',
     imgUrl:
       'https://dn-img-page.kakao.com/download/resource?kid=b1GVlh/hzp2nOLuZJ/qq3oH7NLIRt6ApNh6TadUK',
   },
