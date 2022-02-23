e71614959ea7885bd6830286aa919087a7d4e6f1
ju-kkim <68211156+ju-kkim@users.noreply.github.com>
2022-02-23 17:33:38 +0900

    [J] kakaopage 2주차 PR1 (#87)
    * Refactor: changeTopBanner template코드 반환하는 함수 추가하여 연결
    
    - top banner html 코드 반환하는 함수 creatTopBannerHtml
    
    * Refactor: gnb, lnb, dayNav 클릭 함수로 감싸기
    
    * Fix: join() -> join('') 수정
    
    * Feat: 요일 웹툰 데이터 추가
    
    * Feat: 요일탭 데이터 가져오기 기능 추가
    
    * Style: 아이템 스타일 수정
    
    * Docs: 웹툰_웹툰 스타일로 html 수정
    
    * Refactor: 현재 메뉴 다시 클릭시 return
    
    * Docs: .gitignore 내용추가
    
    * Feat: express 서버 설정
    
    * Fix: Top banner data 수정
    
    * Fix: Top banner data 추가
    
    * Style: slider animate 스타일 추가
    
    * Feat: Slider 초기 설정 추가
    
    * Feat: Slider 좌우 버튼 클릭 기능 추가
    
    * Fix: 슬라이드 좌우 무한 슬라이딩으로 수정

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,25 +1,36 @@
-import {topBanner} from "./data.js";
+import {topBanner, webtoonData} from "./data.js";
 
-const gnb = document.querySelector(".gnb ul");
-const lnb = document.querySelector(".lnb ul");
-const dayNav = document.querySelector(".day_nav ul");
+function clickMenu() {
+    const gnb = document.querySelector(".gnb ul");
+    const lnb = document.querySelector(".lnb ul");
+    const dayNav = document.querySelector(".day_nav ul");
+    const topBannerDefault = "home";
+    const dayNavDefault = "mon";
 
-gnb.addEventListener("click", (e) => {
-    const eventTarget = e.target;
-    toActivateNav(eventTarget, gnb);
-})
+    
+    gnb.addEventListener("click", (e) => {
+        if(e.target.closest('li').classList.contains('active')) return
+        toActivateNav(e.target, gnb);
+    })
+    
+    lnb.addEventListener("click", (e) => {
+        if(e.target.closest('li').classList.contains('active')) return
+        toActivateNav(e.target, lnb);
+        const targetLnb = e.target.closest('li').getAttribute('data-lnb');
+        changeTopBanner(targetLnb);
+    })
+    
+    dayNav.addEventListener("click", (e) => {
+        if(e.target.closest('li').classList.contains('active')) return
+        toActivateNav(e.target, dayNav);
+        const targetDay = e.target.closest('li').getAttribute('data-day');
+        createWebtoonDayContents(targetDay)
 
-lnb.addEventListener("click", (e) => {
-    const eventTarget = e.target;
-    const targetLnb = eventTarget.closest('li').getAttribute('data-lnb');
-    toActivateNav(eventTarget, lnb);
-    changeTopBanner(targetLnb);
-})
+    })
 
-dayNav.addEventListener("click", (e) => {
-    const eventTarget = e.target;
-    toActivateNav(eventTarget, dayNav);
-})
+    changeTopBanner(topBannerDefault);
+    createWebtoonDayContents(dayNavDefault)
+}
 
 function toActivateNav(target, nav) {
     const clickTarget = target.closest('li');
@@ -30,13 +41,15 @@ function toActivateNav(target, nav) {
 
 function changeTopBanner(targetLnb) {
     const banner = document.querySelector('.top_banner');
-    
     const targetBannerData = topBanner[targetLnb];
-    let itemHtml = '';
+    const itemHtml = targetBannerData.map(data => creatTopBannerHtml(data));
+    banner.querySelector('.slider_items').innerHTML = itemHtml.join('');
 
-    targetBannerData.forEach(data => {
-        
-        itemHtml += `
+    initSlider(banner);
+}
+
+function creatTopBannerHtml(data) {
+    return `
         <div class="item item_type1">
             <div class="img">
                 <img src="${data.image}" alt="${data.title}">
@@ -55,12 +68,162 @@ function changeTopBanner(targetLnb) {
                 <p class="desc">${data.description}</p>
             </div>
         </div>`;
-    });
+}
 
+function getBadge(data) {
+    let badge = '';
+    for(let key in data.badge) {
+        if(data.badge[key]) {
+            switch (key) {
+                case 'up' :
+                    badge += '<img src="./assets/images/ico/badge_up_blue.svg" alt="up">'
+                    break;
+                case 'new' :
+                    badge += '<img src="./assets/images/ico/badge_new_red.svg" alt="새작품"></img>'
+                    break;
+                case 'age15' :
+                    badge += '<img src="./assets/images/ico/badge_15.png" alt="15세 작품">'
+                    break;
+            }
+        }
+    }
+    return badge
+}
 
-    banner.querySelector('.slider_items').innerHTML = itemHtml;
+function createItemType3Horizontal(data) {
+    const webtoonBadge = getBadge(data)
+    return `<li class="item item_type3 flex horizontal ${data.class || ''}">
+        <a href="#!">
+            <div class="img">
+                <img src="${data.image}" alt="${data.title}">
+            </div>
+            <div class="txt">
+                <div class="item_ttl_box">
+                    <div class="badge">
+                        ${webtoonBadge}
+                    </div>
+                    <h4 class="item_ttl text_ellipsis">${data.title}</h4>
+                </div>
+                <p class="info_txt item_desc text_ellipsis">${data.description}</p>
+                <div class="info">
+                    <span class="info_txt viewer">${data.viewer}</span>
+                    <span class="info_txt text_ellipsis">${data.writer}</span>
+                </div>
+            </div>
+        </a>
+    </li>`
+}
+
+function createWebtoonDayContents(targetDay) {
+    const items = webtoonData.map(data => {
+        if(data.week === targetDay || targetDay === "all") {
+            return createItemType3Horizontal(data);
+        }
+    })
+    document.querySelector('.day_contents .item_list').innerHTML = items.join('');
+}
+
+function initSlider(sWrapper) {
+    const sliderWrapper = sWrapper;
+    const sliderBox = sliderWrapper.querySelector('.slider_items');
+    const sliderPager = sliderWrapper.querySelector('.slider_pager');
+    const sliderItem = sliderBox.querySelectorAll('.item');
+    const itemLength = sliderItem.length;
+    const firstItem = sliderBox.firstElementChild;
+    const lastItem = sliderBox.lastElementChild;
+    const cloneFirst = firstItem.cloneNode(true);
+    const cloneLast = lastItem.cloneNode(true);
+    cloneFirst.classList.add('clone');
+    cloneLast.classList.add('clone');
+    sliderBox.appendChild(cloneFirst);
+    sliderBox.prepend(cloneLast);
+    const itemWidth = 100;
+    const ZERO = 0;
+    let currentIdx = ZERO;
+    let index = ZERO;
+    updatePager(sliderPager, currentIdx, itemLength)
+    setInitPos(sliderBox, itemWidth);
+    setTimeout(() => {
+        sliderBox.classList.add('animate')
+    }, 0)
+
+    sliderWrapper.addEventListener('click', (e) => {
+        const target = e.target;
+        const prevBtn = sliderWrapper.querySelector('.prev');
+        const nextBtn = sliderWrapper.querySelector('.next');
+    
+        switch (target) {
+            case prevBtn:
+                index = currentIdx - 1;
+                moveSlide(sliderBox, itemWidth, index);
+                currentIdx -= 1
+                break;
+            case nextBtn:
+                index = currentIdx + 1;
+                moveSlide(sliderBox, itemWidth, index)
+                currentIdx += 1
+                break;
+            default :
+                break;
+        }
+        
+        updatePager(sliderPager, currentIdx, itemLength)
+
+        if(currentIdx === itemLength) {
+            currentIdx = ZERO
+        }else if(currentIdx < ZERO) {
+            currentIdx = itemLength - 1
+        }
+    })
+}
+
+function setInitPos(sliderBox, itemWidth) {
+    const initTranslateValue = `-${itemWidth}%` 
+    sliderBox.style.transform = `translateX(${initTranslateValue})`
+}
+
+function moveSlide(sliderBox, itemWidth, idx) {
+    const itemLength = sliderBox.querySelectorAll('.item').length - sliderBox.querySelectorAll('.clone').length;
+    const ZERO = 0;
+    const removeTime = 300;
+    const addTime = 400;
+    const translateValue = -idx * itemWidth;
+    sliderBox.style.marginLeft = `${translateValue}%`
+    
+    if(itemLength === idx) {
+        setTimeout(() => {
+            sliderBox.classList.remove('animate');
+            sliderBox.style.marginLeft = ZERO;
+        }, removeTime)
+        setTimeout(() => {
+            sliderBox.classList.add('animate');
+        }, addTime)
+    } else if(idx < ZERO) {
+        setTimeout(() => {
+            sliderBox.classList.remove('animate');
+            sliderBox.style.marginLeft = `${-itemWidth * (itemLength - 1)}%`;
+        }, removeTime)
+        setTimeout(() => {
+            sliderBox.classList.add('animate');
+        }, addTime)
+    }
+}
+
+function updatePager(pager, currentIdx, itemLength) {
+    const index = currentIdx + 1;
+    const ZERO = 0;
+    const one = 1;
+    const Last = itemLength;
+    
+    if(index > itemLength) {
+        pager.innerText = `${one} / ${itemLength}`;
+    }else if(index === ZERO) {
+        pager.innerText = `${Last} / ${itemLength}`;
+    }else {
+        pager.innerText = `${index} / ${itemLength}`;
+    }
 }
 
 (function() {
-    lnb.querySelector('li:first-child').click();
+    clickMenu();
 }())
