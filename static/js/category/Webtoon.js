75a8008fd09cbd94fd1a07295aa834c43939ff41
yunho <kimyouknow@naver.com>
2022-02-23 12:52:48 +0900

    Chore: 웹툰-서브카테고리 mock데이터 추가
    홈 데이터추가
    요일연재 데이터 추가

--- static/js/category/Webtoon.js
+++ static/js/category/Webtoon.js
@@ -4,7 +4,7 @@ import { renderMainChildDOM } from '../utils.js';
 
 export const Webtoon = async (subCategoryUid) => {
   const subCategory = renderSubCategory(subCategoryIndexArr);
-  const carousel = renderCarousel(mock_carouselElementArr);
+  const carousel = renderCarousel(mock_carouselElementArr[subCategoryUid]);
   const eventBox = `Webtoon`;
   const evenCarouel = `Webtoon`;
   const mainContent = `Webtoon`;
@@ -31,28 +31,56 @@ const subCategoryIndexArr = [
 ];
 
 const mock_carouselElementArr = [
-  {
-    title: '프론트',
-    views: 25.6,
-    genre: '웹툰',
-    subTitle: '프론트: 서브 타이틀 입니다',
-    imgUrl:
-      'https://dn-img-page.kakao.com/download/resource?kid=c0XBz/hzp2jFzCR9/JCvx5dagQ345yKhVf2m5SK',
-  },
-  {
-    title: '백앤드',
-    views: 30.6,
-    genre: '웹툰',
-    subTitle: '백앤드: 서브 타이틀 입니다',
-    imgUrl:
-      'https://dn-img-page.kakao.com/download/resource?kid=bJ0Aku/hzp2moMU6e/cEihkdBNG70ikK3gfKUK81',
-  },
-  {
-    title: '디자인',
-    views: 10.2,
-    genre: '웹툰',
-    subTitle: '디자인: 서브 타이틀 입니다',
-    imgUrl:
-      'https://dn-img-page.kakao.com/download/resource?kid=b1GVlh/hzp2nOLuZJ/qq3oH7NLIRt6ApNh6TadUK',
-  },
+  [
+    {
+      title: '웹툰-홈-프론트',
+      views: 25.6,
+      genre: '웹툰',
+      subTitle: '웹툰-홈-프론트: 서브 타이틀 입니다',
+      imgUrl:
+        'https://dn-img-page.kakao.com/download/resource?kid=c0XBz/hzp2jFzCR9/JCvx5dagQ345yKhVf2m5SK',
+    },
+    {
+      title: '웹툰-홈-백앤드',
+      views: 30.6,
+      genre: '웹툰',
+      subTitle: '웹툰-홈-백앤드: 서브 타이틀 입니다',
+      imgUrl:
+        'https://dn-img-page.kakao.com/download/resource?kid=bJ0Aku/hzp2moMU6e/cEihkdBNG70ikK3gfKUK81',
+    },
+    {
+      title: '웹툰-홈-디자인',
+      views: 10.2,
+      genre: '웹툰',
+      subTitle: '웹툰-홈-디자인: 서브 타이틀 입니다',
+      imgUrl:
+        'https://dn-img-page.kakao.com/download/resource?kid=b1GVlh/hzp2nOLuZJ/qq3oH7NLIRt6ApNh6TadUK',
+    },
+  ],
+  [
+    {
+      title: '웹툰-요일연재-프론트',
+      views: 25.6,
+      genre: '웹툰',
+      subTitle: '웹툰-요일연재-프론트: 서브 타이틀 입니다',
+      imgUrl:
+        'https://dn-img-page.kakao.com/download/resource?kid=QpMhL/hzp2iNusyJ/G1E8OHQzA7zCy0huCDQZgK',
+    },
+    {
+      title: '웹툰-요일연재-백앤드',
+      views: 30.6,
+      genre: '웹툰',
+      subTitle: '웹툰-요일연재-백앤드: 서브 타이틀 입니다',
+      imgUrl:
+        'https://dn-img-page.kakao.com/download/resource?kid=ZwJLy/hzp2mI9q0I/fyvtctMm1oabdBd1XTkAZ1',
+    },
+    {
+      title: '웹툰-요일연재-디자인',
+      views: 10.2,
+      genre: '웹툰',
+      subTitle: '웹툰-요일연재-디자인: 서브 타이틀 입니다',
+      imgUrl:
+        'https://dn-img-page.kakao.com/download/resource?kid=bBKRjp/hzp2mPR87I/ZKcDxkikUCzgNJUth3F3i0',
+    },
+  ],
 ];
