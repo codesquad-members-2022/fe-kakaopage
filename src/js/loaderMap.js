43f0ec1575db680cbfb59bec6a11d009dae177f3
Sangbeom Heo <mansaout@gmail.com>
2022-02-24 14:24:24 +0900

    Feat: "loaderMap 추가. loader.js의 함수들을 맵핑"

---
+++ src/js/loaderMap.js
@@ -0,0 +1,35 @@
+import { loader } from './loader.js';
+
+const loaderMap = {
+  웹툰: {
+    홈: loader.webtoonHome,
+    요일연재: loader.webtoonWeeky,
+    소년: loader.webtoonBoy,
+    드라마: loader.webtoonDrama,
+    로맨스: loader.webtoonRomance,
+    로판: loader.webtoonRomanceFantasy,
+    액션: loader.webtoonAction,
+    BL: loader.webtoonBl,
+  },
+  웹소설: {
+    홈: loader.webnovelHome,
+    요일연재: loader.webnovelWeeky,
+    판타지: loader.webnovelFantasy,
+    로맨스: loader.webnovelRomance,
+    로판: loader.webnovelRomanceFantasy,
+    무협: loader.webnovelWuxia,
+    판드: loader.webnovelFantasyDrama,
+    BL: loader.webnovelBL,
+  },
+  영화: {
+    홈: loader.movieHome,
+    랭킹: loader.movieRanking,
+    액션: loader.movieAction,
+    애니메이션: loader.movieAnimation,
+    장르전체: loader.movieAll,
+  },
+  방송: {},
+  책: {},
+};
+
+export { loaderMap };
