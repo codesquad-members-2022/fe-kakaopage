163ce493da5920f141f9c5cb311fc8c27fa9674e
Cola <bcad1591@gmail.com>
2022-02-24 17:58:17 +0900

    Refactor: util.js에 공통 함수 추가

--- js/pageHeaderSearchBar.js
+++
@@ -1,6 +0,0 @@
-const searchBarForm = document.querySelector('.search-bar');
-
-searchBarForm.addEventListener('submit', (event) => {
-  event.preventDefault();
-  event.target[0].value = '';
-});
--- js/util/util.js
+++ js/util/util.js
@@ -1,7 +1,21 @@
 const selector = (selector, base = document) => {
   return base.querySelector(selector);
 };
+
 const selectorAll = (selector, base = document) => {
   return base.querySelectorAll(selector);
 };
-export { selector, selectorAll };
+
+const addClass = (className, element) => {
+  element.classList.add(className);
+};
+
+const removeClass = (className, element) => {
+  element.classList.remove(className);
+};
+
+const toggleClass = (className, element) => {
+  element.classList.toggle(className);
+};
+
+export { selector, selectorAll, addClass, removeClass, toggleClass };
