b09bb0dabe8853dd7b326c38ac7621842599b32f
dony <rsefa94@gmail.com>
2022-02-25 03:12:11 +0900

    Refactor: addEventListener 내 이벤트 함수 따로 생성

--- src/eventListener.js
+++ src/eventListener.js
@@ -1,25 +1,24 @@
 import { $, activateButton } from './utility.js';
 
-const clickEvent = ({ renderPage, renderDayTop, currentPageData }) => {
+const renderAccordingGnb = (target, renderPage) => {
+    const targetIdx = target.dataset.target;
+    activateButton($('.gnb__link--active'), target, 'gnb__link--active');
+    renderPage(targetIdx);
+}
+
+const renderAccordingDayTopTap = (target, renderDayTop, currentPageData) => {
+    const currentTabIdx = target.dataset.index;
+    activateButton($('.day-top-tab__button--active'), target, 'day-top-tab__button--active');
+    renderDayTop(currentPageData, currentTabIdx);
+}
+
+export const addClickEvent = ({ renderPage, renderDayTop, currentPageData }) => {
     document.body.addEventListener('click', (e) => {
         e.preventDefault();
         const target = e.target;
         const targetClass = target.classList;
 
-        if(targetClass.contains('gnb__link')) {
-            const targetIdx = target.dataset.target;
-            activateButton($('.gnb__link--active'), target, 'gnb__link--active');
-            renderPage(targetIdx);
-        }
-        if(targetClass.contains('day-top-tab__button')) {
-            const currentTabIdx = target.dataset.index;
-            activateButton($('.day-top-tab__button--active'), target, 'day-top-tab__button--active');
-            renderDayTop(currentPageData, currentTabIdx);
-        }
-        if(targetClass.contains('day-top-tab__button')) {
-
-        }
+        if(targetClass.contains('gnb__link')) renderAccordingGnb(target, renderPage);
+        if(targetClass.contains('day-top-tab__button')) renderAccordingDayTopTap(target, renderDayTop, currentPageData);
     });
 }
-
-export { clickEvent };
