733d90862e5e6f5c6299f0e2516e24c11406d990
jm <jm.dev.study@gmail.com>
2022-02-24 18:52:13 +0900

    [fix] bannerMain.js: 불필요한 매개변수와 변수 삭제

--- JS/components/banner/bannerMain.js
+++ JS/components/banner/bannerMain.js
@@ -5,7 +5,7 @@ import {$, $all} from '../../utility.js'
 // ========== mainBanner ==========
 function renderMainBanner() {
   const main = $('.main');
-  const newDiv = makeMainBannerLayout(focus);
+  const newDiv = makeMainBannerLayout();
   main.appendChild(newDiv);
 }
 
@@ -15,14 +15,14 @@ function makeMainBannerLayout() {
   newDiv.classList.add('banner');
 
   const btns = makeBannerBtns();
-  const newList = makeMainBannerList(focus);
+  const newList = makeMainBannerList();
   const counter = makeMainBannerCounter();
   
   newDiv.appendChild(btns);
   newDiv.appendChild(newList);
   newDiv.appendChild(counter);
 
-  return newDiv
+  return newDiv;
 }
 
 function makeMainBannerList() {
@@ -34,21 +34,20 @@ function makeMainBannerList() {
 }
 
 function makeMainBannerItems(list) {
-  const data = mainBannerData;
-  data.forEach((el, index) => {
+  mainBannerData.forEach((el, index) => {
     const item = document.createElement('li');
     item.classList.add('banner__item');
     item.dataset.index = index + 1;
-    item.innerHTML = `<a href="#"><img src="${data[index].img}" alt="${data[index].img_alt}" class="banner__img"/></a>
+    item.innerHTML = `<a href="#"><img src="${mainBannerData[index].img}" alt="${mainBannerData[index].img_alt}" class="banner__img"/></a>
     <div class="banner__info">
-      <h3 class="banner__title">${data[index].title}</h3>
+      <h3 class="banner__title">${mainBannerData[index].title}</h3>
       <div class="banner__viewer">
         <p><i class="fas fa-clock"></i> 웹툰</p>
         <div class="divider"></div>
-        <p><i class="fas fa-user"></i> ${data[index].viewer}</p>
+        <p><i class="fas fa-user"></i> ${mainBannerData[index].viewer}</p>
       </div>
       <div class="banner__summary">
-        <span>${data[index].desc}</span>
+        <span>${mainBannerData[index].desc}</span>
       </div>
     </div>`
 
