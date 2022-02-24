02ea5ebfe616b05151595900df4866b25786953e
youryu0212 <youryu0212@naver.com>
2022-02-25 00:26:38 +0900

    refactor : remove duplicate ( nextImage, prevImage )Fnc

--- js/image-box-handler.js
+++ js/image-box-handler.js
@@ -8,9 +8,17 @@ const imageBoxInfo = imageBoxContainer.querySelector(".image-box__page-info");
 const TRANSLATE_RANGE = 100;
 const lastIdx =
   containerBox.childElementCount * TRANSLATE_RANGE - TRANSLATE_RANGE;
+const firstIdx = 0;
 const IMAGE_CNT = lastIdx / TRANSLATE_RANGE - 1;
 const SLIDER_INTERVAL = 2000;
 const TRANSITION_INTERVAL = 100;
+
+const resetIntervalTimer = (timer) => {
+  clearInterval(timer);
+  return setInterval(() => {
+    nextImage();
+  }, SLIDER_INTERVAL);
+};
 
 const nextImage = () => {
   let classArray = containerBox.className.split(" ");
@@ -28,10 +36,7 @@ const nextImage = () => {
   let nextImgCnt = transformRange / TRANSLATE_RANGE + 1;
   if (nextImgCnt > IMAGE_CNT) nextImgCnt = 1;
   imageBoxInfo.innerHTML = `${nextImgCnt} / ${IMAGE_CNT}`;
-  clearInterval(bannerTimer);
-  bannerTimer = setInterval(() => {
-    nextImage();
-  }, SLIDER_INTERVAL);
+  bannerTimer = resetIntervalTimer(bannerTimer);
 };
 
 const prevImage = () => {
@@ -41,7 +46,7 @@ const prevImage = () => {
     transformRange - TRANSLATE_RANGE
   } transition-ease-in-out-millisecond`;
   classArray = containerBox.className.split(" ");
-  if (classArray[1] === `translateX-${0}`) {
+  if (classArray[1] === `translateX-${firstIdx}`) {
     setTimeout(() => {
       transformRange = lastIdx - TRANSLATE_RANGE;
       containerBox.className = `image-box-container__box translateX-${transformRange}`;
@@ -50,10 +55,7 @@ const prevImage = () => {
   let prevImgCnt = transformRange / TRANSLATE_RANGE - 1;
   if (prevImgCnt === 0) prevImgCnt = IMAGE_CNT;
   imageBoxInfo.innerHTML = `${prevImgCnt} / ${IMAGE_CNT}`;
-  clearInterval(bannerTimer);
-  bannerTimer = setInterval(() => {
-    nextImage();
-  }, SLIDER_INTERVAL);
+  bannerTimer = resetIntervalTimer(bannerTimer);
 };
 
 let bannerTimer = setInterval(() => {
