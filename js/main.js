d3b5f4998a51c42427f860530ae04b380d5c67db
Ubeom Kim <kimub1204@gmail.com>
2022-02-24 10:29:26 +0900

    refactor:  수동 슬라이드 코드 수정

--- js/main.js
+++ js/main.js
@@ -13,15 +13,22 @@ import { data } from './slide-datas.js'
 import { searchSelctor } from './utility.js'
 
 document.addEventListener('DOMContentLoaded', () => {
+<<<<<<< HEAD
   const SLIDE_IMG_FIRST = 3
 
   const dayTab = searchSelctor('.serial__list')
   const slides = searchSelctor('.webtoon-slide__imgs')
   const slideEl = searchSelctor('.webtoon-slide__img')
+=======
+  const dayTab = document.querySelector('.serial__list')
+  const slides = document.querySelector('.webtoon-slide__imgs')
+  const slideEl = slides.querySelector('.webtoon-slide__img')
+>>>>>>> 3fb7482 (refactor:  수동 슬라이드 코드 수정)
 
   dayTab.addEventListener('click', toggleActiveTab)
   dayTab.addEventListener('click', toggleActiveWebtoonTab)
 
+<<<<<<< HEAD
   setSlideImgStart(slides, slideEl, data[SLIDE_IMG_FIRST])
   data.forEach((e) => {
     setSlideImg(slides, slideEl, e)
@@ -29,6 +36,15 @@ document.addEventListener('DOMContentLoaded', () => {
   slides.appendChild(slideEl.cloneNode(true))
   setSlideBtn()
   moveAutoSlide()
+=======
+  slide.setSlideImgStart(slides, slideEl, slideDatas.data[3])
+  slideDatas.data.forEach((e) => {
+    slide.setSlideImg(slides, slideEl, e)
+  })
+  slides.appendChild(slideEl.cloneNode(true))
+  slide.setSlideBtn()
+  // slide.moveAutoSlide()
+>>>>>>> 3fb7482 (refactor:  수동 슬라이드 코드 수정)
 
   setWebtoonList()
 })
