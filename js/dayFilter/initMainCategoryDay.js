d20079877c6052419a74890b6dc8fd7fc3e48b95
Cola <bcad1591@gmail.com>
2022-02-24 20:33:07 +0900

    Refactor: 리팩토링
    매직 리터럴 제거
    나머지는 순차적으로 제거할 계획

--- js/dayFilter/initMainCategoryDay.js
+++ js/dayFilter/initMainCategoryDay.js
@@ -1,10 +1,11 @@
+import { selectorAll } from '../util/util.js';
+const CATEGORY_ITEM_SELECTOR = '.page-main-category__item';
+
 const initMainCategoryDay = () => {
-  const categories = document.querySelectorAll(
-    '.page-main-category__container li'
-  );
-  categories.forEach((category) => {
-    if (!['홈', '웹툰', '요일연재'].includes(category.textContent)) return;
-    category.dataset.curday = new Date().getDay();
+  const $$category = selectorAll(CATEGORY_ITEM_SELECTOR);
+  $$category.forEach(($category) => {
+    if (!['홈', '웹툰', '요일연재'].includes($category.textContent)) return;
+    $category.dataset.curday = new Date().getDay();
   });
 };
 
