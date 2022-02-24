9e17ea17cad6b6a5497d34701e27db11452ddb25
youryu0212 <youryu0212@naver.com>
2022-02-24 14:51:53 +0900

    style : change variableName to show meaning

--- handler.js
+++ handler.js
@@ -1,8 +1,8 @@
 import { changeCirclePosition } from "./change-circle-position.js";
 // import {changeImageBoxInfo} from './change-image-box.js';
 import { changeImageCardInfo } from "./change-image-card.js";
-import { weeklyDataInfo } from "./data.js";
-import { selectedPositionChange } from "./selected-position-change.js";
+import { weeklyWebtoonDataInfo } from "./data.js";
+import { textSelectedPositionChange } from "./selected-position-change.js";
 import { $ } from "./selector.js";
 
 const weeklyWebtoonUl = $(".weekly-webtoon-ul");
@@ -15,19 +15,21 @@ const weeklyIdxChanger = {
   토: 5,
   일: 6,
 };
-
-const handler = (weeklyWebtoonUl, weekly) => {
+const weeklyWebtoonHandler = (weeklyWebtoonUl, weekly) => {
   const i = weeklyIdxChanger[weekly.innerHTML];
   // 배너 광고 추시 사용할 배너 광고 정보
-  // const boxInfo = weeklyDataInfo[i]["box"]
-  const cardInfo = weeklyDataInfo[i]["card"];
-  const dayOfTheWeek = weeklyDataInfo[i]["dayOfTheWeek"];
-  // changeImageBoxInfo : 추후 배너광고 이미지 박스 정보를 변경하고 싶을때 활용하기
-  // changeImageBoxInfo(boxInfo);
-  changeImageCardInfo(cardInfo);
+  // const bannerImageBox = weeklyWebtoonDataInfo[i]["box"]
+  const weeklyWebtoonImageCard = weeklyWebtoonDataInfo[i]["card"];
+  const dayOfTheWeek = weeklyWebtoonDataInfo[i]["dayOfTheWeek"];
+  // changeImageBoxInfo 함수 : 배너 광고에 사용되는 이미지 박스와 부가 정보 (제목, 평점 등)을 첫번째 매개변수 값을 파싱해서 변경해주는 함수
+  // 추후 배너광고 이미지 박스 정보를 변경하고 싶을때 활용할 목적
+  // changeImageBoxInfo(bannerImageBox);
+  changeImageCardInfo(weeklyWebtoonImageCard);
   changeCirclePosition(dayOfTheWeek);
-  selectedPositionChange(weeklyWebtoonUl.querySelectorAll("li"), i);
+  textSelectedPositionChange(weeklyWebtoonUl.querySelectorAll("li"), i);
 };
 weeklyWebtoonUl.addEventListener("click", (evt) => {
-  if (evt.target.nodeName === "LI") handler(weeklyWebtoonUl, evt.target);
+  if (evt.target.nodeName === "LI") {
+    weeklyWebtoonHandler(weeklyWebtoonUl, evt.target);
+  }
 });
--- selected-position-change.js
+++ selected-position-change.js
@@ -1,6 +1,6 @@
-export const selectedPositionChange = (el,idx) => {
-  for (let j = 0 ; j<el.length ; j++){
+export const textSelectedPositionChange = (el, idx) => {
+  for (let j = 0; j < el.length; j++) {
     el[j].className = "nav-box__text weekly-nav";
   }
   el[idx].className = "nav-box__text weekly-nav selected-text";
-}
+};
