8d8b5c487c92a5759742216a420fea1df050348b
Cola <bcad1591@gmail.com>
2022-02-24 17:59:00 +0900

    Refactor: Header category.js 리팩토링
    마크업 중 페이지 헤더 관련 js파일을 header하위디렉토리로 옮김
    현재 유저 idx를 나타내는 curIdx와 이 값을 변경하는 함수를 객체 리터럴로
    묶음
    util.js 사용
    매직 리터럴을 상단으로 빼서 식별자에 할당
    Element를 나타내는 변수명 앞에 $붙이기
    Element List를 나타내는 변수명 앞에 $$붙이기

---
+++ js/header/category.js
@@ -0,0 +1,63 @@
+import { selector, selectorAll, addClass, removeClass } from '../util/util.js';
+
+/* selector */
+const CATEGORY_LI_SELECTOR = '.header-category__item';
+const MAIN_PAGE_SELECTOR = '.page-main';
+const DUMMY_PAGE_SELECTOR = '.dummy';
+
+/* class name */
+const VISIBILITY_HIDDEN = 'visibility-hidden';
+const DISPLAY_NONE = 'display-none';
+const HIGHLIGHT = 'yellow-border';
+
+const categoryState = {
+  userIdx: 1,
+  mainIdx: 1,
+  getUserIdx() {
+    return this.userIdx;
+  },
+  setUserIdx(idx) {
+    this.userIdx = idx;
+  },
+  isUserOnMain() {
+    return this.userIdx === this.mainIdx;
+  },
+};
+
+const drawDummyPage = () => {
+  const $mainPage = selector(MAIN_PAGE_SELECTOR);
+  const $dummyPage = selector(DUMMY_PAGE_SELECTOR);
+  if (categoryState.isUserOnMain()) {
+    removeClass(VISIBILITY_HIDDEN, $mainPage);
+    addClass(DISPLAY_NONE, $dummyPage);
+    return;
+  }
+  removeClass(DISPLAY_NONE, $dummyPage);
+  addClass(VISIBILITY_HIDDEN, $mainPage);
+};
+
+const offHighLight = (element) => {
+  removeClass(HIGHLIGHT, element);
+};
+
+const onHighLight = (element) => {
+  addClass(HIGHLIGHT, element);
+};
+
+const $$category = selectorAll(CATEGORY_LI_SELECTOR);
+
+$$category.forEach(($category, clickedIdx, $$category) => {
+  $category.addEventListener('click', (event) => {
+    event.preventDefault();
+    const userIdx = categoryState.getUserIdx();
+    if (userIdx === clickedIdx) return;
+    const $prevCategory = $$category[userIdx];
+    console.log(event.target);
+    const $curCategory = event.target.closest(CATEGORY_LI_SELECTOR);
+    categoryState.setUserIdx(clickedIdx);
+
+    offHighLight($prevCategory);
+    onHighLight($curCategory);
+    drawDummyPage(categoryState.getUserIdx());
+  });
+});
