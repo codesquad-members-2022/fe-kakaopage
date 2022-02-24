c6d0492918fb60df487f7dd436bc494c7f044f3e
Cola <bcad1591@gmail.com>
2022-02-24 14:01:18 +0900

    Refactor: 템플릿 변경

--- js/preview/createPreviewEl.js
+++ js/preview/createPreviewEl.js
@@ -14,23 +14,32 @@ const createPreviewEl = (preview) => {
     </div>
 
     <div class="preview-detail">
-      <span class="preview-state">
-        <img src="./assets/data/state/${preview.state}.svg" />
-      </span>
-
+    ${
+      preview.state
+        ? ` <span class="preview-state">
+    <img src="./assets/data/state/${preview.state}.svg" />
+  </span>`
+        : ''
+    }
       <div class="preview-class">
       ${
         isWaitFree
           ? `<img src="./assets/data/state/waitfree.svg" alt="waitfree.svg"/>`
           : ''
-      }<span>${preview.class}</span>
+      }
+      ${preview.class ? `<span>${preview.class}</span>` : ''}
       </div>
-      <span class="seperator">
-        <img src="assets/static/Seperator.png" alt="seperator"></span>
-      <div class="preview-subscribers">
-        <img src="./assets/static/subscriber.svg" alt="ico-bigthum-person">
-        <span>${(preview.subscriber / 10000).toFixed(1)}만명</span>
-      </div>
+      ${
+        preview.subscriber
+          ? `      <span class="seperator">
+      <img src="assets/static/Seperator.png" alt="seperator"></span>
+    <div class="preview-subscribers">
+      <img src="./assets/static/subscriber.svg" alt="ico-bigthum-person">
+      <span>${(preview.subscriber / 10000).toFixed(1)}만명</span>
+    </div>`
+          : ''
+      }
+
     </div>
   </div>
   <div class="preview-summary">
