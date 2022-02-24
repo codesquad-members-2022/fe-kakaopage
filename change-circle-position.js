ab68bae9d8c9269a399b47695621f71355811a56
youryu0212 <youryu0212@naver.com>
2022-02-24 14:22:47 +0900

    style : rename className from main to webtoon-weekly to have meaning

--- change-circle-position.js
+++ change-circle-position.js
@@ -1,8 +1,8 @@
-import { $ } from './selector.js';
+import { $ } from "./selector.js";
 export const changeCirclePosition = (dayOfTheWeek) => {
   const circle = $(".circle");
-  const newClassName = `circle main-content__nav__circile-${dayOfTheWeek}`;
+  const newClassName = `circle webtoon-weekly-content__nav__circile-${dayOfTheWeek}`;
   circle.className = newClassName;
   // console.log(dayOfTheWeek);
   // console.log(circle,circle.className);
-}
+};
