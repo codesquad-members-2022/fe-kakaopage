7d95c89f3c233018f0fbd2b802d21c882da531d4
jm <jm.dev.study@gmail.com>
2022-02-24 19:16:54 +0900

    [fix] moveApp.js: 사용하지 않는 매개변수 삭제 - 매개변수로 값을 대신 고정된 값으로 변경했다.

--- JS/components/moveApp.js
+++ JS/components/moveApp.js
@@ -1,21 +1,22 @@
 import {$, $all} from '../utility.js'
 
 // ========== makeMoveApp ==========
-function renderMoveApp(selector) {
+function renderMoveApp() {
   const main = $('.main');
-  const newList = makeMoveApp(selector);
+  const newList = makeMoveApp();
   main.appendChild(newList);
 
   return newList;
 }
 
-function makeMoveApp(selector) {
+function makeMoveApp() {
   const newList = document.createElement("div");
-  newList.classList.add(selector);
-  newList.innerHTML = `<a href="#">
-  <span><strong>코코아페이지</strong> 앱으로 보기</span>
-  <i class="fas fa-chevron-right"></i>
-</a>`;
+  newList.classList.add('move-app');
+  newList.innerHTML = `
+  <a href="#">
+    <span><strong>코코아페이지</strong> 앱으로 보기</span>
+    <i class="fas fa-chevron-right"></i>
+  </a>`;
 
   return newList;
 }
