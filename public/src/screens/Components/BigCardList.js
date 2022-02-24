6c7f6c1142c9c3e29057532d96f06f976da41150
healtheloper <58503584+healtheloper@users.noreply.github.com>
2022-02-24 23:58:05 +0900

    style: main banner style

--- public/src/screens/Components/BigCardList.js
+++ public/src/screens/Components/BigCardList.js
@@ -1,22 +1,22 @@
 import Component from "../Component.js";
 import { createExtendsRelation, getComponentsTemplate } from "../../utils.js";
-
-import BigCard from "./BigCard.js";
+import BannerImage from "./BannerImage.js";
 
 function BigCardList() {
   const webtoons = JSON.parse(localStorage.getItem("webtoons"));
   const wtForBigCard = webtoons.filter(
     (wt) => wt.status === "N" && wt.imageHorizontalUrl
   );
-  const bigCards = wtForBigCard
+  const bigCardsTemplates = wtForBigCard
     .sort((c1, c2) => c2.rank - c1.rank)
     .slice(0, 2)
-    .map((bigCardInfo) => new BigCard("_", bigCardInfo));
+    .map((bigCardInfo) => new BannerImage("_", bigCardInfo))
+    .map((bannerImage) => `<li class="bigCard">${bannerImage.template()}</li>`);
 
   this.template = function () {
     return `
       <ul class="contentsBigCard">
-        ${getComponentsTemplate(bigCards)}
+        ${bigCardsTemplates.join("")}
       </ul>`;
   };
 }
