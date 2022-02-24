0943a6c6fb4288700b9005bfb0157ae9c1b25229
Sangbeom Heo <mansaout@gmail.com>
2022-02-24 14:32:53 +0900

    Refactor: "기존 if문을 분기했던 부분을 loaderMap으로 객체 맵핑해 불러옴"

--- src/js/navHandler.js
+++ src/js/navHandler.js
@@ -1,5 +1,6 @@
 import { renderer } from './renderer.js';
 import { loader } from './loader.js';
+import { loaderMap } from './loaderMap.js';
 import {
   DATA_WEBTOON,
   DATA_WEBNOVEL,
@@ -81,74 +82,9 @@ const loadContentFromSnb = (subCategory) => {
   updateNavStyle('snb', subCategory);
 
   const gnbLabel = document.querySelector('.gnb__item--active').dataset.category;
-  if (gnbLabel === '웹툰') loadWebtoon(subCategory);
-  if (gnbLabel === '웹소설') loadWebnovel(subCategory);
-  if (gnbLabel === '영화') loadMovie(subCategory);
-  if (gnbLabel === '방송') loadBroadcast(subCategory);
-  if (gnbLabel === '책') loadBook(subCategory);
-};
 
-const loadWebtoon = (subCategory) => {
-  const DATA = DATA_WEBTOON[subCategory];
-  updateDocumentTitle(`${DATA_WEBTOON.category[subCategory]} - 웹툰`);
-
-  if (subCategory === '0') loader.webtoonHome(DATA);
-  if (subCategory === '1') loader.webtoonWeeky(DATA);
-  if (subCategory === '2') loader.webtoonBoy(DATA);
-  if (subCategory === '3') loader.webtoonDrama(DATA);
-  if (subCategory === '4') loader.webtoonRomance(DATA);
-  if (subCategory === '5') loader.webtoonRomanceFantasy(DATA);
-  if (subCategory === '6') loader.webtoonAction(DATA);
-  if (subCategory === '7') loader.webtoonBl(DATA);
-};
-
-const loadWebnovel = (subCategory) => {
-  const DATA = DATA_WEBNOVEL[subCategory];
-  updateDocumentTitle(`${DATA_WEBNOVEL.category[subCategory]} - 웹소설`);
-
-  if (subCategory === '0') loader.webnovelHome(DATA);
-  if (subCategory === '1') loader.webnovelWeeky(DATA);
-  if (subCategory === '2') loader.webnovelFantasy(DATA);
-  if (subCategory === '3') loader.webnovelRomance(DATA);
-  if (subCategory === '4') loader.webnovelRomanceFantasy(DATA);
-  if (subCategory === '5') loader.webnovelWuxia(DATA);
-  if (subCategory === '6') loader.webnovelFantasyDrama(DATA);
-  if (subCategory === '7') loader.webnovelBL(DATA);
-};
-
-const loadMovie = (subCategory) => {
-  const DATA = DATA_MOVIE[subCategory];
-  updateDocumentTitle(`${DATA_MOVIE.category[subCategory]} - 영화`);
-
-  if (subCategory === '0') loader.movieHome(DATA);
-  if (subCategory === '1') loader.movieRanking(DATA);
-  if (subCategory === '2') loader.movieAction(DATA);
-  if (subCategory === '3') loader.movieAnimation(DATA);
-  if (subCategory === '4') loader.movieAll(DATA);
-};
-
-const loadBroadcast = (subCategory) => {
-  const DATA = DATA_BROADCAST[subCategory];
-  updateDocumentTitle(`${DATA_BROADCAST.category[subCategory]} - 방송`);
-
-  if (subCategory === '0') loader.broadcastHome(DATA);
-  if (subCategory === '1') loader.broadcastVarietyshow(DATA);
-  if (subCategory === '2') loader.broadcastDrama(DATA);
-  if (subCategory === '3') loader.broadcastAnimation(DATA);
-  if (subCategory === '4') loader.broadcastForeign(DATA);
-  if (subCategory === '5') loader.broadcastAll(DATA);
-};
-
-const loadBook = (subCategory) => {
-  const DATA = DATA_BOOK[subCategory];
-  updateDocumentTitle(`${DATA_BOOK.category[subCategory]} - 책`);
-
-  if (subCategory === '0') loader.bookHome(DATA);
-  if (subCategory === '1') loader.bookSeries(DATA);
-  if (subCategory === '2') loader.bookBestseller(DATA);
-  if (subCategory === '3') loader.bookRanking(DATA);
-  if (subCategory === '4') loader.bookThriller(DATA);
-  if (subCategory === '6') loader.bookSelfimprovement(DATA);
+  const DATA = getData(gnbLabel)[subCategory];
+  loaderMap[gnbLabel][subCategory](DATA);
 };
 
 export { addHandlerOnNav, loadContentFromGnb };
