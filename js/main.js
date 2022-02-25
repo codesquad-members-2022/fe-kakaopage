f396c4d8c1912126b553bdffc6a711b5a15b60e9
Ubeom Kim <kimub1204@gmail.com>
2022-02-25 10:51:40 +0900

    docs: README 수정

--- js/main.js
+++ js/main.js
@@ -13,6 +13,8 @@ import { data } from './slide-datas.js'
 import { searchSelctor } from './utility.js'
 
 document.addEventListener('DOMContentLoaded', () => {
+  const SLIDE_IMG_FIRST = 3
+
   const dayTab = searchSelctor('.serial__list')
   const slides = searchSelctor('.webtoon-slide__imgs')
   const slideEl = searchSelctor('.webtoon-slide__img')
@@ -20,7 +22,7 @@ document.addEventListener('DOMContentLoaded', () => {
   dayTab.addEventListener('click', toggleActiveTab)
   dayTab.addEventListener('click', toggleActiveWebtoonTab)
 
-  setSlideImgStart(slides, slideEl, data[3])
+  setSlideImgStart(slides, slideEl, data[SLIDE_IMG_FIRST])
   data.forEach((e) => {
     setSlideImg(slides, slideEl, e)
   })
