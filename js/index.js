4b1d96a2bdc0fa2fbc9d9f3894ee73cff9bd872a
sju02048 <sju02048@naver.com>
2022-02-25 18:30:46 +0900

    추가: 이벤트 리스너 즉시실행함수

--- js/index.js
+++ js/index.js
@@ -1,7 +1,23 @@
-import Header from "./header.js";
-import Main from "./main.js";
-import Footer from "./footer.js";
+import { $ } from "./dom-lib.js";
+import { Render } from "./render.js";
 
-new Header();
-new Main();
-new Footer();
+Render.fixedHeader();
+Render.header_toon();
+Render.toon_main();
+Render.footerContent();
+
+(function addEvent() {
+  $("body").addEventListener("click", ({ target }) => {
+    let action = target.dataset.action;
+
+    if (!action) {
+      action = target.parentNode.dataset.action;
+      target = target.parentNode;
+    }
+
+    //? render 클래스에 함수가 없을 때 오류
+    if (action) {
+      Render[action](target);
+    }
+  });
+})();
