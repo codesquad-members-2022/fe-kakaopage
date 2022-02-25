143af0b68e37ddf28caf68f6732a734621c527cb
bangdler <zbthz90@gmail.com>
2022-02-25 14:37:10 +0900

    update: contents 무한클릭 구현
    컨텐츠 클릭 시 transition, transform 을 통해 무한 이동

--- js/webtoon_contents.js
+++ js/webtoon_contents.js
@@ -4,10 +4,11 @@ import { contentsData } from "./webtoonData.js";
 
 export function initContents() {
     createContents();
+    createCloneNode();
     listenEvent();
 }
 
-export function createContents() {
+function createContents() {
     const $contents = document.createElement('div');
     $contents.className = 'contents';
     $contents.innerHTML = `<div class="contents__viewer"></div>
@@ -21,53 +22,77 @@ export function createContents() {
     contentsData.forEach((content) => $contentsViewer.innerHTML += getContentsTemplate(content))
 }
 
-function listenEvent() {
-    const $prevButton = select('.contents__prevButton');
-    const $nextButton = select('.contents__nextButton');
-    const $viewer = select('.contents__viewer')
+function createCloneNode() {
+    const $contentsViewer = select('.contents__viewer')
+    const $cloneFirstNode = $contentsViewer.firstElementChild.cloneNode(true);
+    $cloneFirstNode.id = 'firstClone'
+    const $cloneLastNode = $contentsViewer.lastElementChild.cloneNode(true);
+    $cloneLastNode.id = 'lastClone'
 
-    $prevButton.addEventListener('click', (e) => handleClick(e.target, $viewer));
-    $nextButton.addEventListener('click', (e) => handleClick(e.target, $viewer));
-    autoSlide($viewer)
+    $contentsViewer.prepend($cloneLastNode);
+    $contentsViewer.appendChild($cloneFirstNode)
+
+}
+
+function listenEvent() {
+    const $contents = select('.contents');
+    const $viewer = select('.contents__viewer')
+    animateTranslateX($viewer)
+    $contents.addEventListener('click', (e) => handleClick(e.target, $viewer));
+    //autoSlide($viewer)
 }
 
 // 전역으로 해야만 가능..?
-let current = 0;
-const imageWidth = 647.7
+let index = 1
 
 function handleClick(target, $viewer) {
-    if(target.className === 'contents__prevButton' ) {
-        slideLeft($viewer);
+    const $$contentsWrap = [...$viewer.children];
+    const maxIndex = $$contentsWrap.length - 1 // first clone node index
+    const minIndex = 0 // last clone node index
+    const realLastIndex = $$contentsWrap.length -2;
+    const realFirstIndex = 1;
+
+    if(target.className === 'contents__prevButton') {
+        if(index <= minIndex) {
+            index = realLastIndex
+            animateReset($viewer, index)
+        }
+        index --
+        animateTranslateX($viewer)
+
     }
     else if(target.className === 'contents__nextButton') {
-        slideRight($viewer);
+        if(index >= maxIndex) {
+            index = realFirstIndex
+            animateReset($viewer, index)
+        }
+        index ++
+        animateTranslateX($viewer)
+
     }
 }
 
-function slideLeft(target) {
-    current -= imageWidth
-    animateTranslateX(target, current)
-}
-
-function slideRight(target) {
-    current += imageWidth
-    animateTranslateX(target, current)
-}
-
-function animateTranslateX(target, distance) {
-    target.style.transform = `translateX(${distance}px)`
+function animateTranslateX(target)  {
+    const width = target.clientWidth * -1;
+    target.style.transform = `translateX(${index * width}px)`
+    //target.style.transition = 'transform 2s ease-out'
     target.style.transitionProperty = `transform`
     target.style.transitionDuration = `2s`
+}
+
+function animateReset(target, index) {
+    const width = target.clientWidth * -1;
+    target.style.transition = 'none';
+    target.style.transform = `translateX(${index * width}px)`
 }
 
 function autoSlide(target) {
     let count = 1;
     let setTimer = setInterval(function() {
-        console.log(count)
         slideLeft(target)
         count += 1;
         if(count > 2) {
-            return clearTimer(setTimer)
+            clearTimer(setTimer)
         }
         }, 3000)
 }
@@ -76,7 +101,7 @@ function clearTimer(timer) {
     clearInterval(timer)
 }
 
-function getContentsTemplate(object) {
+function getContentsTemplate(object, index) {
     let template = ``;
     template += `<div class="contents__wrap">
                     <img class="contents__img" src=${object.src} alt=${object.title}>
@@ -86,7 +111,7 @@ function getContentsTemplate(object) {
                             <div class="text__event">이벤트</div>
                             <span class="text__info-style">웹툰</span>
                             <span class="text__info-style">|</span>
-                            <span class="text__info-style">${object.views}</span>
+                            <span class="text__info-style">${object.views}만명</span>
                         </div>
                     </div>
                     <div class="contents__sentence">
