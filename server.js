6c60bff3c80101182ce15c9ffa1e96c8a9d78927
Millie <jay.lee.2280@gmail.com>
2022-02-25 11:30:42 +0900

    Add express server

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const cors = require('cors');
+const data = require('./data/data.json');
+
+const app = express();
+const PORT = 3001;
+
+app.use(cors());
+
+app.get('/data', (req, res) => {
+  res.send(data);
+});
+
+app.use(express.static('src'));
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/express.html');
+});
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}`);
+});
