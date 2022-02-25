c4eca653bc61a0ed87ae98a64867090c021bb79b
nanjeong <whdwkxj7@naver.com>
2022-02-25 11:24:43 +0900

    feat: 추천 이벤트 데이터 추가, 추천 이벤트 블록에 이벤트 이미지 여러 개 넣어두기

---
+++ public/data/home/event.js
@@ -0,0 +1,24 @@
+const event = [
+  {
+    img: "https://dn-img-page.kakao.com/download/resource?kid=DHuH7/hzp2gCacSm/CxKcdmOaKcmNKqULsxPtm0",
+    desc: "재능 삼킨 마법사 게임 출시 기념, 경품에 캐시까지!",
+  },
+  {
+    img: "https://dn-img-page.kakao.com/download/resource?kid=uK5UK/hzmU21KjTA/SvF1U2sJveCP7Sy2EmiJoK",
+    desc: "어게인 마이 라이프 기간한정, 한시한편+경품 이벤트!",
+  },
+  {
+    img: "https://dn-img-page.kakao.com/download/resource?kid=uVime/hzp2mvCsem/le2NuLTWJkK1kdmYkqhAak",
+    desc: "킹덤 '기간한정' 삼시세편+캐시 이벤트!",
+  },
+  {
+    img: "https://dn-img-page.kakao.com/download/resource?kid=cFRNYY/hzmU3fkAbK/jAeQIOY1xG5v8mKw8035eK",
+    desc: "생사결: 여래의 복수 완결 기념 연참&한시한편, 캐시!",
+  },
+  {
+    img: "https://dn-img-page.kakao.com/download/resource?kid=FIep1/hzmU3sGhNi/ltEgI1VyEMT2RwQZYdXhqk",
+    desc: "왕실교사 하이네 기다무 런칭 기념, 경품 이벤트!",
+  },
+];
+
+export { event as dataOfEvent };
