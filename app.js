dc0276a4b5d3798bc5453b7e2bca9b1a6bc8ea7c
dlwnstjrzz <95525638+dlwnstjrzz@users.noreply.github.com>
2022-02-25 12:28:35 +0900

    REFACTOR: template 확장묘듈인 handlebars를 활용하기 위해 디렉토리 구조변경-서버에서 받아오는 데이터를 미들웨어를 통해 렌더하는 방식으로 변경

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import path from 'path';
-// import { hbs } from "express-handlebars"; 
+import pkg from 'express-handlebars';
+const { hbs } = pkg; 
 import { engine } from 'express-handlebars';
 const server = express();
 const __dirname = path.resolve();
@@ -9,18 +10,24 @@ const __dirname = path.resolve();
 server.engine("hbs", engine({
     extname: "hbs",
     defaultLayout: "layout.hbs",
-    partialsDir: "partials",
+  partialsDir: [
+    "partials",
+    path.join(__dirname, 'views/partials'),
+  ]
   })
 );
 server.set("view engine", "hbs");
 server.use(express.static(__dirname + "/public"));
 
 
-server.get("/", (req, res) => {
+
+server.get("/main", (req, res) => {
   res.render("home");
   // res.sendFile(__dirname + "/index.html");
   
 });
+
+// server.get("/main/")
 
 //GET get 메소드로 서버에 요청이 왔을 경우 어떤 걸 보내줄건지 결정
 server.listen(3000, (err) => {
