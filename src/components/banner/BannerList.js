e78a55068898b1a27c558f9c77ffd4077f567065
mina-gwak <mina.gwak@kakao.com>
2022-02-25 11:09:43 +0900

    feat: Add data attribute to banner item

--- src/components/banner/BannerList.js
+++ src/components/banner/BannerList.js
@@ -3,10 +3,10 @@ import { BannerItem, SubBannerItem, EventBannerItem } from '../index.js';
 const BannerList = (list, bannerList) => {
 
   return `<ul class="banner-container">
-            ${bannerList.reduce((prev, cur) => {
-              if (list === 'main') prev += BannerItem(cur);
-              else if (list === 'sub') prev += SubBannerItem(cur);
-              else prev += EventBannerItem(cur);
+            ${bannerList.reduce((prev, cur, idx) => {
+              if (list === 'main') prev += BannerItem(idx + 1, cur);
+              else if (list === 'sub') prev += SubBannerItem(idx + 1, cur);
+              else prev += EventBannerItem(idx + 1, cur);
               return prev;
             }, '')}
           </ul>`;
--- src/components/banner/EventBannerItem.js
+++ src/components/banner/EventBannerItem.js
@@ -1,5 +1,5 @@
-const EventBannerItem = ({ title, bannerImage, link }) => {
-  return `<li class="banner">
+const EventBannerItem = (idx, { title, bannerImage, link }) => {
+  return `<li class="banner" data-count="${idx}">
             <a href="${link}">
               <img src="${bannerImage}" alt="${title}" />
             </a>
