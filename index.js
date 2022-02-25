c7d976253cb2727c626b69836389ce4757e23bb8
Serin Kim <68533016+Serin-Kim@users.noreply.github.com>
2022-02-25 15:40:13 +0900

    [Maeve] Step4 (#125)
    * Style: 요일 클릭 시 글씨 색 변화, 노란 줄 추가
    
    * Refactor: html, css 이름 변경
    
    KAKAO.html에서 index.html 로 변경
    KAKAOstyle.html에서 style.css로 변경
    
    * Feat: 웹 슬라이딩 애니메이션 구현
    
    버튼 클릭 시 다음 이미지로 슬라이딩
    
    * Refactor: PR 리뷰 관련 사항 수정
    
    * Feat: 웹 슬라이딩 애니메이션
    
    무한 슬라이딩 구현 중
    
    * Docs: README.md 업데이트

--- index.js
+++ index.js
@@ -1,159 +1,212 @@
 const tabContent = [
   `<div class="tab_content">
-    <div class="main__dailyRank__content__row">
-        <div class="content__info">
-
-            <div class="imageBox">
-                <img src="./img/qaZBIDe7wC2le5ROw3q9A0&filename_th2.png" />
-                <div class="rank yellow">1위 </div>
+        <div class="main__dailyRank__content__row">
+            <div class="content__info">
+    
+                <div class="imageBox">
+                    <img src="./img/qaZBIDe7wC2le5ROw3q9A0&filename_th2.png" />
+                    <div class="rank yellow">1위 </div>
+                </div>
+    
+                <div class="content__info__title">인소의 법칙</div>
+                <div class="content__info__view">142만명</div>
             </div>
-
-            <div class="content__info__title">인소의 법칙</div>
-            <div class="content__info__view">142만명</div>
-        </div>
-        <div class="content__info">
-            <div class="imageBox">
-                <img src="./img/QKoyuFgYDkf53tKkWcm2yk&filename_th2.png" />
-                <div class="rank yellow"> 2위</div>
+            <div class="content__info">
+                <div class="imageBox">
+                    <img src="./img/QKoyuFgYDkf53tKkWcm2yk&filename_th2.png" />
+                    <div class="rank yellow"> 2위</div>
+                </div>
+    
+                <div class="content__info__title">정령왕의 딸</div>
+                <div class="content__info__view">75만명</div>
             </div>
-
-            <div class="content__info__title">정령왕의 딸</div>
-            <div class="content__info__view">75만명</div>
-        </div>
-        <div class="content__info">
-            <div class="imageBox">
-                <img src="./img/TnEdnMTNLrPgSehcKJIOek&filename_th2.png" />
-                <div class="rank yellow"> 3위</div>
+            <div class="content__info">
+                <div class="imageBox">
+                    <img src="./img/TnEdnMTNLrPgSehcKJIOek&filename_th2.png" />
+                    <div class="rank yellow"> 3위</div>
+                </div>
+                <div class="content__info__title">태양의 주인</div>
+                <div class="content__info__view">6.1만명</div>
             </div>
-            <div class="content__info__title">태양의 주인</div>
-            <div class="content__info__view">6.1만명</div>
-        </div>
-        <div class="content__info">
-            <div class="imageBox">
-                <img src="./img/YoDlK2IoUrelLh02Ria3f1&filename_th2.png" />
-                <div class="rank yellow"> 4위</div>
+            <div class="content__info">
+                <div class="imageBox">
+                    <img src="./img/YoDlK2IoUrelLh02Ria3f1&filename_th2.png" />
+                    <div class="rank yellow"> 4위</div>
+                </div>
+                <div class="content__info__title">FFF급 관심용사</div>
+                <div class="content__info__view">2.2만명</div>
             </div>
-            <div class="content__info__title">FFF급 관심용사</div>
-            <div class="content__info__view">2.2만명</div>
-        </div>
-        <div class="content__info">
-            <div class="imageBox">
-                <img src="./img/YRYAYBF6WZ2qQNH4IBafw0&filename_th2.png" />
-                <div class="rank yellow"> 5위</div>
+            <div class="content__info">
+                <div class="imageBox">
+                    <img src="./img/YRYAYBF6WZ2qQNH4IBafw0&filename_th2.png" />
+                    <div class="rank yellow"> 5위</div>
+                </div>
+                <div class="content__info__title">닥터 최태수</div>
+                <div class="content__info__view">1만명</div>
             </div>
-            <div class="content__info__title">닥터 최태수</div>
-            <div class="content__info__view">1만명</div>
         </div>
-    </div>
-    <div class="main__dailyRank__content__row">
-        <div class="content__info">
-            <div class="imageBox">
-                <img src="./img/XVzRKIJCdlg4slfZFnTKnK&filename_th2.png" />
-                <div class="rank yellow"> 9.9</div>
+        <div class="main__dailyRank__content__row">
+            <div class="content__info">
+                <div class="imageBox">
+                    <img src="./img/XVzRKIJCdlg4slfZFnTKnK&filename_th2.png" />
+                    <div class="rank yellow"> 9.9</div>
+                </div>
+                <div class="content__info__title">인소의 법칙</div>
+                <div class="content__info__view">142만명</div>
             </div>
-            <div class="content__info__title">인소의 법칙</div>
-            <div class="content__info__view">142만명</div>
-        </div>
-        <div class="content__info">
-            <div class="imageBox">
-                <img src="./img/xqCOyfM19jcgdBbUkflNKK&filename_th2.png" />
-                <div class="rank yellow"> 7.0</div>
+            <div class="content__info">
+                <div class="imageBox">
+                    <img src="./img/xqCOyfM19jcgdBbUkflNKK&filename_th2.png" />
+                    <div class="rank yellow"> 7.0</div>
+                </div>
+                <div class="content__info__title">정령왕의 딸</div>
+                <div class="content__info__view">75만명</div>
             </div>
-            <div class="content__info__title">정령왕의 딸</div>
-            <div class="content__info__view">75만명</div>
-        </div>
-        <div class="content__info">
-            <div class="imageBox">
-                <img src="./img/W0v5wU5mb8boVWJLghJuW0&filename_th2.png" />
-                <div class="rank yellow">3.4</div>
+            <div class="content__info">
+                <div class="imageBox">
+                    <img src="./img/W0v5wU5mb8boVWJLghJuW0&filename_th2.png" />
+                    <div class="rank yellow">3.4</div>
+                </div>
+                <div class="content__info__title">태양의 주인</div>
+                <div class="content__info__view">6.1만명</div>
             </div>
-            <div class="content__info__title">태양의 주인</div>
-            <div class="content__info__view">6.1만명</div>
-        </div>
-        <div class="content__info">
-            <div class="imageBox">
-                <img src="./img/u3m5gW9VYP1mP6xNTTAafk&filename_th2.png" />
-                <div class="rank yellow"> 8.7</div>
+            <div class="content__info">
+                <div class="imageBox">
+                    <img src="./img/u3m5gW9VYP1mP6xNTTAafk&filename_th2.png" />
+                    <div class="rank yellow"> 8.7</div>
+                </div>
+                <div class="content__info__title">FFF급 관심용사</div>
+                <div class="content__info__view">2.2만명</div>
             </div>
-            <div class="content__info__title">FFF급 관심용사</div>
-            <div class="content__info__view">2.2만명</div>
-        </div>
-        <div class="content__info">
-            <div class="imageBox">
-                <img src="./img/Spy2JkLBLu3zUwNl9TaznK&filename_th2.png" />
-                <div class="rank yellow"> 3.9</div>
+            <div class="content__info">
+                <div class="imageBox">
+                    <img src="./img/Spy2JkLBLu3zUwNl9TaznK&filename_th2.png" />
+                    <div class="rank yellow"> 3.9</div>
+                </div>
+                <div class="content__info__title">닥터 최태수</div>
+                <div class="content__info__view">1만명</div>
             </div>
-            <div class="content__info__title">닥터 최태수</div>
-            <div class="content__info__view">1만명</div>
         </div>
-    </div>
-</div>`,
+    </div>`,
   `                    <div class="tab_content">
-  <div class="main__dailyRank__content__row">
-      <div class="content__info">
-          <img src="./img/jkmHH89mEUplIjpyfuZ4e1&filename_th2.png" />
-          <div class=" content__info__title">인소의 법칙
+      <div class="main__dailyRank__content__row">
+          <div class="content__info">
+              <img src="./img/jkmHH89mEUplIjpyfuZ4e1&filename_th2.png" />
+              <div class=" content__info__title">인소의 법칙
+              </div>
+              <div class="content__info__view">142만명</div>
           </div>
-          <div class="content__info__view">142만명</div>
+          <div class="content__info">
+              <img src="./img/jkmHH89mEUplIjpyfuZ4e1&filename_th2.png" />
+              <div class="content__info__title">정령왕의 딸</div>
+              <div class="content__info__view">75만명</div>
+          </div>
+          <div class="content__info">
+              <img src="./img/jkmHH89mEUplIjpyfuZ4e1&filename_th2.png" />
+              <div class="content__info__title">태양의 주인</div>
+              <div class="content__info__view">6.1만명</div>
+          </div>
+          <div class="content__info">
+              <img src="./img/jkmHH89mEUplIjpyfuZ4e1&filename_th2.png" />
+              <div class="content__info__title">FFF급 관심용사</div>
+              <div class="content__info__view">2.2만명</div>
+          </div>
+          <div class="content__info">
+              <img src="./img/jkmHH89mEUplIjpyfuZ4e1&filename_th2.png" />
+              <div class="content__info__title">닥터 최태수</div>
+              <div class="content__info__view">1만명</div>
+          </div>
       </div>
-      <div class="content__info">
-          <img src="./img/jkmHH89mEUplIjpyfuZ4e1&filename_th2.png" />
-          <div class="content__info__title">정령왕의 딸</div>
-          <div class="content__info__view">75만명</div>
+      <div class="main__dailyRank__content__row">
+          <div class="content__info">
+              <img src="./img/IAy6e8p17VpMjWaIfr3bkK&filename_th2.png" />
+              <div class="content__info__title">인소의 법칙</div>
+              <div class="content__info__view">142만명</div>
+          </div>
+          <div class="content__info">
+              <img src="./img/IAy6e8p17VpMjWaIfr3bkK&filename_th2.png" />
+              <div class="content__info__title">정령왕의 딸</div>
+              <div class="content__info__view">75만명</div>
+          </div>
+          <div class="content__info">
+              <img src="./img/IAy6e8p17VpMjWaIfr3bkK&filename_th2.png" />
+              <div class="content__info__title">태양의 주인</div>
+              <div class="content__info__view">6.1만명</div>
+          </div>
+          <div class="content__info">
+              <img src="./img/IAy6e8p17VpMjWaIfr3bkK&filename_th2.png" />
+              <div class="content__info__title">FFF급 관심용사</div>
+              <div class="content__info__view">2.2만명</div>
+          </div>
+          <div class="content__info">
+              <img src="./img/IAy6e8p17VpMjWaIfr3bkK&filename_th2.png" />
+              <div class="content__info__title">닥터 최태수</div>
+              <div class="content__info__view">1만명</div>
+          </div>
       </div>
-      <div class="content__info">
-          <img src="./img/jkmHH89mEUplIjpyfuZ4e1&filename_th2.png" />
-          <div class="content__info__title">태양의 주인</div>
-          <div class="content__info__view">6.1만명</div>
-      </div>
-      <div class="content__info">
-          <img src="./img/jkmHH89mEUplIjpyfuZ4e1&filename_th2.png" />
-          <div class="content__info__title">FFF급 관심용사</div>
-          <div class="content__info__view">2.2만명</div>
-      </div>
-      <div class="content__info">
-          <img src="./img/jkmHH89mEUplIjpyfuZ4e1&filename_th2.png" />
-          <div class="content__info__title">닥터 최태수</div>
-          <div class="content__info__view">1만명</div>
-      </div>
-  </div>
-  <div class="main__dailyRank__content__row">
-      <div class="content__info">
-          <img src="./img/IAy6e8p17VpMjWaIfr3bkK&filename_th2.png" />
-          <div class="content__info__title">인소의 법칙</div>
-          <div class="content__info__view">142만명</div>
-      </div>
-      <div class="content__info">
-          <img src="./img/IAy6e8p17VpMjWaIfr3bkK&filename_th2.png" />
-          <div class="content__info__title">정령왕의 딸</div>
-          <div class="content__info__view">75만명</div>
-      </div>
-      <div class="content__info">
-          <img src="./img/IAy6e8p17VpMjWaIfr3bkK&filename_th2.png" />
-          <div class="content__info__title">태양의 주인</div>
-          <div class="content__info__view">6.1만명</div>
-      </div>
-      <div class="content__info">
-          <img src="./img/IAy6e8p17VpMjWaIfr3bkK&filename_th2.png" />
-          <div class="content__info__title">FFF급 관심용사</div>
-          <div class="content__info__view">2.2만명</div>
-      </div>
-      <div class="content__info">
-          <img src="./img/IAy6e8p17VpMjWaIfr3bkK&filename_th2.png" />
-          <div class="content__info__title">닥터 최태수</div>
-          <div class="content__info__view">1만명</div>
-      </div>
-  </div>
-</div>`,
+    </div>`,
 ];
 
 const rankBox = document.querySelector(".main__dailyRank__content");
-const tab = document.querySelector(".main__dailyRank__day");
-tab.addEventListener("click", (evt) => {
-  let tabNum = evt.target.getAttribute("data-tabnum");
-  changeTab(rankBox, tabNum);
+const dailyRank_tab = document.querySelector(".main__dailyRank__day");
+
+dailyRank_tab.addEventListener("click", (evt) => {
+  const target = evt.target;
+  const tabNum = target.getAttribute("data-tabnum");
+  changeTab(target, rankBox, tabNum);
 });
 
-const changeTab = (rankBox, tabNum) => {
+const changeTab = (target, rankBox, tabNum) => {
   rankBox.innerHTML = tabContent[tabNum];
+
+  const elems = document.querySelectorAll(".tab_list");
+
+  [...elems].forEach((elem) => {
+    if (elem.className === "tab_list tab_active") {
+      elem.className = "tab_list";
+    }
+  });
+
+  target.className = "tab_list tab_active";
 };
+
+const nextBtn = document.querySelector(".button__next");
+const prevBtn = document.querySelector(".button__previous");
+const slider = document.querySelector(".slider-list");
+const sliderContents = document.querySelectorAll(".slider-list img");
+
+let curIndex = 0;
+
+const showNext = () => {
+  const image = slider;
+  const Time = 300;
+  if (curIndex < sliderContents.length - 1) {
+    setTimeout(() => {
+      image.style.transform = "translateX(-" + (curIndex + 1) * 720 + "px)";
+      curIndex++;
+    }, Time);
+  } else {
+    setTimeout(() => {
+      image.style.transform = "translateX(0px)";
+      curIndex = -1;
+    }, Time);
+  }
+};
+
+const showPrev = () => {
+  const image = slider;
+  const Time = 300;
+  if (curIndex > 0)
+    setTimeout(() => {
+      image.style.transform = "translateX(-" + (curIndex - 1) * 720 + "px)";
+      curIndex--;
+    }, Time);
+};
+
+nextBtn.addEventListener("click", (evt) => {
+  showNext();
+});
+
+prevBtn.addEventListener("click", (evt) => {
+  showPrev();
+});
