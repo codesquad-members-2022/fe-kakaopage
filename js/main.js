6f1abbf1e979b25b8932555b3ac35f2d4c3f7985
soralee <sora2821@gmail.com>
2022-02-25 12:20:57 +0900

    refactor: revise image file name for coherence & feat: add passive slide feature to promotion webtoon section

--- js/main.js
+++ js/main.js
@@ -4,6 +4,7 @@ import {createWebtoonBoardSection} from "./common.js";
 import {weeklyWebtoonList} from "./data.js";
 let previousSubCategory = document.createElement("div");
 let previousWeekdayCategory = document.createElement("div");
+let clickButtonCount = 0;
 
 init();
 
@@ -23,12 +24,46 @@ function attachWebtoonBoardHandler() {
 }
 
 function delegateEventListener(parentElement, func) {
-  parentElement.addEventListener("click", (event) => {
-    const targetElement = event.target;
-    if (targetElement.tagName === "DIV") {
-      func(targetElement);
+  parentElement.addEventListener("click", ({target}) => {
+    if (target.tagName === "DIV") {
+      func(target);
     }
   });
+}
+
+function attachButtonHandler() {
+  const slideButtonDiv = document.querySelector(".slide_button");
+  const slideSize = slideButtonDiv.clientWidth;
+  slideButtonDiv.addEventListener("click", ({target}) => {
+    if (target.closest(".next_button")) {
+      clickButtonCount++;
+      moveWebtoonSlide(slideSize);
+      return;
+    }
+    if (target.closest(".previous_button")) {
+      clickButtonCount--;
+      moveWebtoonSlide(slideSize);
+      return;
+    }
+  });
+}
+
+function moveWebtoonSlide(slideSize) {
+  console.log(clickButtonCount);
+  const promotionWebtoonSlide = document.querySelector(".promotion_webtoon_slide");
+  const promotionWebtoons = promotionWebtoonSlide.children;
+  if (clickButtonCount < 0) {
+    clickButtonCount = promotionWebtoons.length;
+    promotionWebtoonSlide.style.transform = `translateX(${- slideSize * (promotionWebtoons.length - 1)}px)`;
+    return;
+  }
+  if (clickButtonCount >= promotionWebtoons.length) {
+    promotionWebtoonSlide.style.transform = "";
+    clickButtonCount = 0;
+    return;
+  }
+  promotionWebtoonSlide.style.translate = "transform 0.3s ease-in-out";
+  promotionWebtoonSlide.style.transform = `translateX(${-slideSize * clickButtonCount}px)`;
 }
 
 function clickSubCategoryNav(subCategoryNav) {
@@ -44,9 +79,11 @@ function renderSubCategory(subCategory) {
     case "요일연재":
       subCategorySection.innerHTML = weeklyPublicationTemplate;
       attachWebtoonBoardHandler();
+      attachButtonHandler();
       break;
     case "홈":
       subCategorySection.innerHTML = homeTemplate;
+      attachButtonHandler();
       break;
     case "웹툰": case "소년": case "드라마": case "로맨스": case "로판": case "액션무협": case "BL":
       subCategorySection.innerHTML = "";
--- js/weeklyPublication.js
+++ js/weeklyPublication.js
@@ -18,7 +18,7 @@ function createWebtoonCategorySection() {
 }
 
 function createWebtoonTypeCategoryNav() {
-  let webtoonTypeCategoryTemplate = `<nav class="webtoon_type_navigation category_align">`;
+  let webtoonTypeCategoryTemplate = `<nav class="webtoon_type_navigation align_spce_around">`;
   const webtoonTypeFilterNav = createWebtoonTypeFilterNav();
   const webtoonGenreFilterUl = createWebtoonGenreFilterUl();
   webtoonTypeCategoryTemplate += webtoonTypeFilterNav + webtoonGenreFilterUl + "</nav>";
@@ -26,10 +26,10 @@ function createWebtoonTypeCategoryNav() {
 }
 
 function createWebtoonTypeFilterNav() {
-  return `<nav class="webtoon_type_filter category_align">
+  return `<nav class="webtoon_type_filter align_spce_around">
     <div class="webtoon_type">전체</div>
     <div class="webtoon_type">웹툰</div>
-    <div class="webtoon_type"><img src="./image/wait_sign.svg">웹툰</div>
+    <div class="webtoon_type"><img src="./image/waitSign.svg">웹툰</div>
   </nav>`;
 }
 
