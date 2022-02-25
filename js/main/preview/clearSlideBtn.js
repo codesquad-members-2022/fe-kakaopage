388bd6ff979353c38b7da786ffa28bc450cee410
Cola <bcad1591@gmail.com>
2022-02-25 10:59:18 +0900

    Remove: 사용되지 않는 js파일 제거
    clearSlideBtn.js, drawSlideBtn.js는 사용하지 않아서 gitignore에 추가함

--- js/main/preview/clearSlideBtn.js
+++
@@ -1,9 +0,0 @@
-// const clearSlideBtn = () => {
-//   const prevBtn = document.querySelector('.prev-btn');
-//   const nextBtn = document.querySelector('.next-btn');
-//   if (!prevBtn || !nextBtn) return;
-//   prevBtn.parentElement.removeChild(prevBtn);
-//   nextBtn.parentElement.removeChild(nextBtn);
-// };
-
-// export default clearSlideBtn;
