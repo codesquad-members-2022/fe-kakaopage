b9167a66415f816a645dc8dfa1106c5d1918ea57
otterp012 <otterp012@gmail.com>
2022-02-25 15:46:10 +0900

    feat: 캐러셀 수동 클릭 이벤트 기능 추가

--- public/app.js
+++ public/app.js
@@ -29,6 +29,32 @@ navListsEl.forEach((navList) => {
         }
     })
 })
+bannerClickEventHandler();
+function bannerClickEventHandler() {
+    const leftButton = document.querySelector('#banner-left-icon');
+    const rightButton = document.querySelector('#banner-right-icon');
+    const slides = document.querySelectorAll('.aside__banner-slide');
+    const slideLists = document.querySelector('.aside__banner-slide-list');
+    const size = slides.length;
+    const width =  document.querySelector('.aside__banner-slide').clientWidth;
+
+    let transform = 0;
+    const min = -((size-1) * width);
+    leftButton.addEventListener(('click'), () => {
+        transform -= width;
+        if(transform < min) transform = 0;
+        slideLists.style.transform = `translateX(${transform}px)`
+    })
+
+    rightButton.addEventListener(('click'), () => {
+        transform += width;
+        if(transform > 0) transform = min;
+        slideLists.style.transform = `translateX(${transform}px)`
+    })
+}
+
+// 버튼을 누를때만 동작하는 함수
+// 수정해야함.
 
 import { renderBase } from './component/base.js';
 import { renderHome } from './pages/home.js';
