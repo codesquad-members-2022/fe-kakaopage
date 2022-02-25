a72bf1ba6603954f38a1a87aefefedf4fb69e53c
Ubeom Kim <kimub1204@gmail.com>
2022-02-25 17:55:40 +0900

    git conflict

--- js/utility.js
+++ js/utility.js
@@ -1,17 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 96ce764 (style: 오타 수정)
-// 오타
-export function searchSelctor(el, base = document) {
+export function searchSelector(el, base = document) {
   return base.querySelector(el)
-=======
-export function searchSelctor(el) {
-  return document.querySelector(el)
->>>>>>> 37d60f0 (refactor: js import, export 부분 수정, utility.js 추가)
-=======
-export function searchSelctor(el, base = document) {
-  return base.querySelector(el)
->>>>>>> f09cddd (refactor: util 함수 base 추가)
 }
