6baeb5c2399221fb26fdcf44adad353972fa9b61
otterp012 <otterp012@gmail.com>
2022-02-25 10:05:00 +0900

    fix: renderNav함수 reduce로 수정

--- public/component/nav.js
+++ public/component/nav.js
@@ -1,15 +1,9 @@
 function renderNav(className, textLists) {
-    let lists = '';
-    textLists.forEach((textList) => {
-        lists += `<li class="${className}-list">${textList}</li>`
-    })
 
-    // textLists.reduce((acc, cur) => {
-    //     cur = `<li class="${className}-list">${textList}</li>`
-    //     acc + cur;
-    // }, ) 
-    // reduce로 수정하기.
-    
+    let lists = textLists.reduce((acc,textList) => {
+        return acc + `<li class="${className}-list">${textList}</li>`;
+    }, '')
+
     return `<nav class="${className}">
     <ul class="${className}-lists">
         ${lists}
