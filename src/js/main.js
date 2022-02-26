c8c962ddcc20f1742ae9b60b3ad4059ac9ec4a23
Sangbeom Heo <mansaout@gmail.com>
2022-02-27 02:42:48 +0900

    Refactor: "init함수 로직 수정 : 첫 화면이 웹툰-홈 이 되게"

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,12 @@
 import { renderer } from './renderer.js';
-import { addHandlerOnNav, loadContentFromGnb } from './navHandler.js';
+import { addHandlerOnGnb, loadMain } from './handler.js';
 
 const init = () => {
   renderer.header();
   renderer.footer();
   renderer.gnb();
-  addHandlerOnNav('gnb');
-  loadContentFromGnb('웹툰');
+  addHandlerOnGnb();
+  loadMain('웹툰', '홈');
 };
 
 window.addEventListener('DOMContentLoaded', init);
