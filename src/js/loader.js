7af0ed12aadf3dd282e401ab286752d102bd8a03
Sangbeom Heo <mansaout@gmail.com>
2022-02-27 02:21:52 +0900

    Refactor: "홈탭 콘텐츠 로드를 렌더링 함수로 처리하게 변경"

--- src/js/loader.js
+++ src/js/loader.js
@@ -4,8 +4,7 @@ import { addHandlerOnBanner } from './bannerHandler.js';
 const loader = {
   // 홈
   home() {
-    const mainEl = document.querySelector('.main');
-    mainEl.innerHTML = `<p style="text-align:center;padding:100px 0;">홈 탭은 준비 중입니다.</p>`;
+    renderer.preparing();
   },
   // 홈 - 웹툰
   webtoonHome(DATA) {
