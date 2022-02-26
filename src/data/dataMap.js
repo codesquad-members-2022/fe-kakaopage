ad1937a0c322d461f79cb70f03ea39cd9149c19d
Sangbeom Heo <mansaout@gmail.com>
2022-02-27 02:21:16 +0900

    Feat: "dataMap 추가"

---
+++ src/data/dataMap.js
@@ -0,0 +1,19 @@
+import {
+  DATA_HOME,
+  DATA_WEBTOON,
+  DATA_WEBNOVEL,
+  DATA_MOVIE,
+  DATA_BROADCAST,
+  DATA_BOOK,
+} from './data.js';
+
+const dataMap = {
+  홈: DATA_HOME,
+  웹툰: DATA_WEBTOON,
+  웹소설: DATA_WEBNOVEL,
+  영화: DATA_MOVIE,
+  방송: DATA_BROADCAST,
+  책: DATA_BOOK,
+};
+
+export { dataMap };
