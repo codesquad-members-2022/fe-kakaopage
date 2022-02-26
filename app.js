1d88a6dad6a7ae8a1989cd23cc624d3896d7c0ab
BB-choi <choibobae0809@gmail.com>
2022-02-26 15:21:22 +0900

    feat: express 설치 및 app.js 추가

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+
+const app = express();
+const port = 3000;
+
+app.use(express.static(__dirname));
+
+app.get("/*", (req, res) => {
+    res.sendFile(__dirname + "/index.html");
+});
+
+app.listen(port, () => {
+    console.log(`App listening on port ${port}`);
+});
