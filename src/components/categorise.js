a819124055c68765f419d84133e4e96a064b3609
dony <rsefa94@gmail.com>
2022-02-27 21:51:32 +0900

    Chore: prettier, eslint 설치

--- src/components/categorise.js
+++ src/components/categorise.js
@@ -1,16 +1,12 @@
-const getCategoryTemplate = (dataEl) => {
-    return (
-        `<li class="category"><a href="javascript:;" class="category__link" data-category="${dataEl}">${dataEl}</a></li>`
-    );
-}
+const getCategoryTemplate = dataEl => {
+  return `<li class="category"><a href="javascript:;" class="category__link" data-category="${dataEl}">${dataEl}</a></li>`;
+};
 
-const getCategoriesTemplate = (data) => {
-    const categoryList = data.reduce((acc, cur) => acc += getCategoryTemplate(cur), '');
-    return (
-        `<ul class="categories">
-            ${categoryList}
-        </ul>`
-    );
-}
+const getCategoriesTemplate = data => {
+  const categoryList = data.reduce((acc, cur) => (acc += getCategoryTemplate(cur)), "");
+  return `<ul class="categories">
+          ${categoryList}
+      </ul>`;
+};
 
 export { getCategoriesTemplate };
