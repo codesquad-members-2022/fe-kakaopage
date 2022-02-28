b1a8bca8733db71f4783b46e1bc25bba9ef19308
BB-choi <choibobae0809@gmail.com>
2022-02-28 23:07:22 +0900

    rename: static 파일을 static/위치로 이동

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-app.use(express.static(__dirname));
+app.use(express.static(__dirname + "/static/"));
 
 app.get("/*", (req, res) => {
     res.sendFile(__dirname + "/index.html");
