fe1a31e9b0713cd3157e96c5fa0bbd0c0b7392b6
BB-choi <choibobae0809@gmail.com>
2022-02-28 21:50:25 +0900

    refactor: event바인딩 함수 index.js에서 분리

--- js/index.js
+++ js/index.js
@@ -1,26 +1,8 @@
 import { $ } from "./utils/dom.js";
-import { toggleClass, toggleWeekDayMenu, preventDefaults } from "./tools.js";
+import { toggleWeekDayMenu, preventDefaults } from "./tools.js";
+import { bindSubMenuEvent } from "./submenu.js";
 import { bindCaroulselEvent } from "./carousel.js";
-import { render, renderHome, renderWeekday } from "./renderer.js";
-
-const bindSubMenuEvent = () => {
-    $(".sub-menu").addEventListener("click", ({ target }) => {
-        const curEl = target.parentNode;
-        const targetPage = target.innerText;
-
-        toggleClass(curEl, "active");
-
-        if (targetPage === "홈") {
-            renderHome();
-            return;
-        }
-        if (targetPage === "요일연재") {
-            renderWeekday();
-            return;
-        }
-        render("");
-    });
-};
+import { renderHome, renderWeekday } from "./renderer.js";
 
 const bindEventListeners = () => {
     bindSubMenuEvent();
