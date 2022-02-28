7b65b3dee2321846010339e200b57baa2887fcab
BB-choi <choibobae0809@gmail.com>
2022-02-28 11:26:51 +0900

    comment: 불필요한 주석 삭제
    - /data/datas.js 의 주석 삭제

--- js/data/datas.js
+++ js/data/datas.js
@@ -1,11 +1,4 @@
-// import { homeData } from "./homeData.js";
-// import { romanceTop } from "./genreItems.js";
-// import { weekdayData } from "./weekdayData.js";
-// import { carouselImgs } from "./carouselImgs.js";
-
 export { homeData } from "./homeData.js";
 export { romanceTop } from "./genreItems.js";
 export { weekdayData } from "./weekdayData.js";
 export { carouselImgs } from "./carouselImgs.js";
-
-// import { homeData , romanceTop,weekdayData,carouselImgs } from "./data/datas.js";
