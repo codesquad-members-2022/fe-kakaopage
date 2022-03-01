90e36574bc8094b1fc75a3858c44a067919358b7
inust33 <inust33@gmail.com>
2022-03-02 03:51:54 +0900

    static rAF loop

--- Core/Component.js
+++ Core/Component.js
@@ -5,13 +5,31 @@ export default class Component{
     $target;
     $state;
     $props;
+    #willRender = false;
+    static #subjects = new Set;
+    static #inited = false;
+
+    static #notify(v){
+        this.#subjects.add(v);
+        if(this.#inited)return;
+        this.#inited = true;
+        const f = _=>{
+            this.#subjects.forEach(v=>{
+                if(!v.#willRender)return;
+                v.render()
+                v.#willRender = false;
+            })
+            requestAnimationFrame(f);
+        }
+        requestAnimationFrame(f);
+    }
     constructor(target,props) {
         this.$target = target;
         this.$props = props;
         this.setup();
-
         this.render();
         this.setEvent();
+        Component.#notify(this);
     }
     setup(){
     }
@@ -25,7 +43,7 @@ export default class Component{
         return ``;
     }
     setEvent(){}
-    setState(newState){this.$state = {...this.$state, ...newState}; this.render();}
+    setState(newState){this.$state = {...this.$state, ...newState}; this.#willRender=true}
     addEvent = ( eventType, selector, callback) => {
         const children = [...this.$target.querySelectorAll(selector)]
         const ok = (eventTarget) => children.includes(eventTarget) || eventTarget.closest(selector)
