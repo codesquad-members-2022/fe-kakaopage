69b9874b2faf2460da072bf6de6abdf2593482d1
yunho <kimyouknow@naver.com>
2022-03-01 16:53:29 +0900

    Feat: 서버와 클라이언트 분리 후 cors문제 해결하여 데이터 통신완료

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
-const mockDB = require('./db/store');
+const mockDB = require('./db/store.js');
+const cors = require('cors');
 
 const PORT = process.env.PORT || 4000;
 const app = express();
@@ -11,25 +12,38 @@ db.initDB();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-// app.get('/*', async (req, res) => {
-//   // req.params나 req.query 알아보고 url에서 바로 받아오기
-//   const { categoryUid, subCategoryUid } = req.body;
-//   const resultObj = {};
-//   try {
-//     const content = await db.findData(categoryUid, subCategoryUid);
-//     resultObj.content = content;
-//     resultObj.isSuccess = true;
-//     resultObj.msg = '성공!';
-//   } catch (error) {
-//     resultObj.isSuccess = false;
-//     resultObj.msg = error.message;
-//   }
-//   res.send(resultObj);
+const corsOption = {
+  origin: 'http://localhost:8080/',
+  credentials: true, // 응답 헤더에 Access-Control-Allow-Credentials 추가
+  optionsSuccessStatus: 200, // 응답 상태 200으로 설정
+};
+
+app.use(cors());
+
+// app.use((req, res, next) => {
+//   req.header('Access-Control-Allow-Origin', '*');
+//   req.header('Content-Type', 'application/x-www-form-urlencoded');
+//   res.header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, DELETE');
+//   next();
 // });
 
+app.post('/', async (req, res, next) => {
+  const { categoryUid, subCategoryUid } = req.body;
+  const resultObj = {};
+  try {
+    const content = await db.findData(categoryUid, subCategoryUid);
+    resultObj.content = content;
+    resultObj.isSuccess = true;
+    resultObj.msg = '성공!';
+  } catch (error) {
+    resultObj.isSuccess = false;
+    resultObj.msg = error.message;
+  }
+  res.json(resultObj);
+});
+
 app.get('/', (req, res) => {
-  console.log(req);
-  res.send('프론트와 분리된 서버입니다');
+  res.send('프론트와 분리된 서버입니다.');
 });
 
 app.listen(PORT, () =>
