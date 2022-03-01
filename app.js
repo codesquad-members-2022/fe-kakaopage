7a3a5c317f80591a75ffb6a1e22365323a7f540d
BB-choi <choibobae0809@gmail.com>
2022-03-01 18:20:45 +0900

    refactor: app.js에 불필요한 __dirname 삭제

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const port = 3000;
 
 const genreItemAPI = require("./data/genreItems.json");
 
-app.use(express.static(__dirname + "/static/"));
+app.use(express.static("static"));
 
 app.get("/genre/romanceTop", (req, res) => {
     res.json(genreItemAPI.romanceTop);
