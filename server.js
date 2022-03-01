0c7e8949249c00eb3a593ccb8feab2b339c70ed4
Millie <jay.lee.2280@gmail.com>
2022-03-02 00:11:54 +0900

    Add: filter webtoons using default route parameter

--- server.js
+++ server.js
@@ -7,11 +7,24 @@ const PORT = 3001;
 
 app.use(cors());
 
+app.use(express.static('src'));
+
 app.get('/data', (req, res) => {
   res.send(data);
 });
 
-app.use(express.static('src'));
+app.get('/webtoon', (req, res) => {
+  res.json(data.webtoon);
+});
+
+app.get('/webtoon/:day', (req, res) => {
+  const day = req.params.day;
+  const filteredWebtoon = data.webtoon.filter(wt => wt.day.includes(day));
+  if (!filteredWebtoon) {
+    return res.status(400).end();
+  }
+  res.json(filteredWebtoon);
+});
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/express.html');
