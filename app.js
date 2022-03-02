f3490f95c535dda276c60cec2aed333146b26fb3
Sangbeom Heo <mansaout@gmail.com>
2022-03-03 02:20:32 +0900

    Feat: " '/' 요청 시 '/main'으로 리다이렉트"
    - 특별한 이유는 없습니다. 실제 카카오페이지에 요청시 /main으로 리다이렉트되어 시도해봤습니다.

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
+app.get('/', (req, res) => res.redirect('/main'));
+
 app.use(express.static('public'));
+
+app.get('/main', (req, res) => res.sendFile(`${__dirname}/public/index.html`));
 
 app.listen(PORT, () => console.log(`Server running on ${PORT}`));
