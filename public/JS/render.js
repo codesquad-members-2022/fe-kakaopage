93f27b1abf79b657d40793da7eff90cc4662abdd
jm <jm.dev.study@gmail.com>
2022-03-02 17:54:20 +0900

    [fix] render.js: 각 렌더 함수가 프로미스를 반환하도록 변경 - render.js의 renderMain 함수와 main.js의 moveGenreNav 함수가 비슷한 기능을 하기 때문에 renderMain 함수를 삭제했다.

--- public/JS/render.js
+++ public/JS/render.js
@@ -10,27 +10,10 @@ import {homeContainerInfo} from './containerInfo/home.js'
 // ============== 유틸리티 모듈 ==============
 import {$} from './utility.js';
 
-function renderMain(tab) {
-  switch (tab) {
-    case 'home':
-      renderHome(tab);
-      break
-    case 'daily':
-      renderDaily(tab);
-      break;
-    case 'webtoon':
-      renderWebtoon(tab);
-      break;
-    case 'boy':
-      renderBoy(tab);
-      break;
-  }
-}
-
 async function renderHome(tab) {
   const containerInfo = homeContainerInfo;
 
-  const templete = await Promise.all([
+  return await Promise.all([
     renderMainBanner(tab), 
     renderThemeMenu(tab),
     renderPromotionBanner(tab),
@@ -38,34 +21,30 @@ async function renderHome(tab) {
     renderContainer(containerInfo.romenceTop, tab),
     renderContainer(containerInfo.dailyRanking, tab),
     renderMoveApp()
-  ]);
-
-  $('.main').insertAdjacentHTML('beforeend', templete.join(''));
+  ])
+  .then(templete =>  $('.main').insertAdjacentHTML('beforeend', templete.join('')));
 }
 
 async function renderDaily(tab) {
-  const templete = await Promise.all([
+  return await Promise.all([
     renderMainBanner(tab),
     renderSelectDayDaily()
-  ]);
-
-  $('.main').insertAdjacentHTML('beforeend', templete.join(''));
+  ])
+  .then(templete =>  $('.main').insertAdjacentHTML('beforeend', templete.join('')));
 }
 
 async function renderWebtoon(tab) {
-  const templete = await Promise.all([renderMainBanner(tab)]);
-
-  $('.main').insertAdjacentHTML('beforeend', templete.join(''));
+  return await Promise.all([renderMainBanner(tab)])
+    .then(templete =>  $('.main').insertAdjacentHTML('beforeend', templete.join('')));
 }
 
 async function renderBoy(tab) {
-  const templete = await Promise.all([
+  return await Promise.all([
     renderMainBanner(tab), 
     renderThemeMenu(tab),
     renderPromotionBanner(tab),
   ])
-
-  $('.main').insertAdjacentHTML('beforeend', templete.join(''));
+  .then(templete =>  $('.main').insertAdjacentHTML('beforeend', templete.join('')));
 }
 
-export {renderMain}
+export {renderHome, renderDaily, renderWebtoon, renderBoy}
