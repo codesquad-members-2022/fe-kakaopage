7b02fc67bff3a150aba7c4c564eeabf443eb83f4
연 <mogoo22@naver.com>
2022-03-02 19:26:03 +0900

    refactor: 변수 이름 변경 : filter->selectedData, 콜백함수 합침

--- public/js/rendering/main-ad-banner.js
+++ public/js/rendering/main-ad-banner.js
@@ -6,12 +6,12 @@ export const renderingMainAd = (selectedValue) => {
   const item = fetch("http://localhost:3000/webtoons/main-ad")
     .then((response) => response.json())
     .then((json) => json.filter((e) => e.category === selectedValue))
-    .then((filter) =>
-      filter
-        .map((e, i) => mainAdBanner(e, i, filter.length))
-        .reduce((acc, cur) => acc + cur)
+    .then(
+      (selectedData) =>
+        ($(".event-slider").innerHTML = selectedData
+          .map((e, i) => mainAdBanner(e, i, selectedData.length))
+          .reduce((acc, cur) => acc + cur))
     )
-    .then((data) => ($(".event-slider").innerHTML = data))
     .then(() => {
       makingClone();
       changeTab();
