55ff4196afe8c4eb88bf87fc88f2b7338de03c82
dony <rsefa94@gmail.com>
2022-03-02 16:42:37 +0900

    Comment: 사용하지 않는 주석 삭제

--- server.js
+++ server.js
@@ -2,13 +2,11 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 3000;
-// const bodyParser = require('body-parser');
 
 app.use(cors());
 
 app.use(express.static('static'));
 app.use(express.json());
-// app.use(bodyParser.urlencoded({ extended: true }));
 // app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
