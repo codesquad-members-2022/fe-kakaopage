245f7da0f1fe0a95d3cc37e53f88a7187b806fb0
연 <mogoo22@naver.com>
2022-03-03 01:16:25 +0900

    refactor: 콜백 함수 render로 분리

--- public/js/rendering/main-ad-banner.js
+++ public/js/rendering/main-ad-banner.js
@@ -2,16 +2,17 @@ import { $ } from "../utils.js";
 import { mainAdBanner } from "../components/main-ad-banner.js";
 import { makingClone, changeTab } from "../event/event-slide.js";
 
+const render = (selectedData) => {
+  $(".event-slider").innerHTML = selectedData
+    .map((e, i) => mainAdBanner(e, i, selectedData.length))
+    .reduce((acc, cur) => acc + cur);
+};
+
 export const renderingMainAd = (selectedValue) => {
   const item = fetch("http://localhost:3000/webtoons/main-ad")
     .then((response) => response.json())
     .then((json) => json.filter((e) => e.category === selectedValue))
-    .then(
-      (selectedData) =>
-        ($(".event-slider").innerHTML = selectedData
-          .map((e, i) => mainAdBanner(e, i, selectedData.length))
-          .reduce((acc, cur) => acc + cur))
-    )
+    .then((selectedData) => render(selectedData))
     .then(() => {
       makingClone();
       changeTab();
