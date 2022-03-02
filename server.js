78b7ee911eb623c00e6a8059fc3fb2639da29442
Hemudi <ksum1205@naver.com>
2022-03-02 16:15:38 +0900

    Feat: 서버 라우터 기능 추가

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
-const express = require('express');
+const router = require('./server/api/DataRouter.js');
+const express = require("express");
 const cors = require('cors');
 
 const server = express();
@@ -6,12 +7,14 @@ const PORT = 3000;
 
 server.use(cors());
 server.use(express.static(__dirname));
+server.use('/data', router);
+
 server.set('port', process.env.PORT || 3000);
 
 server.get('/', (req, res) => {
-    res.render(__dirname + './index.html');
+    res.sendFile(__dirname + './index.html');
 });
 
 server.listen(PORT, () => {
     console.log('서버 시작 : ' + server.get('port'));
-})
+});
