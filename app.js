013201e0f1147f776f365f431d25e3b507d2a428
suuzin <8oozin@gmail.com>
2022-03-02 22:42:31 +0900

    feat:  json파일 연결

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
 const express = require("express");
-const app = express();
+const server = express();
+const getImageAPI = require('./webtoonData/data.json');
 
-app.use(express.static(__dirname + "/"))
+server.use(express.static(__dirname + "/"))
 
-app.get("/", (req, res) => {
-    res.sendFile(__dirname + "/kakaopage.html");
+
+server.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
 });
 
-app.listen(3000, (err) => {
+server.get("/webtoonData/data", (request,response,next) => { 
+  response.json(getImageAPI); 
+});
+
+server.listen(3000, (err) => {
   if (err) return console.log(err);
-  console.log("The app is listening on port 3000");
+  console.log("The server is listening on port 3000");
 });
