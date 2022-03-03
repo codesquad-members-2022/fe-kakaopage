11353497bf2bfaa9fa43ee1f89c7d1bf75b5fa73
Sangbeom Heo <mansaout@gmail.com>
2022-03-04 05:34:42 +0900

    Feat: "updateDocumentTitle 함수에 파라미터에 따른 세부 로직 분기 추가"
    - 파라미터가 있으면 해당 내용으로 title 변경
    - 파라미터가 없으면 현재 카테고리로 title 변경

--- static/js/utility/util.js
+++ static/js/utility/util.js
@@ -4,11 +4,25 @@ const initElement = (selector) => (select(selector).innerHTML = '');
 
 const removeElement = (selector) => select(selector).remove();
 
-const updateDocumentTitle = (title) => (document.title = `${title} | 카카오페이지`);
+const updateDocumentTitle = (title) => {
+  if (title) {
+    document.title = `${title} | 카카오페이지`;
+    return;
+  }
+
+  const [global, sub] = [getCurrentCategory('gnb'), getCurrentCategory('snb')];
+  const currentCategory = `${sub ? sub + ' - ' : ''}${global}`;
+  document.title = `${currentCategory} | 카카오페이지`;
+};
+
+const getCurrentCategory = (nav) => {
+  const currentEl = select(`.${nav}__item--active`);
+  return currentEl ? currentEl.dataset.category : null;
+};
 
 const getData = (...paths) => {
   const resource = paths.join('/');
   return fetch(resource).then((response) => response.json());
 };
 
-export { select, initElement, removeElement, updateDocumentTitle, getData };
+export { select, initElement, removeElement, updateDocumentTitle, getCurrentCategory, getData };
