2cfee98e6fd88a064821e723c1e2774c4665e101
SeungHyun <fm10033@gmail.com>
2022-03-03 20:37:56 +0900

    html 로딩 성공, 경로 설정

--- kakaoPage/app.js
+++ kakaoPage/app.js
@@ -1,42 +1,58 @@
-var createError = require("http-errors");
-var express = require("express");
-var path = require("path");
-var cookieParser = require("cookie-parser");
-var logger = require("morgan");
+// let createError = require("http-errors");
+// let express = require("express");
+// let path = require("path");
+// let cookieParser = require("cookie-parser");
+// let logger = require("morgan");
 
-var indexRouter = require("./routes/index");
-var usersRouter = require("./routes/users");
+// let indexRouter = require("./routes/index");
+// let usersRouter = require("./routes/users");
 
-var app = express();
+// let app = express();
 
-// view engine setup
-app.set("views", path.join(__dirname, "views"));
-app.engine("html", require("ejs").renderFile);
-app.set("view engine", "html");
+// // view engine setup
+// app.set("views", path.join(kakaoPage, "views"));
+// app.engine("html", require("ejs").renderFile);
+// app.set("view engine", "ejs");
 
-app.use(logger("dev"));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+// app.use(logger("dev"));
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
+// app.use(cookieParser());
+// app.use(express.static(path.join(__dirname, "public")));
+
+// app.use("/", indexRouter);
+// app.use("/users", usersRouter);
+
+// // catch 404 and forward to error handler
+// app.use(function (req, res, next) {
+//   next(createError(404));
+// });
+
+// // error handler
+// app.use(function (err, req, res, next) {
+//   // set locals, only providing error in development
+//   res.locals.message = err.message;
+//   res.locals.error = req.app.get("env") === "development" ? err : {};
+
+//   // render the error page
+//   res.status(err.status || 500);
+//   res.render("error");
+// });
+
+// module.exports = app;
+
+const express = require("express");
+const server = express();
+
+server.use(express.static(__dirname + "/public"));
 app.use(express.static(path.join(__dirname, "public")));
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use("/", indexRouter);
-app.use("/users", usersRouter);
-
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  next(createError(404));
+server.get("/", (req, res) => {
+  res.sendFile(__dirname + "/" + "views/kakaopageMain.html");
 });
 
-// error handler
-app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render("error");
+server.listen(3000, (err) => {
+  if (err) return console.log(err);
+  console.log("The server is listening on port 3000");
 });
-
-module.exports = app;
