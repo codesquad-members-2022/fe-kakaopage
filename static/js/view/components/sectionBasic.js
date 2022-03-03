8c5c5ff33941bdd7e65fadb433d2be36f6fa8e29
Sangbeom Heo <mansaout@gmail.com>
2022-03-04 06:40:29 +0900

    Refactor: "sectionBasic 컴포넌트에 count 요소 추가"

--- static/js/view/components/sectionBasic.js
+++ static/js/view/components/sectionBasic.js
@@ -1,7 +1,7 @@
-const createSectionBasic = (title, content) => {
+const createSectionBasic = (title, count, content) => {
   return `
     <div class="section-basic">
-      <h4 class="section-basic__title">${title}<span></span></h4>
+      <h4 class="section-basic__title">${title} <span class="section-basic__count">(${count})</span></h4>
       <span class="section-basic__view-more">
         <span>더보기</span><img src="images/icon/ic-more-gray.svg"/>
       </span>
