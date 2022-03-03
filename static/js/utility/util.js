0d8de07097b35568c5b0cb80b33b39969397f521
Sangbeom Heo <mansaout@gmail.com>
2022-03-04 03:25:18 +0900

    Feat: "특정 엘리먼트를 삭제하는 유틸 함수 removeElement 추가"

--- static/js/utility/util.js
+++ static/js/utility/util.js
@@ -2,6 +2,8 @@ const select = (selector, where = document) => where.querySelector(selector);
 
 const initElement = (selector) => (select(selector).innerHTML = '');
 
+const removeElement = (selector) => select(selector).remove();
+
 const updateDocumentTitle = (title) => (document.title = `${title} | 카카오페이지`);
 
-export { select, initElement, updateDocumentTitle };
+export { select, initElement, removeElement, updateDocumentTitle };
