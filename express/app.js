4bdf28e5ee733d60a5247ac533f3f4623fcddc60
seungchanHan <seungchanhan00@gmail.com>
2022-03-03 15:10:16 +0900

    [ADD] json router

--- express/app.js
+++ express/app.js
@@ -1,13 +1,19 @@
 const express = require('express');
 const app = express();
+const router = express.Router();
 const port = 5050;
+const slideImageData = require('./public/js/data/slide-data/slide-data.json');
 
-app.get('/', function (req, res) {
+app.get('/', (req, res) => {
   res.sendFile(__dirname + '/public/main.html');
+});
+
+router.get('/', (req, res) => {
+  res.json(slideImageData);
 });
 
 app.use(express.static(__dirname));
 
-app.listen(port, function () {
+app.listen(port, () => {
   console.log(`start! express server on port ${port}`);
 });
