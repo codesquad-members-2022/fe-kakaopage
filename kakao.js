fa063f480993bb32c2fe5469b2d0ef93d24dc24d
suuzin <8oozin@gmail.com>
2022-03-04 07:17:55 +0900

    refactor: js addEventListener 수정

--- kakao.js
+++ kakao.js
@@ -4,30 +4,24 @@ import { webtoonCard } from "./webtoon.js";
 const weekCategory = document.querySelector(".tab .main_category ul");
 const mainCategory = document.querySelector(".main_category ul");
 
-function showMain(){
-    function mainContentClickHandler(event){
-        if(event.target.tagName !=="LI")return;
-        else{
-            mainCategory.querySelector(".sub_active").classList.remove("sub_active");
-            event.target.classList.add("sub_active");
-        }
+mainCategory.addEventListener("click",(event) => {
+    if(event.target.tagName !=="LI")return;
+    else{
+        mainCategory.querySelector(".sub_active").classList.remove("sub_active");
+        event.target.classList.add("sub_active");
     }
-    mainCategory.addEventListener("click",mainContentClickHandler);
-}
+});
 
-function showDayWebtoon(){
-    function getDay(event){
-        if(event.target.tagName !== "LI") return;
-        else{
-            weekCategory.querySelector(".weekend_check").classList.remove("weekend_check");
-            weekCategory.querySelector(".sub_active").classList.remove("sub_active");
-            event.target.classList.add("weekend_check");
-            event.target.classList.add("sub_active"); 
-            getWebtoonData(event);
-        }
+weekCategory.addEventListener("click",(event) => {
+    if(event.target.tagName !== "LI") return;
+    else{
+        weekCategory.querySelector(".weekend_check").classList.remove("weekend_check");
+        weekCategory.querySelector(".sub_active").classList.remove("sub_active");
+        event.target.classList.add("weekend_check");
+        event.target.classList.add("sub_active"); 
+        getWebtoonData(event);
     }
-    weekCategory.addEventListener("click",getDay);
-}
+});
 
 // ==================json
 function getWebtoonData(event){
@@ -37,53 +31,10 @@ function getWebtoonData(event){
 };
 
 function getImgData(data,event){
-    document.querySelector('.webtoon_list ul').innerHTML = data.filter((e) => event.target.dataset.value === e.day)
+    document.querySelector('.webtoon_list ul').innerHTML = data.filter((dataEl) => event.target.dataset.value === dataEl.day)
     .map(e => webtoonCard(e))
     .reduce((acc,cur)=>acc+cur,"");
 }
 
-// ==================slide
-function showSlides(){
-    const slides = document.querySelectorAll('.slideshow_slide');
-    const btnLeft = document.querySelector('.slider_btn_left');
-    const btnRight = document.querySelector(".slider_btn_right");
-    let curSlide = 0;
-    const maxSlide = slides.length;
-    
-    function moveSlide(slide){
-        slides.forEach(
-            (slide,index) => (slide.style.transform = `translateX(${100 * (index-curSlide)}%)`)
-        );
-    }
-    moveSlide(0);
-    
-    function nextSlide(){
-        if(curSlide === maxSlide-1){
-            curSlide = 0;
-        }else{
-            curSlide++;
-        }
-        moveSlide(curSlide);
-    }
-    
-    function prevSlide(){
-        if(curSlide === 0){
-            curSlide = maxSlide -1;
-        }else{
-            curSlide--;
-        }
-        moveSlide(curSlide);
-    }
-    
-    btnRight.addEventListener("click",nextSlide);
-    btnLeft.addEventListener("click",prevSlide);
-    setInterval(nextSlide, 3000);
-};
 
-function init(){
-    showMain();
-    showDayWebtoon()
-    showSlides();
-}
-init();
 
