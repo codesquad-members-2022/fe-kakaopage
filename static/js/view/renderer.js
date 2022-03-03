123ef3cd18501537b5fc4c8ad1970e765aab9aa3
Sangbeom Heo <mansaout@gmail.com>
2022-03-03 15:27:23 +0900

    Refactor: "renderer의 내부를 key:value 형태로 수정"

--- static/js/view/renderer.js
+++ static/js/view/renderer.js
@@ -13,41 +13,41 @@ import { createPromotionBanner } from '../components/promotionBanner.js';
 import { createSectionBasic } from '../components/sectionBasic.js';
 
 const renderer = {
-  header() {
+  header: () => {
     select('.header').insertAdjacentHTML('afterbegin', createHeader());
   },
 
-  footer() {
+  footer: () => {
     select('.footer').insertAdjacentHTML('afterbegin', createFooter());
   },
 
-  gnb() {
+  gnb: () => {
     select('.gnb').insertAdjacentHTML('afterbegin', createGnb());
     addHandlerOnGnb();
   },
 
-  snb(subCategorys) {
+  snb: (subCategorys) => {
     select('.main').insertAdjacentHTML('afterbegin', createSnb(subCategorys));
     addHandlerOnSnb();
   },
 
-  gridMenu(menuItemLabels) {
+  gridMenu: (menuItemLabels) => {
     select('.category-content').insertAdjacentHTML('beforeend', createGridMenu(menuItemLabels));
   },
 
-  mainBanner(bannerItems) {
+  mainBanner: (bannerItems) => {
     select('.category-content').insertAdjacentHTML('beforeend', createMainBanner(bannerItems));
     addHandlerOnBanner('main-banner');
   },
 
-  promotionBanner(bannerImgUrls) {
+  promotionBanner: (bannerImgUrls) => {
     select('.category-content').insertAdjacentHTML(
       'beforeend',
       createPromotionBanner(bannerImgUrls)
     );
   },
 
-  sectionBasic(title, content) {
+  sectionBasic: (title, content) => {
     select('.category-content').insertAdjacentHTML('beforeend', createSectionBasic(title, content));
   },
 
@@ -55,9 +55,10 @@ const renderer = {
     select(
       '.category-content'
     ).innerHTML = `<p style="text-align:center;padding:100px 0;">이 탭은 준비 중입니다.</p>`;
+  preparing: () => {
   },
 
-  categoryContentWrap() {
+  categoryContentWrap: () => {
     const categoryContentEl = document.createElement('div');
     categoryContentEl.classList.add('category-content');
     select('.main').append(categoryContentEl);
