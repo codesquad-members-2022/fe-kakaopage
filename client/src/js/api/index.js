e86428ab86e46d181621ae5166e359946104640f
mina-gwak <mina.gwak@kakao.com>
2022-03-03 17:34:25 +0900

    feat: Create data api

--- client/src/js/api/index.js
+++ client/src/js/api/index.js
@@ -3,13 +3,26 @@ const BASE_URL = 'http://localhost:3000';
 const request = (url) => {
   return fetch(url)
     .then((response) => response.json())
-    .catch((err) => console.log(err));
+    .catch((err) => console.error(err));
 };
 
-const WebtoonApi = {
-  getAllWebtoon() {
-    return request(`${BASE_URL}/webtoon`);
+const DataApi = {
+  getWebtoon(callback) {
+    return request(`${BASE_URL}/webtoon`)
+      .then(callback);
+  },
+  getNavItem(callback) {
+    return request(`${BASE_URL}/nav-item`)
+      .then(callback);
+  },
+  getBanner(callback) {
+    return request(`${BASE_URL}/banner`)
+      .then(callback);
+  },
+  getCategory(callback) {
+    return request(`${BASE_URL}/category`)
+      .then(callback);
   },
 };
 
-export default WebtoonApi;
+export default DataApi;
