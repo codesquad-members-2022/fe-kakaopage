92d4ec6b9b920ce6568f68c231c3c2de0dff917e
suuzin <8oozin@gmail.com>
2022-03-04 07:19:28 +0900

    feat:슬라이드 앞뒤에 cloneNode

--- kakao.js
+++ kakao.js
@@ -36,5 +36,34 @@ function getImgData(data,event){
     .reduce((acc,cur)=>acc+cur,"");
 }
 
+// ==================slide
+function showSlides(){
+    const slides = document.querySelector(".slides");
+    const slide = document.querySelectorAll(".slides > div");
+    const prevBtn = document.querySelector(".slider_btn_left");
+    const nextBtn = document.querySelector(".slider_btn_right");
+    let currentSlide = 0;
+    const slideWidth = 720;
+    const slideCount = slide.length;
+    let timer = 0;
+
+    function appendClone(){
+        for (let i = 0; i < slideCount; i++) {
+            const cloneSlide = slide[i].cloneNode(true);
+            cloneSlide.classList.add("clone");
+            slides.appendChild(cloneSlide);
+        }
+    }
+
+    function prependClone(){
+        for (let i = slideCount - 1; i >= 0; i--) {
+            const cloneSlide = slide[i].cloneNode(true);
+            cloneSlide.classList.add("clone");
+            slides.prepend(cloneSlide);
+        }
+    }
+};
+
+showSlides();
 
 
