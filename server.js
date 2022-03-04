bcad001a77513613725236bd59a439dc57ecbfec
dony <rsefa94@gmail.com>
2022-03-04 20:19:52 +0900

    Feat: url 접속시 404 에러 문구 출력

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 3000;
 app.use(cors());
 
 app.use(express.static(`${__dirname}/static`));
-// app.use(express.json());
+app.use(express.json());
 // app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
@@ -15,8 +15,10 @@ app.get('/', (req, res) => {
 
 app.get('/:id', (req, res) => {
   const pageData = require(`${__dirname}/static/src/data/${req.params.id}.json`);
-  res.send(pageData);
-  // res.sendFile(`${__dirname}/static/index.html`);
+});
+
+app.use((req, res) => {
+  res.send('Page Not Found 404');
 });
 
 app.listen(PORT, () => {
