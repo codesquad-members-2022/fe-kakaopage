314b981889bc5ca0e580cc2f5e67de6510c74853
Muffin <jinlog9@gmail.com>
2022-03-04 11:44:32 +0900

    refactor: TopicComponent css 수정

--- src/public/components/TopicComponent.js
+++ src/public/components/TopicComponent.js
@@ -1,6 +1,9 @@
 export const TopicComponent = (title) => {
     return /* html */ `<div class="topic">
                 <h1 class="topic__title">${title}</h1>
-                <span>더보기</span>
+                <a class="topic__see" href="#">
+                    <span>더보기</span>
+                    <img class="topic__see_img" src="https://static-page.kakao.com/static/common/ic-more-gray.svg?639494b81c8127013d0e627243aee94e" alt="더보기 이미지"/>
+                </a>
             </div>`;
 }
