9584834922eca51aa14a6101e7b386aa4315afe8
nanjeong <whdwkxj7@naver.com>
2022-03-04 12:02:14 +0900

    chore: 데모페이지 띄우기 위해 port 수정

--- express.js
+++ express.js
@@ -10,7 +10,7 @@ const dataOfDailyContentsBanner = require("./data/daily-contents/banner.js");
 
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.static("public"));
 
