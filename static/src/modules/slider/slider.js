4cde95021af94a92596b5091a0c31b65e6bc28d7
dony <rsefa94@gmail.com>
2022-03-04 20:17:36 +0900

    Refactor: slider.js 모듈처럼 다른 슬라이드에서도 사용할 수 있도록 수정

--- static/src/modules/slider/slider.js
+++ static/src/modules/slider/slider.js
@@ -1,51 +1,57 @@
 import { $ } from '../../util/utility.js';
 
-const addNewSlide = (newSlideTemplate, insertPosition) => {
-  $('.main-banners__contents').insertAdjacentHTML(insertPosition, newSlideTemplate);
+const moveSlide = (sliderName, oldSlide, newSlide) => {
+  $(`.${sliderName} .${oldSlide}`).remove();
+  $(`.${sliderName} .current-slide`).classList.add(oldSlide);
+  $(`.${sliderName} .current-slide`).classList.remove('current-slide');
+  $(`.${sliderName} .${newSlide}`).classList.add('current-slide');
+  $(`.${sliderName} .${newSlide}`).classList.remove(newSlide);
 };
 
-const moveSlide = (slideName, [oldSlide, newSlide]) => {
-  $(`.${slideName} .${oldSlide}`).remove();
-  $(`.${slideName} .current-slide`).classList.add(oldSlide);
-  $(`.${slideName} .current-slide`).classList.remove('current-slide');
-  $(`.${slideName} .${newSlide}`).classList.add('current-slide');
-  $(`.${slideName} .${newSlide}`).classList.remove(newSlide);
+const moveToPrev = (sliderIdx, sliderOption) => {
+  const slideTemplate = sliderOption.slideFun(sliderOption.data[sliderIdx.currentIdx]);
+  const slideLength = sliderOption.data.length;
+  moveSlide(sliderOption.name, 'next-slide', 'prev-slide');
+  $(`.${sliderOption.name}__contents`).insertAdjacentHTML('afterbegin', slideTemplate);
+  sliderIdx.currentIdx--;
+  sliderIdx.viewIdx--;
+  if (sliderIdx.currentIdx <= 0) sliderIdx.currentIdx = slideLength;
+  if (sliderIdx.viewIdx <= 0) sliderIdx.viewIdx = slideLength;
+  $(`.${sliderOption.name} .slide-paging-number__current`).textContent = sliderIdx.currentIdx;
 };
 
-const sliderIdx = currentIdx => {
-  $('.main-banners .slide-paging-number__current').textContent = currentIdx;
+const moveToNext = (sliderIdx, sliderOption) => {
+  const slideTemplate = sliderOption.slideFun(sliderOption.data[sliderIdx.currentIdx]);
+  const slideLength = sliderOption.data.length;
+  moveSlide(sliderOption.name, 'prev-slide', 'next-slide');
+  $(`.${sliderOption.name}__contents`).insertAdjacentHTML('beforeend', slideTemplate);
+  sliderIdx.currentIdx++;
+  sliderIdx.viewIdx++;
+  if (sliderIdx.currentIdx >= slideLength - 1) sliderIdx.currentIdx = -1;
+  if (sliderIdx.viewIdx > slideLength) sliderIdx.viewIdx = 1;
+  $(`.${sliderOption.name} .slide-paging-number__current`).textContent = sliderIdx.currentIdx;
 };
 
-const slideTimer = (slideName, sliderData, parmToMoveMainBanner) => {
+const addSlideButtonEvent = (sliderIdx, sliderOption) => {
+  $(`.${sliderOption.name} .slide-buttons`).addEventListener('click', ({ target }) => {
+    target.classList.contains('slide-button--prev')
+      ? moveToPrev(sliderIdx, sliderOption)
+      : moveToNext(sliderIdx, sliderOption);
+  });
+};
+
+const sliderTimer = (sliderIdx, sliderOption) => {
   setTimeout(() => {
-    SLIDER_INFO[parmToMoveMainBanner.idxFun](parmToMoveMainBanner.slideLength);
-    moveSlide(slideName, parmToMoveMainBanner.moveSlide);
-    addNewSlide(newSlideTemplate, parmToMoveMainBanner.insertPosition);
-    sliderIdx(SLIDER_INFO.viewIdx);
-    slideTimer(sliderData, parmToMoveMainBanner);
-  }, SLIDER_INFO.slideTime);
+    moveToNext(sliderIdx, sliderOption);
+    sliderTimer(sliderIdx, sliderOption);
+  }, sliderOption.time);
 };
 
-export const startSlide = (slideName, sliderData, parmToMoveMainBanner) => {
-  const SLIDER_INFO = {
-    slideTimer: 0,
+export const startSlider = sliderOption => {
+  const sliderIdx = {
     currentIdx: 1,
-    viewIdx: 1,
-    subtractIdx: function (slideLength) {
-      this.currentIdx--;
-      this.viewIdx--;
-      if (this.currentIdx <= 0) this.currentIdx = slideLength;
-      if (this.viewIdx <= 0) this.viewIdx = slideLength;
-    },
-    addIdx: function (slideLength) {
-      this.currentIdx++;
-      this.viewIdx++;
-      if (this.currentIdx >= slideLength - 1) this.currentIdx = -1;
-      if (this.viewIdx > slideLength) this.viewIdx = 1;
-    },
-    slideTime: 3000,
-    throttling: null
+    viewIdx: 1
   };
-
-  slideTimer(slideName, sliderData, parmToMoveMainBanner);
+  sliderTimer(sliderIdx, sliderOption);
+  addSlideButtonEvent(sliderIdx, sliderOption);
 };
