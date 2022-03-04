692168d821355e99b49a76645a5a96e65c257fa9
dony <rsefa94@gmail.com>
2022-03-04 11:11:18 +0900

    Refactor: dataUrl 전역변수 삭제

--- static/src/data/pageDataFinder.js
+++ static/src/data/pageDataFinder.js
@@ -1,12 +1,13 @@
-const DATA_URL = '/src/data/';
+export const getPageData = page => {
+  const DATA_URL = '/src/data/';
+  const dataUrl = {
+    홈: `${DATA_URL}home.json`,
+    웹툰: `${DATA_URL}webtoon.json`,
+    웹소설: `${DATA_URL}webFiction.json`,
+    영화: `${DATA_URL}movie.json`,
+    방송: `${DATA_URL}tv.json`,
+    책: `${DATA_URL}book.json`
+  };
 
-const dataUrl = {
-  홈: `${DATA_URL}home.json`,
-  웹툰: `${DATA_URL}webtoon.json`,
-  웹소설: `${DATA_URL}webFiction.json`,
-  영화: `${DATA_URL}movie.json`,
-  방송: `${DATA_URL}tv.json`,
-  책: `${DATA_URL}book.json`
+  return dataUrl[page];
 };
-
-export const getPageData = page => dataUrl[page];
