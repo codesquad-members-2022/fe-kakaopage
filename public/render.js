b1c15a245a74e8c5609478bd96700977d417dd99
nanjeong <whdwkxj7@naver.com>
2022-03-04 14:48:29 +0900

    Revert "fix: 블록들의 순서가 뒤죽박죽 나오는 것 수정"
    This reverts commit c8232b0b65b62321191afb15f1e7b9a28557b9e1.

--- public/render.js
+++ public/render.js
@@ -12,7 +12,8 @@ const fillWithData = (req) => {
   // `http://localhost:3000/${req.path}`
   fetch(`https://kakapage-nanjeong.herokuapp.com/${req.path}`)
     .then((res) => res.json())
-    .then((data) => req.createBlock(data));
+    .then((data) => req.createBlock(data))
+    .catch((err) => console.log(err));
 };
 
 const renderHome = () => {
@@ -20,66 +21,47 @@ const renderHome = () => {
   fillWithData({
     path: `${curTab}/webtoons/banner`,
     createBlock: createBanner,
-  })
-    .then(
-      fillWithData({
-        path: `${curTab}/category-btn`,
-        createBlock: createCateBtnBlock,
-      })
-    )
-    .then(
-      fillWithData({
-        path: `${curTab}/webtoons/promotion`,
-        createBlock: createPromotionBlock,
-      })
-    )
-    .then(
-      fillWithData({
-        path: `${curTab}/webtoons/days`,
-        createBlock: createDaysBlock,
-      })
-    )
-    .then(
-      fillWithData({
-        path: `${curTab}/webtoons/banner/small`,
-        createBlock: createSmallBannerBlock,
-      })
-    )
-    .then(
-      fillWithData({
-        path: `${curTab}/webtoons/drama`,
-        createBlock: createGenreBlock,
-      })
-    )
-    .then(
-      fillWithData({
-        path: `${curTab}/webtoons/top3`,
-        createBlock: createDailyRankBlock,
-      })
-    )
-    .then(
-      fillWithData({
-        path: `${curTab}/webtoons/event`,
-        createBlock: createEventBlock,
-      })
-    )
-    .catch((err) => console.log(err));
+  });
+  fillWithData({
+    path: `${curTab}/category-btn`,
+    createBlock: createCateBtnBlock,
+  });
+  fillWithData({
+    path: `${curTab}/webtoons/promotion`,
+    createBlock: createPromotionBlock,
+  });
+  fillWithData({
+    path: `${curTab}/webtoons/days`,
+    createBlock: createDaysBlock,
+  });
+  fillWithData({
+    path: `${curTab}/webtoons/banner/small`,
+    createBlock: createSmallBannerBlock,
+  });
+  fillWithData({
+    path: `${curTab}/webtoons/drama`,
+    createBlock: createGenreBlock,
+  });
+  fillWithData({
+    path: `${curTab}/webtoons/top3`,
+    createBlock: createDailyRankBlock,
+  });
+  fillWithData({
+    path: `${curTab}/webtoons/event`,
+    createBlock: createEventBlock,
+  });
 };
 
 const renderDailyContents = () => {
   const curTab = "daily-contents";
-
   fillWithData({
     path: `${curTab}/webtoons/banner`,
     createBlock: createBanner,
-  })
-    .then(
-      fillWithData({
-        path: `${curTab}/webtoons/days`,
-        createBlock: createDailyContents,
-      })
-    )
-    .catch((err) => console.log(err));
+  });
+  fillWithData({
+    path: `${curTab}/webtoons/days`,
+    createBlock: createDailyContents,
+  });
 };
 
 export { renderHome, renderDailyContents };
