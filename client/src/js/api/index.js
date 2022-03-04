99a756c2744a453b063544f64432bb8b5aeec50d
mina-gwak <mina.gwak@kakao.com>
2022-03-04 12:05:19 +0900

    refactor: Put callback into request's parameter

--- client/src/js/api/index.js
+++ client/src/js/api/index.js
@@ -1,27 +1,24 @@
 const BASE_URL = 'http://localhost:3000';
 
-const request = (url) => {
+const request = (url, callback) => {
   return fetch(url)
     .then((response) => response.json())
+    .then(callback)
     .catch((err) => console.error(err));
 };
 
 const DataApi = {
   getWebtoon(callback) {
-    return request(`${BASE_URL}/webtoon`)
-      .then(callback);
+    return request(`${BASE_URL}/webtoon`, callback);
   },
   getNavItem(callback) {
-    return request(`${BASE_URL}/nav-item`)
-      .then(callback);
+    return request(`${BASE_URL}/nav-item`, callback);
   },
   getBanner(callback) {
-    return request(`${BASE_URL}/banner`)
-      .then(callback);
+    return request(`${BASE_URL}/banner`, callback);
   },
   getCategory(callback) {
-    return request(`${BASE_URL}/category`)
-      .then(callback);
+    return request(`${BASE_URL}/category`, callback);
   },
 };
 
