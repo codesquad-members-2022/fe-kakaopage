8795e98551d73e85b74407b70c1bea6ee2b761b3
Hemudi <ksum1205@naver.com>
2022-03-04 13:26:12 +0900

    Feat: getDataWithCallback 기능 구현

--- src/Utils.js
+++ src/Utils.js
@@ -1,6 +1,13 @@
-const serverAddress = 'http://localhost:3000/';
+const serverAddress = 'http://localhost:3000';
 
 export function getData(...tag) {
-    const url = `${serverAddress}${tag.join('/')}`;
+    const url = `${serverAddress}/data/${tag.join('/')}`;
     return fetch(url).then((response) => response.json());
+}
+
+export function getDataWithCallback(callback, ...tags) {
+    const url = `${serverAddress}/data/${tags.join('/')}`;
+    fetch(url)
+        .then((response) => response.json())
+        .then((data) => callback(data));
 }
