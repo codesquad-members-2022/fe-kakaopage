1426f61c59139b9ac5fcda0fe779d259e269474d
jm <jm.dev.study@gmail.com>
2022-03-04 12:22:00 +0900

    [move] data 폴더를 public 폴더 밖으로 이동 - 폴더 이동에 따라 라우터 경로도 수정했다.

--- router/dataRouter.js
+++ router/dataRouter.js
@@ -2,12 +2,12 @@ const express = require('express');
 const router = express.Router();
 const fs = require('fs');
 
-const mainBanner = JSON.parse(fs.readFileSync('./public/data/mainBannerData.json', 'utf-8'));
-const promotionBanner = JSON.parse(fs.readFileSync('./public/data/promotionBannerData.json', 'utf-8'));
-const themeMenu = JSON.parse(fs.readFileSync('./public/data/themeMenuData.json', 'utf-8'));
-const dailyTop = JSON.parse(fs.readFileSync('./public/data/dailyTopData.json', 'utf-8'));
-const dailyRanking = JSON.parse(fs.readFileSync('./public/data/dailyRanking.json', 'utf-8'));
-const homeGenreTop = JSON.parse(fs.readFileSync('./public/data/homeGenreTopData.json', 'utf-8'));
+const mainBanner = JSON.parse(fs.readFileSync('./data/mainBannerData.json', 'utf-8'));
+const promotionBanner = JSON.parse(fs.readFileSync('./data/promotionBannerData.json', 'utf-8'));
+const themeMenu = JSON.parse(fs.readFileSync('./data/themeMenuData.json', 'utf-8'));
+const dailyTop = JSON.parse(fs.readFileSync('./data/dailyTopData.json', 'utf-8'));
+const dailyRanking = JSON.parse(fs.readFileSync('./data/dailyRanking.json', 'utf-8'));
+const homeGenreTop = JSON.parse(fs.readFileSync('./data/homeGenreTopData.json', 'utf-8'));
 
 router.get('/main-banner', function(req, res, next) {
   res.json(mainBanner);
