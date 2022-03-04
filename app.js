1d3ca0b962b95e0af58b080ca709d5792cccc2e6
jm <jm.dev.study@gmail.com>
2022-03-04 16:07:39 +0900

    [fix] app.js: 포트 설정 변경

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const app = express();
 
-app.listen(3000, function() {
-  console.log('[START] express server on port 3000');
+const port = process.env.PORT || 3000;
+app.listen(port, function() {
+  console.log('[START] express server on!');
 });
 
 app.use(express.static('public'));
