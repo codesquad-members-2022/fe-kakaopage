daa93196e32e7aee2d8d999235c00df288feaa13
sju02048 <sju02048@naver.com>
2022-03-05 22:26:48 +0900

    Squashed commit of the following:
    commit 42d3b490ca1b81dea73099399fb72c0542a0d508
    Author: jwu <sju02048@naver.com>
    Date:   Thu Mar 3 16:16:39 2022 +0900
    
        [jwu] Step5 (#187)
    
        * Add: Express Server
    
        * feat: DaySeriesList
    
        * Docs: gitignore
    
        * Add: getJson Func
    
        * Docs: modified data
    
        * Feat: express server
    
        * Feat: Template "pug"
    
        * Fix: modifiy public path
    
        * Create: pug html
    
        * Docs: modify simple data
    
        * Docs: Step 5
    
        * Fix: delete import getJson
    
        * Fix: server info
    
        * Fix: getJson URL
    
        * Fix: Rendering error

--- js/component/empty.js
+++
@@ -1,3 +0,0 @@
-export const empty = (data) => {
-  return `<h1 style="font-size: 50px; text-align:center;">${data}</h1>`;
-};
--- js/component/toon-main.js
+++
@@ -1,8 +0,0 @@
-export const toonMain = () => {
-  const template = `
-    <div class="toon-jumbotron"></div>
-    <div class="toon-todaymenu"></div>
-    <div class="toon-ad-banner-1"></div>
-    <div class="toon-daytop"></div>`;
-  return template;
-};
