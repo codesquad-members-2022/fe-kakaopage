7500141be4ce46e8d3aa0ccd20c934b8f0808608
herrakam <54533561+herrakam@users.noreply.github.com>
2022-03-08 13:58:53 +0900

    [JS] 파일 변경 부분 pr (#222)
    * .gitingore 재적용
    
    * .gitignore재적용 2차
    
    * html 로딩 성공, 경로 설정
    
    * .gitingore node-modules 미적용 고치는 중
    
    * .gitingore 오류 해결, json data server 등록 해결중
    
    * json response 성공
    
    * eventSlide 이벤트 main에서 slide로  분리, 데이터 비동기화 처리중
    
    * 요일 클릭시 컨텐츠 변환 데이터 json 기반 변환
    
    * readme 작성
    
    * .gitingore 오류 해결, json data server 등록 해결중

--- kakaoPage/app.js
+++ kakaoPage/app.js
@@ -1,42 +1,20 @@
-var createError = require("http-errors");
-var express = require("express");
-var path = require("path");
-var cookieParser = require("cookie-parser");
-var logger = require("morgan");
+const express = require("express");
+const router = require("./routes/dataRouter.js");
+const app = express();
+const cors = require("cors");
+const PORT = 3000;
+app.use(cors());
+app.use(express.static(__dirname + "/public"));
 
-var indexRouter = require("./routes/index");
-var usersRouter = require("./routes/users");
+app.use("/data", router);
 
-var app = express();
+// app.set("port", process.env.PORT || 3000);
 
-// view engine setup
-app.set("views", path.join(__dirname, "views"));
-app.engine("html", require("ejs").renderFile);
-app.set("view engine", "html");
-
-app.use(logger("dev"));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
-
-app.use("/", indexRouter);
-app.use("/users", usersRouter);
-
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  next(createError(404));
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/" + "views/kakaopageMain.html");
 });
 
-// error handler
-app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render("error");
+app.listen(PORT, (err) => {
+  if (err) return console.log(err);
+  console.log("The server is listening on port 3000");
 });
-
-module.exports = app;
--- kakaoPage/public/javascripts/main.js
+++ kakaoPage/public/javascripts/main.js
@@ -1,14 +1,25 @@
 import datas from "./data.js";
-import * as section from "./section.js";
-const weekdayContents = datas.weekdayContents;
-const eventContents = datas.eventContents;
-const sectionContents = datas.sectionContents;
-function init() {
-  section.renderWebtoonGenreBar();
+import * as webtoonSection from "./section.js";
+import getData from "./render.js";
+import eventSlide from "./slide.js";
+getData("/data").then((data) => init(data));
+function init(datas) {
+  // const weekdayContents = datas.weekdayContents;
+  const eventContents = datas.eventContents;
+  // const sectionContents = datas.sectionContents;
+  const webtoons = datas.webtoon;
+  webtoonSection.renderWebtoonGenreBar();
   // renderEvent("웹툰");
+
   section.renderEventButtons();
 }
 init();
+checkApi();
+function checkApi() {
+  const promise = fetch("http://localhost:3000/data").then((response) =>
+    console.log(response)
+  );
+}
 function changeTabsColor(target) {
   const Tabs = target.parentNode.childNodes;
   for (let i = 1; i < Tabs.length; i += 2) {
@@ -16,144 +27,99 @@ function changeTabsColor(target) {
     tab.style.borderBottom = "none";
     tab.style.color = "#999";
   }
-  target.style.color = `black`;
-}
-function removeContents(tag) {
-  while (tag.hasChildNodes()) {
-    tag.removeChild(tag.firstChild);
+  function removeContents(tag) {
+    while (tag.hasChildNodes()) {
+      tag.removeChild(tag.firstChild);
+    }
   }
-}
-function changeBorderColor(target) {
-  changeTabsColor(target);
-  target.style.borderBottom = "3px solid #ffd200";
-}
-function changeDayAndContents(target) {
-  changeBorderColor(target);
-  const dayInfo = target.textContent;
-  const weekdayContentWrap = document.querySelector("#weekdayContentWrapId");
-  removeContents(weekdayContentWrap);
-  weekdayContents.map((day) => {
-    if (dayInfo === day[0]) {
-      let temp = "";
-      for (let i = 0; i < 10; i++) {
-        temp += `${day[1]}`;
+  function changeBorderColor(target) {
+    changeTabsColor(target);
+    target.style.borderBottom = "3px solid #ffd200";
+  }
+  function changeDayAndContents(target) {
+    changeBorderColor(target);
+    const dayInfo = target.textContent;
+    const weekdayContentWrap = document.querySelector("#weekdayContentWrapId");
+    removeContents(weekdayContentWrap);
+    let temp = "";
+    webtoons.map((webtoon) => {
+      if (dayInfo === webtoon.day) {
+        let daytoonTemp = `
+        <div class="toonContent">
+        <div class="toonContentTop">
+            <img src="${webtoon.imgsrc}" alt="" class="contentImage">
+        </div>
+        <div class="contentLank heightCenterSort ">
+            <div class="contentLankInfo">${webtoon.rating}</div>
+            <img src="/images/bmbadge_waitfree.svg" class="contentClock"></img>
+        </div>
+        <div class="contentTitle">${webtoon.title}</div>
+        <div class="contentInfo">
+            <img src="/images/새 작품.svg" alt="" class="contentStatus">
+            <img src="/images/icon_read_count.png" alt="" class="contentCount">
+            <span class="contentFollow">${webtoon.fallow}만 명</span>
+        </div>
+    </div>`;
+        for (let i = 0; i < 10; i++) {
+          temp += `${daytoonTemp}`;
+        }
+        weekdayContentWrap.insertAdjacentHTML("afterbegin", temp);
       }
-      weekdayContentWrap.insertAdjacentHTML("afterbegin", temp);
-    }
-  });
-}
-function changeEventSlideContents(target) {
-  const genre = target.textContent;
-  const slide = document.querySelector(".eventSlideId");
-  const eventTitle = document.querySelector("#eventTitleId");
-  const eventFollow = document.querySelector("#eventFollowId");
-  let imageUrl, title, follow;
-  eventContents.map((content) => {
-    if (genre.trim() === content.genre) {
-      imageUrl = content.imgsrc;
-      title = content.title;
-      follow = content.follow;
-    }
-  });
-  slide.style.backgroundImage = `url('${imageUrl}')`;
-  eventTitle.textContent = title;
-  eventFollow.textContent = follow;
-}
-function changeSectionContents(target) {
-  let navGenre;
-  if (target.childElementCount === 0) {
-    // 자식요소 갯수 카운팅 해서 클릭한 테그클릭별 navGenre 정의 다르게 할 수 있었음.
-    navGenre = target.parentNode.textContent;
-  } else {
-    navGenre = target.textContent;
+    });
   }
-  const sections = document.querySelector("#sectionId");
-  removeContents(sections);
-  let temp;
-  sectionContents.map((section) => {
-    if (navGenre.trim() === section[0]) {
-      temp = section[1];
+  function changeEventSlideContents(target) {
+    const genre = target.textContent;
+    const slide = document.querySelector(".eventSlideId");
+    const eventTitle = document.querySelector("#eventTitleId");
+    const eventFollow = document.querySelector("#eventFollowId");
+    let imageUrl, title, follow;
+    eventContents.map((content) => {
+      if (genre.trim() === content.genre) {
+        imageUrl = content.imgsrc;
+        title = content.title;
+        follow = content.follow;
+      }
+    });
+    slide.style.backgroundImage = `url('${imageUrl}')`;
+    eventTitle.textContent = title;
+    eventFollow.textContent = follow;
+  }
+  function changeSectionContents(target) {
+    let navGenre;
+    if (target.childElementCount === 0) {
+      // 자식요소 갯수 카운팅 해서 클릭한 테그클릭별 navGenre 정의 다르게 할 수 있었음.
+      navGenre = target.parentNode.textContent;
+    } else {
+      navGenre = target.textContent;
     }
-  });
-  sections.insertAdjacentHTML("afterbegin", temp);
-}
+    const sections = document.querySelector("#sectionId");
+    removeContents(sections);
+    let temp;
+    sectionContents.map((section) => {
+      if (navGenre.trim() === section[0]) {
+        temp = section[1];
+      }
+    });
+    sections.insertAdjacentHTML("afterbegin", temp);
+  }
 
-const daysWrap = document.querySelector("#daysWrap");
-daysWrap.addEventListener("click", function (e) {
-  changeDayAndContents(e.target);
-});
-const webtoonGenresWrap = document.querySelector("#webtoonGenresWrap");
-webtoonGenresWrap.addEventListener("click", function (e) {
-  changeTabsColor(e.target);
-  changeEventSlideContents(e.target);
-});
-const navWrap = document.querySelector("#navWrap");
-navWrap.addEventListener("click", function (e) {
-  changeSectionContents(e.target);
-});
-const navTabs = document.querySelectorAll(".navTab");
-navTabs.forEach((tab) => {
-  tab.addEventListener("click", function (e) {
-    changeBorderColor(e.currentTarget);
+  const daysWrap = document.querySelector("#daysWrap");
+  daysWrap.addEventListener("click", function (e) {
+    changeDayAndContents(e.target);
   });
-});
-
-const prev = document.querySelector("#eventSlidePrevBtn");
-const next = document.querySelector("#eventSlideNextBtn");
-const eventSlideWrap = document.querySelector("#eventSlideId");
-const slideImgs = document.querySelectorAll(".eventSlideBox");
-const itemWidth = 720;
-let currentWidth = -720;
-let curEventPage = document.querySelector("#curEventPage");
-let curPage = 1;
-makeClone();
-eventInitLocation();
-function makeClone() {
-  // 슬라이드 양 끝에 복사
-  const cloneSlideFirst = slideImgs[0].cloneNode(true);
-  const cloneSlideLast = slideImgs[slideImgs.length - 1].cloneNode(true);
-  eventSlideWrap.append(cloneSlideFirst);
-  eventSlideWrap.insertBefore(cloneSlideLast, eventSlideWrap.firstElementChild);
+  const webtoonGenresWrap = document.querySelector("#webtoonGenresWrap");
+  webtoonGenresWrap.addEventListener("click", function (e) {
+    changeTabsColor(e.target);
+    changeEventSlideContents(e.target);
+  });
+  const navWrap = document.querySelector("#navWrap");
+  navWrap.addEventListener("click", function (e) {
+    changeSectionContents(e.target);
+  });
+  const navTabs = document.querySelectorAll(".navTab");
+  navTabs.forEach((tab) => {
+    tab.addEventListener("click", function (e) {
+      changeBorderColor(e.currentTarget);
+    });
+  });
 }
-function eventInitLocation() {
-  // 복사 슬라이드 아닌 첫번째 슬라이드가 보이게!
-  eventSlideWrap.style.transform = `translate(-${itemWidth}px)`;
-}
-next.addEventListener("click", function () {
-  if (curPage <= slideImgs.length) {
-    console.log(currentWidth);
-    currentWidth -= itemWidth;
-    eventSlideWrap.style.transform = `translate(${currentWidth}px)`;
-    eventSlideWrap.style.transition = "0.5s";
-  }
-  if (curPage === slideImgs.length) {
-    setTimeout(() => {
-      currentWidth = -720;
-      eventSlideWrap.style.transform = `translate(${currentWidth}px)`;
-      eventSlideWrap.style.transition = "0s";
-    }, 500);
-    curPage = 0;
-    curEventPage.textContent = curPage;
-  }
-  curPage += 1;
-  curEventPage.textContent = curPage;
-});
-prev.addEventListener("click", function () {
-  if (curPage >= 1) {
-    currentWidth += itemWidth;
-    eventSlideWrap.style.transform = `translate(${currentWidth}px)`;
-    eventSlideWrap.style.transition = "0.5s";
-  }
-  if (curPage === 1) {
-    setTimeout(() => {
-      currentWidth = -((slideImgs.length - 1) * itemWidth);
-      console.log(currentWidth);
-      eventSlideWrap.style.transform = `translate(${currentWidth}px)`;
-      eventSlideWrap.style.transition = "0s";
-    }, 500);
-    curPage = slideImgs.length + 1;
-    curEventPage.textContent = curPage;
-  }
-  curPage -= 1;
-  curEventPage.textContent = curPage;
-});
